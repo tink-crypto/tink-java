30c13f803c97d76a15e2d87a348b8616178dcc1d
Moreno Ambrosin <ambrosin@google.com>
2022-08-05 05:59:11 -0700

    Remove manual tests from Kokoro test script.
    PiperOrigin-RevId: 465545804
    Change-Id: I8465b75ffeecbf08d2496a742f58ddb7eed6fc7e

--- kokoro/gcp_ubuntu/examples/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/examples/bazel/run_tests.sh
@@ -16,40 +16,24 @@
 
 set -euo pipefail
 
-readonly WORKSPACE_FOLDER="examples"
-
 # If we are running on Kokoro cd into the repository.
 if [[ -n "${KOKORO_ROOT:-}" ]]; then
   cd "${KOKORO_ARTIFACTS_DIR}/git/tink_java"
-  use_bazel.sh "$(cat ${WORKSPACE_FOLDER}/.bazelversion)"
+  use_bazel.sh "$(cat examples/.bazelversion)"
 fi
 
 TINK_BASE_DIR="$(pwd)/.."
 
 # Sourcing required to update caller's environment.
 source ./kokoro/testutils/install_python3.sh
-./kokoro/testutils/copy_credentials.sh "${WORKSPACE_FOLDER}/testdata"
 ./kokoro/testutils/update_android_sdk.sh
 
-cp "${WORKSPACE_FOLDER}/WORKSPACE" "${WORKSPACE_FOLDER}/WORKSPACE.bak"
+cp "examples/WORKSPACE" "examples/WORKSPACE.bak"
 
 ./kokoro/testutils/replace_http_archive_with_local_repository.py \
-  -f "${WORKSPACE_FOLDER}/WORKSPACE" \
+  -f "examples/WORKSPACE" \
   -t "${TINK_BASE_DIR}"
 
-# Targets tagged as "manual" that require setting GCP credentials.
-MANUAL_EXAMPLE_JAVA_TARGETS=()
-if [[ -n "${KOKORO_ROOT:-}" ]]; then
-  MANUAL_EXAMPLE_JAVA_TARGETS=(
-    "//gcs:gcs_envelope_aead_example_test"
-    "//encryptedkeyset:encrypted_keyset_example_test"
-    "//envelopeaead:envelope_aead_example_test"
-  )
-fi
-readonly MANUAL_EXAMPLE_JAVA_TARGETS
+./kokoro/testutils/run_bazel_tests.sh "examples"
 
-./kokoro/testutils/run_bazel_tests.sh \
-  "${WORKSPACE_FOLDER}" \
-  "${MANUAL_EXAMPLE_JAVA_TARGETS[@]}"
-
-mv "${WORKSPACE_FOLDER}/WORKSPACE.bak" "${WORKSPACE_FOLDER}/WORKSPACE"
+mv "examples/WORKSPACE.bak" "examples/WORKSPACE"
