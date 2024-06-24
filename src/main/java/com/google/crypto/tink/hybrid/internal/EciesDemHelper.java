e13941db022569234a0a8d779abf243210f312fa
Juerg Wullschleger <juerg@google.com>
2024-06-24 07:39:08 -0700

    Avoid memory allocation and copy in ECIES Decryption with AES GCM as DEM.
    PiperOrigin-RevId: 646090114
    Change-Id: Ic1789f39a18d0f010e26938acb17684e6c360a0b

--- src/main/java/com/google/crypto/tink/hybrid/internal/EciesDemHelper.java
+++ src/main/java/com/google/crypto/tink/hybrid/internal/EciesDemHelper.java
@@ -23,18 +23,21 @@ import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.Parameters;
 import com.google.crypto.tink.aead.AesCtrHmacAeadKey;
 import com.google.crypto.tink.aead.AesCtrHmacAeadParameters;
-import com.google.crypto.tink.aead.AesGcmKey;
 import com.google.crypto.tink.aead.AesGcmParameters;
+import com.google.crypto.tink.aead.internal.AesGcmJceUtil;
 import com.google.crypto.tink.daead.AesSivKey;
 import com.google.crypto.tink.daead.AesSivParameters;
 import com.google.crypto.tink.hybrid.EciesParameters;
-import com.google.crypto.tink.subtle.AesGcmJce;
 import com.google.crypto.tink.subtle.AesSiv;
 import com.google.crypto.tink.subtle.Bytes;
 import com.google.crypto.tink.subtle.EncryptThenAuthenticate;
+import com.google.crypto.tink.subtle.Random;
 import com.google.crypto.tink.util.SecretBytes;
 import java.security.GeneralSecurityException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 /** Helper functions for ECIES Data Encryption Mechanism (DEM). */
 public final class EciesDemHelper {
@@ -52,11 +55,21 @@ public final class EciesDemHelper {
   }
 
   private static final class AesGcmDem implements Dem {
-    private final AesGcmParameters parameters;
+    private static final int AES_GCM_IV_SIZE_IN_BYTES = AesGcmJceUtil.IV_SIZE_IN_BYTES;
+    private static final int AES_GCM_TAG_SIZE_IN_BYTES = AesGcmJceUtil.TAG_SIZE_IN_BYTES;
+
     private final int keySizeInBytes;
 
-    public AesGcmDem(AesGcmParameters parameters) {
-      this.parameters = parameters;
+    public AesGcmDem(AesGcmParameters parameters) throws GeneralSecurityException {
+      if (parameters.getIvSizeBytes() != AES_GCM_IV_SIZE_IN_BYTES) {
+        throw new GeneralSecurityException("invalid IV size");
+      }
+      if (parameters.getTagSizeBytes() != AES_GCM_TAG_SIZE_IN_BYTES) {
+        throw new GeneralSecurityException("invalid tag size");
+      }
+      if (parameters.getVariant() != AesGcmParameters.Variant.NO_PREFIX) {
+        throw new GeneralSecurityException("invalid variant");
+      }
       this.keySizeInBytes = parameters.getKeySizeBytes();
     }
 
@@ -65,20 +78,47 @@ public final class EciesDemHelper {
       return keySizeInBytes;
     }
 
-    @AccessesPartialKey
-    private Aead getAead(byte[] symmetricKeyValue) throws GeneralSecurityException {
-      return AesGcmJce.create(
-          AesGcmKey.builder()
-              .setParameters(parameters)
-              .setKeyBytes(SecretBytes.copyFrom(symmetricKeyValue, InsecureSecretKeyAccess.get()))
-              .build());
-    }
-
     @Override
     public byte[] encrypt(byte[] demKeyValue, byte[] prefix, byte[] header, byte[] plaintext)
         throws GeneralSecurityException {
-      byte[] ciphertext = getAead(demKeyValue).encrypt(plaintext, EMPTY_AAD);
-      return Bytes.concat(prefix, header, ciphertext);
+      if (demKeyValue.length != keySizeInBytes) {
+        throw new GeneralSecurityException("invalid key size");
+      }
+      SecretKey keySpec = AesGcmJceUtil.getSecretKey(demKeyValue);
+      byte[] nonce = Random.randBytes(AES_GCM_IV_SIZE_IN_BYTES);
+      AlgorithmParameterSpec params = AesGcmJceUtil.getParams(nonce);
+      Cipher cipher = AesGcmJceUtil.getThreadLocalCipher();
+      cipher.init(Cipher.ENCRYPT_MODE, keySpec, params);
+      int outputSize = cipher.getOutputSize(plaintext.length);
+      int prefixAndHeaderSize = prefix.length + header.length;
+      if (outputSize > Integer.MAX_VALUE - prefixAndHeaderSize - AES_GCM_IV_SIZE_IN_BYTES) {
+        throw new GeneralSecurityException("plaintext too long");
+      }
+      int len = prefixAndHeaderSize + AES_GCM_IV_SIZE_IN_BYTES + outputSize;
+      byte[] output = Arrays.copyOf(prefix, len);
+      System.arraycopy(
+          /* src= */ header,
+          /* srcPos= */ 0,
+          /* dest= */ output,
+          /* destPos= */ prefix.length,
+          /* length= */ header.length);
+      System.arraycopy(
+          /* src= */ nonce,
+          /* srcPos= */ 0,
+          /* dest= */ output,
+          /* destPos= */ prefixAndHeaderSize,
+          /* length= */ AES_GCM_IV_SIZE_IN_BYTES);
+      int written =
+          cipher.doFinal(
+              plaintext,
+              0,
+              plaintext.length,
+              output,
+              prefixAndHeaderSize + AES_GCM_IV_SIZE_IN_BYTES);
+      if (written != outputSize) {
+        throw new GeneralSecurityException("not enough data written");
+      }
+      return output;
     }
 
     @Override
@@ -87,8 +127,21 @@ public final class EciesDemHelper {
       if (ciphertext.length < prefixAndHeaderSize) {
         throw new GeneralSecurityException("ciphertext too short");
       }
-      byte[] demCiphertext = Arrays.copyOfRange(ciphertext, prefixAndHeaderSize, ciphertext.length);
-      return getAead(demKeyValue).decrypt(demCiphertext, EMPTY_AAD);
+      if (demKeyValue.length != keySizeInBytes) {
+        throw new GeneralSecurityException("invalid key size");
+      }
+      SecretKey key = AesGcmJceUtil.getSecretKey(demKeyValue);
+      if (ciphertext.length
+          < prefixAndHeaderSize + AES_GCM_IV_SIZE_IN_BYTES + AES_GCM_TAG_SIZE_IN_BYTES) {
+        throw new GeneralSecurityException("ciphertext too short");
+      }
+      AlgorithmParameterSpec params =
+          AesGcmJceUtil.getParams(ciphertext, prefixAndHeaderSize, AES_GCM_IV_SIZE_IN_BYTES);
+      Cipher cipher = AesGcmJceUtil.getThreadLocalCipher();
+      cipher.init(Cipher.DECRYPT_MODE, key, params);
+      int offset = prefixAndHeaderSize + AES_GCM_IV_SIZE_IN_BYTES;
+      int len = ciphertext.length - prefixAndHeaderSize - AES_GCM_IV_SIZE_IN_BYTES;
+      return cipher.doFinal(ciphertext, offset, len);
     }
   }
 
