95793815e901102a78edeee07b9fcb0829aae21e
Juerg Wullschleger <juerg@google.com>
2024-07-17 01:37:20 -0700

    Add implementations of RSA SSA PSS verification using Conscrypt.
    It is very similar to RsaSsaPkcs1VerifyConscrypt.java, and implements the same
    functionality as RsaSsaPkcs1VerifyJce, but lets Conscrypt do all the work, so that
    it can be used in FIPS-only mode later.
    
    Note that the algorithm names for different providers are different, see:
    https://github.com/C2SP/wycheproof/blob/master/doc/rsa.md,
    https://github.com/google/conscrypt/blob/master/CAPABILITIES.md#signature
    
    PiperOrigin-RevId: 653138810
    Change-Id: Icba2aab792fb7c066255ac52f5e75968d20de948

---
+++ src/main/java/com/google/crypto/tink/signature/internal/RsaSsaPssVerifyConscrypt.java
@@ -0,0 +1,194 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature.internal;
+
+import static com.google.crypto.tink.internal.Util.isPrefix;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.PublicKeyVerify;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.Util;
+import com.google.crypto.tink.signature.RsaSsaPssParameters;
+import com.google.crypto.tink.signature.RsaSsaPssPublicKey;
+import com.google.crypto.tink.subtle.Validators;
+import com.google.errorprone.annotations.Immutable;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.Provider;
+import java.security.Security;
+import java.security.Signature;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.MGF1ParameterSpec;
+import java.security.spec.PSSParameterSpec;
+import java.security.spec.RSAPublicKeySpec;
+import javax.annotation.Nullable;
+
+/** RSA SSA PSS verification with Conscrypt. */
+@Immutable
+public final class RsaSsaPssVerifyConscrypt implements PublicKeyVerify {
+  public static final TinkFipsUtil.AlgorithmFipsCompatibility FIPS =
+      TinkFipsUtil.AlgorithmFipsCompatibility.ALGORITHM_REQUIRES_BORINGCRYPTO;
+
+  private static final byte[] EMPTY = new byte[0];
+  private static final byte[] LEGACY_MESSAGE_SUFFIX = new byte[] {0};
+
+  private static final String[] CONSCRYPT_PROVIDER_NAMES =
+      new String[] {"GmsCore_OpenSSL", "AndroidOpenSSL", "Conscrypt"};
+
+  private static final String MGF_1 = "MGF1";
+
+  private static final int TRAILER_FIELD_BC = 1;
+
+  @Nullable
+  private static Provider conscryptProviderOrNull() {
+    if (Util.isAndroid() && Util.getAndroidApiLevel() <= 23) {
+      // On Android API level 23 or lower, RSA SSA PSS is not supported.
+      return null;
+    }
+    for (String providerName : CONSCRYPT_PROVIDER_NAMES) {
+      Provider provider = Security.getProvider(providerName);
+      if (provider != null) {
+        return provider;
+      }
+    }
+    return null;
+  }
+
+  private static final Provider PROVIDER = conscryptProviderOrNull();
+
+  /** Returns true if Conscrypt is available and supports RSA SSA PSS. */
+  public static boolean isSupported() {
+    return PROVIDER != null;
+  }
+
+  @SuppressWarnings("Immutable")
+  private final RSAPublicKey publicKey;
+
+  private final String signatureAlgorithm;
+
+  @SuppressWarnings("Immutable")
+  private final PSSParameterSpec parameterSpec;
+
+  @SuppressWarnings("Immutable")
+  private final byte[] outputPrefix;
+
+  @SuppressWarnings("Immutable")
+  private final byte[] messageSuffix;
+
+  // These are the RSA SSA PSS algorithm names used by Conscrypt. See:
+  // https://github.com/google/conscrypt/blob/master/CAPABILITIES.md#signature
+  // Conscrypt does not support "RSASSA-PSS" used by OpenJDK,
+  // see: https://github.com/C2SP/wycheproof/blob/master/doc/rsa.md
+  private static String toConscryptRsaSsaPssAlgo(RsaSsaPssParameters.HashType hash) {
+    if (hash == RsaSsaPssParameters.HashType.SHA256) {
+      return "SHA256withRSA/PSS";
+    } else if (hash == RsaSsaPssParameters.HashType.SHA384) {
+      return "SHA384withRSA/PSS";
+    } else if (hash == RsaSsaPssParameters.HashType.SHA512) {
+      return "SHA512withRSA/PSS";
+    }
+    throw new IllegalArgumentException("Unsupported hash: " + hash);
+  }
+
+  // The MD name value in PSSParameterSpec must match the hash type in the algorithm name.
+  private static String getMdName(RsaSsaPssParameters.HashType sigHash) {
+    if (sigHash == RsaSsaPssParameters.HashType.SHA256) {
+      return "SHA-256";
+    } else if (sigHash == RsaSsaPssParameters.HashType.SHA384) {
+      return "SHA-384";
+    } else if (sigHash == RsaSsaPssParameters.HashType.SHA512) {
+      return "SHA-512";
+    }
+    throw new IllegalArgumentException("Unsupported MD hash: " + sigHash);
+  }
+
+  private static MGF1ParameterSpec getMgf1Hash(RsaSsaPssParameters.HashType mgf1Hash) {
+    if (mgf1Hash == RsaSsaPssParameters.HashType.SHA256) {
+      return MGF1ParameterSpec.SHA256;
+    } else if (mgf1Hash == RsaSsaPssParameters.HashType.SHA384) {
+      return MGF1ParameterSpec.SHA384;
+    } else if (mgf1Hash == RsaSsaPssParameters.HashType.SHA512) {
+      return MGF1ParameterSpec.SHA512;
+    }
+    throw new IllegalArgumentException("Unsupported MGF1 hash: " + mgf1Hash);
+  }
+
+  private RsaSsaPssVerifyConscrypt(
+      final RSAPublicKey pubKey,
+      RsaSsaPssParameters.HashType sigHash,
+      RsaSsaPssParameters.HashType mgf1Hash,
+      int saltLength,
+      byte[] outputPrefix,
+      byte[] messageSuffix)
+      throws GeneralSecurityException {
+    if (!FIPS.isCompatible()) {
+      throw new GeneralSecurityException(
+          "Cannot use RSA SSA PSS in FIPS-mode, as BoringCrypto module is not available.");
+    }
+    if (!sigHash.equals(mgf1Hash)) {
+      throw new GeneralSecurityException("sigHash and mgf1Hash must be the same");
+    }
+    Validators.validateRsaModulusSize(pubKey.getModulus().bitLength());
+    Validators.validateRsaPublicExponent(pubKey.getPublicExponent());
+    this.publicKey = pubKey;
+    this.signatureAlgorithm = toConscryptRsaSsaPssAlgo(sigHash);
+    this.parameterSpec =
+        new PSSParameterSpec(
+            getMdName(sigHash), MGF_1, getMgf1Hash(mgf1Hash), saltLength, TRAILER_FIELD_BC);
+    this.outputPrefix = outputPrefix;
+    this.messageSuffix = messageSuffix;
+  }
+
+  @AccessesPartialKey
+  public static PublicKeyVerify create(RsaSsaPssPublicKey key) throws GeneralSecurityException {
+    if (!isSupported()) {
+      throw new GeneralSecurityException("RSA SSA PSS using Conscrypt is not supported.");
+    }
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA", PROVIDER);
+    RSAPublicKey publicKey =
+        (RSAPublicKey)
+            keyFactory.generatePublic(
+                new RSAPublicKeySpec(key.getModulus(), key.getParameters().getPublicExponent()));
+    RsaSsaPssParameters params = key.getParameters();
+    return new RsaSsaPssVerifyConscrypt(
+        publicKey,
+        params.getSigHashType(),
+        params.getMgf1HashType(),
+        params.getSaltLengthBytes(),
+        key.getOutputPrefix().toByteArray(),
+        key.getParameters().getVariant().equals(RsaSsaPssParameters.Variant.LEGACY)
+            ? LEGACY_MESSAGE_SUFFIX
+            : EMPTY);
+  }
+
+  @Override
+  public void verify(final byte[] signature, final byte[] data) throws GeneralSecurityException {
+    if (!isPrefix(outputPrefix, signature)) {
+      throw new GeneralSecurityException("Invalid signature (output prefix mismatch)");
+    }
+    Signature verifier = Signature.getInstance(signatureAlgorithm, PROVIDER);
+    verifier.initVerify(publicKey);
+    verifier.setParameter(parameterSpec);
+    verifier.update(data);
+    if (messageSuffix.length > 0) {
+      verifier.update(messageSuffix);
+    }
+    if (!verifier.verify(signature, outputPrefix.length, signature.length - outputPrefix.length)) {
+      throw new GeneralSecurityException("signature verification failed");
+    }
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/signature/internal/RsaSsaPssVerifyConscryptTest.java
@@ -0,0 +1,183 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.PublicKeyVerify;
+import com.google.crypto.tink.internal.Util;
+import com.google.crypto.tink.signature.RsaSsaPssParameters;
+import com.google.crypto.tink.signature.RsaSsaPssPublicKey;
+import com.google.crypto.tink.signature.internal.testing.RsaSsaPssTestUtil;
+import com.google.crypto.tink.signature.internal.testing.SignatureTestVector;
+import com.google.crypto.tink.subtle.Bytes;
+import com.google.crypto.tink.subtle.Hex;
+import com.google.crypto.tink.testing.WycheproofTestUtil;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.Security;
+import java.util.Arrays;
+import org.conscrypt.Conscrypt;
+import org.junit.Before;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/** Unit tests for {@link RsaSsaPssVerifyConscrypt}. */
+@RunWith(Theories.class)
+public class RsaSsaPssVerifyConscryptTest {
+
+  @Before
+  public void useConscrypt() throws Exception {
+    if (!Util.isAndroid()) {
+      Conscrypt.checkAvailability();
+      Security.addProvider(Conscrypt.newProvider());
+    }
+  }
+
+  @Theory
+  public void isSupported() throws Exception {
+    assertThat(RsaSsaPssVerifyConscrypt.isSupported()).isTrue();
+  }
+
+  @DataPoints("testVectors")
+  public static final SignatureTestVector[] TEST_VECTORS =
+      RsaSsaPssTestUtil.createRsaPssTestVectors();
+
+  @Theory
+  public void verifySignatureInTestVector_works(
+      @FromDataPoints("testVectors") SignatureTestVector testVector) throws Exception {
+    PublicKeyVerify verifier =
+        RsaSsaPssVerifyConscrypt.create(
+            (RsaSsaPssPublicKey) testVector.getPrivateKey().getPublicKey());
+    verifier.verify(testVector.getSignature(), testVector.getMessage());
+
+    // Test that verify fails when message is modified.
+    byte[] modifiedMessage = Bytes.concat(testVector.getMessage(), new byte[] {1});
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> verifier.verify(testVector.getSignature(), modifiedMessage));
+  }
+
+  @Theory
+  public void modifiedOutputPrefix_throws(
+      @FromDataPoints("testVectors") SignatureTestVector testVector) throws Exception {
+    RsaSsaPssPublicKey testPublicKey =
+        (RsaSsaPssPublicKey) testVector.getPrivateKey().getPublicKey();
+    if (testPublicKey.getOutputPrefix().size() == 0) {
+      return;
+    }
+    byte[] modifiedSignature = testVector.getSignature();
+    modifiedSignature[1] ^= 0x01;
+    PublicKeyVerify verifier = RsaSsaPssVerifyConscrypt.create(testPublicKey);
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            verifier.verify(
+                Arrays.copyOf(modifiedSignature, modifiedSignature.length),
+                testVector.getMessage()));
+  }
+
+  private static RsaSsaPssParameters.HashType getHashType(String sha) {
+    switch (sha) {
+      case "SHA-256":
+        return RsaSsaPssParameters.HashType.SHA256;
+      case "SHA-512":
+        return RsaSsaPssParameters.HashType.SHA512;
+      default:
+        throw new IllegalArgumentException("Unsupported hash: " + sha);
+    }
+  }
+
+  @DataPoints("wycheproofTestVectorPaths")
+  public static final String[] WYCHEPROOF_TEST_VECTORS_PATHS =
+      new String[] {
+        "../wycheproof/testvectors/rsa_pss_2048_sha256_mgf1_0_test.json",
+        "../wycheproof/testvectors/rsa_pss_2048_sha256_mgf1_32_test.json",
+        "../wycheproof/testvectors/rsa_pss_3072_sha256_mgf1_32_test.json",
+        "../wycheproof/testvectors/rsa_pss_4096_sha256_mgf1_32_test.json",
+        "../wycheproof/testvectors/rsa_pss_4096_sha512_mgf1_32_test.json"
+      };
+
+  @Theory
+  public void wycheproofVectors(@FromDataPoints("wycheproofTestVectorPaths") String path)
+      throws Exception {
+    JsonObject jsonObj = WycheproofTestUtil.readJson(path);
+
+    int errors = 0;
+    JsonArray testGroups = jsonObj.getAsJsonArray("testGroups");
+    for (int i = 0; i < testGroups.size(); i++) {
+      JsonObject group = testGroups.get(i).getAsJsonObject();
+      BigInteger modulus = new BigInteger(group.get("n").getAsString(), 16);
+      BigInteger exponent = new BigInteger(group.get("e").getAsString(), 16);
+      RsaSsaPssParameters.HashType hashType = getHashType(group.get("sha").getAsString());
+      RsaSsaPssParameters.HashType mgf1HashType = getHashType(group.get("mgfSha").getAsString());
+      int saltLength = group.get("sLen").getAsInt();
+
+      JsonArray tests = group.getAsJsonArray("tests");
+      for (int j = 0; j < tests.size(); j++) {
+        JsonObject testcase = tests.get(j).getAsJsonObject();
+        String tcId =
+            String.format(
+                "testcase %d (%s)",
+                testcase.get("tcId").getAsInt(), testcase.get("comment").getAsString());
+        RsaSsaPssParameters parameters =
+            RsaSsaPssParameters.builder()
+                .setModulusSizeBits(modulus.bitLength())
+                .setPublicExponent(exponent)
+                .setSigHashType(hashType)
+                .setMgf1HashType(mgf1HashType)
+                .setSaltLengthBytes(saltLength)
+                .setVariant(RsaSsaPssParameters.Variant.NO_PREFIX)
+                .build();
+        RsaSsaPssPublicKey publicKey =
+            RsaSsaPssPublicKey.builder().setParameters(parameters).setModulus(modulus).build();
+        PublicKeyVerify verifier = RsaSsaPssVerifyConscrypt.create(publicKey);
+        byte[] msg = getMessage(testcase);
+        byte[] sig = Hex.decode(testcase.get("sig").getAsString());
+        String result = testcase.get("result").getAsString();
+        try {
+          verifier.verify(sig, msg);
+          if (result.equals("invalid")) {
+            System.out.printf("FAIL %s: accepting invalid signature%n", tcId);
+            errors++;
+          }
+        } catch (GeneralSecurityException ex) {
+          if (result.equals("valid")) {
+            System.out.printf("FAIL %s: rejecting valid signature, exception: %s%n", tcId, ex);
+            errors++;
+          }
+        }
+      }
+    }
+    assertThat(errors).isEqualTo(0);
+  }
+
+  private static byte[] getMessage(JsonObject testcase) {
+    // Previous version of Wycheproof test vectors uses "message" while the new one uses "msg".
+    if (testcase.has("msg")) {
+      return Hex.decode(testcase.get("msg").getAsString());
+    } else {
+      return Hex.decode(testcase.get("message").getAsString());
+    }
+  }
+}
