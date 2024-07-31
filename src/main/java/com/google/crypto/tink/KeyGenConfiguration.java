a18f789ef303eaf7a65664f3f022c53c3a7f50e3
Liza Tretyakova <lizatretyakova@google.com>
2024-07-31 04:20:55 -0700

    Introduce the KeyGenConfiguration interface.
    PiperOrigin-RevId: 657954084
    Change-Id: I120a7e60e4791c5e769a0a6fa8f2763872deb020

---
+++ src/main/java/com/google/crypto/tink/KeyGenConfiguration.java
@@ -0,0 +1,24 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink;
+
+/**
+ * An object of this class represents a collection of key types that a user wants Tink to
+ * understand. For most users, one of the predefined {@link KeyGenConfiguration} objects is to
+ * be used at key creation time.
+ */
+public abstract class KeyGenConfiguration {}
