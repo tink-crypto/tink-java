c7ad4dba704bacbf04bfea5e81dc382e35057464
Juerg Wullschleger <juerg@google.com>
2024-10-23 00:09:20 -0700

    Use JCE implementation of X25519 in HPKE, if it is available.
    This allows the user to use Conscrypt, which is our preferred choice in Java.
    Also, it uses less memory and is faster.
    
    PiperOrigin-RevId: 688844291
    Change-Id: I3a524c2da97507e9c53c1d4944e833f75ec701de

--- src/main/java/com/google/crypto/tink/hybrid/internal/X25519HpkeKem.java
+++ src/main/java/com/google/crypto/tink/hybrid/internal/X25519HpkeKem.java
@@ -22,14 +22,22 @@ import com.google.errorprone.annotations.Immutable;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 
-/** Diffie-Hellman-based X25519-HKDF HPKE KEM variant. */
+/**
+ * Diffie-Hellman-based X25519-HKDF HPKE KEM variant.
+ *
+ * <p>It uses the JCE implementation if available. If not, it uses Tink's subtle implementation.
+ *
+ * <p>On Android and on Java since JDK 11, the JCE implementation is available by default.
+ */
 @Immutable
 final class X25519HpkeKem implements HpkeKem {
   private final HkdfHpkeKdf hkdf;
+  private final boolean useJce;
 
   /** Construct X25519-HKDF HPKE KEM using {@code hkdf}. */
   X25519HpkeKem(HkdfHpkeKdf hkdf) {
     this.hkdf = hkdf;
+    this.useJce = X25519Jce.isSupported();
   }
 
   private byte[] deriveKemSharedSecret(
@@ -66,7 +74,12 @@ final class X25519HpkeKem implements HpkeKem {
   HpkeKemEncapOutput encapsulateWithFixedEphemeralKey(
       byte[] recipientPublicKey, byte[] ephemeralPrivateKey, byte[] ephemeralPublicKey)
       throws GeneralSecurityException {
-    byte[] dhSharedSecret = X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey);
+    byte[] dhSharedSecret;
+    if (useJce) {
+      dhSharedSecret = X25519Jce.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey);
+    } else {
+      dhSharedSecret = X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey);
+    }
     byte[] kemSharedSecret =
         deriveKemSharedSecret(dhSharedSecret, ephemeralPublicKey, recipientPublicKey);
     return new HpkeKemEncapOutput(kemSharedSecret, ephemeralPublicKey);
@@ -74,8 +87,17 @@ final class X25519HpkeKem implements HpkeKem {
 
   @Override
   public HpkeKemEncapOutput encapsulate(byte[] recipientPublicKey) throws GeneralSecurityException {
-    byte[] ephemeralPrivateKey = X25519.generatePrivateKey();
-    byte[] ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    byte[] ephemeralPrivateKey;
+    byte[] ephemeralPublicKey;
+    if (useJce) {
+      X25519Jce.KeyPair keyPair = X25519Jce.generateKeyPair();
+      ephemeralPrivateKey = keyPair.privateKey;
+      ephemeralPublicKey = keyPair.publicKey;
+    } else {
+      ephemeralPrivateKey = X25519.generatePrivateKey();
+      ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    }
+
     return encapsulateWithFixedEphemeralKey(
         recipientPublicKey, ephemeralPrivateKey, ephemeralPublicKey);
   }
@@ -87,11 +109,20 @@ final class X25519HpkeKem implements HpkeKem {
       byte[] ephemeralPublicKey,
       HpkeKemPrivateKey senderPrivateKey)
       throws GeneralSecurityException {
-    byte[] dhSharedSecret =
-        Bytes.concat(
-            X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey),
-            X25519.computeSharedSecret(
-                senderPrivateKey.getSerializedPrivate().toByteArray(), recipientPublicKey));
+    byte[] dhSharedSecret;
+    if (useJce) {
+      dhSharedSecret =
+          Bytes.concat(
+              X25519Jce.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey),
+              X25519Jce.computeSharedSecret(
+                  senderPrivateKey.getSerializedPrivate().toByteArray(), recipientPublicKey));
+    } else {
+      dhSharedSecret =
+          Bytes.concat(
+              X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey),
+              X25519.computeSharedSecret(
+                  senderPrivateKey.getSerializedPrivate().toByteArray(), recipientPublicKey));
+    }
     byte[] senderPublicKey = senderPrivateKey.getSerializedPublic().toByteArray();
     byte[] kemSharedSecret =
         deriveKemSharedSecret(
@@ -103,8 +134,16 @@ final class X25519HpkeKem implements HpkeKem {
   public HpkeKemEncapOutput authEncapsulate(
       byte[] recipientPublicKey, HpkeKemPrivateKey senderPrivateKey)
       throws GeneralSecurityException {
-    byte[] ephemeralPrivateKey = X25519.generatePrivateKey();
-    byte[] ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    byte[] ephemeralPrivateKey;
+    byte[] ephemeralPublicKey;
+    if (useJce) {
+      X25519Jce.KeyPair keyPair = X25519Jce.generateKeyPair();
+      ephemeralPrivateKey = keyPair.privateKey;
+      ephemeralPublicKey = keyPair.publicKey;
+    } else {
+      ephemeralPrivateKey = X25519.generatePrivateKey();
+      ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    }
     return authEncapsulateWithFixedEphemeralKey(
         recipientPublicKey, ephemeralPrivateKey, ephemeralPublicKey, senderPrivateKey);
   }
@@ -112,9 +151,16 @@ final class X25519HpkeKem implements HpkeKem {
   @Override
   public byte[] decapsulate(byte[] encapsulatedKey, HpkeKemPrivateKey recipientPrivateKey)
       throws GeneralSecurityException {
-    byte[] dhSharedSecret =
-        X25519.computeSharedSecret(
-            recipientPrivateKey.getSerializedPrivate().toByteArray(), encapsulatedKey);
+    byte[] dhSharedSecret;
+    if (useJce) {
+      dhSharedSecret =
+          X25519Jce.computeSharedSecret(
+              recipientPrivateKey.getSerializedPrivate().toByteArray(), encapsulatedKey);
+    } else {
+      dhSharedSecret =
+          X25519.computeSharedSecret(
+              recipientPrivateKey.getSerializedPrivate().toByteArray(), encapsulatedKey);
+    }
     return deriveKemSharedSecret(
         dhSharedSecret, encapsulatedKey, recipientPrivateKey.getSerializedPublic().toByteArray());
   }
@@ -124,10 +170,18 @@ final class X25519HpkeKem implements HpkeKem {
       byte[] encapsulatedKey, HpkeKemPrivateKey recipientPrivateKey, byte[] senderPublicKey)
       throws GeneralSecurityException {
     byte[] privateKey = recipientPrivateKey.getSerializedPrivate().toByteArray();
-    byte[] dhSharedSecret =
-        Bytes.concat(
-            X25519.computeSharedSecret(privateKey, encapsulatedKey),
-            X25519.computeSharedSecret(privateKey, senderPublicKey));
+    byte[] dhSharedSecret;
+    if (useJce) {
+      dhSharedSecret =
+          Bytes.concat(
+              X25519Jce.computeSharedSecret(privateKey, encapsulatedKey),
+              X25519Jce.computeSharedSecret(privateKey, senderPublicKey));
+    } else {
+      dhSharedSecret =
+          Bytes.concat(
+              X25519.computeSharedSecret(privateKey, encapsulatedKey),
+              X25519.computeSharedSecret(privateKey, senderPublicKey));
+    }
     byte[] recipientPublicKey = recipientPrivateKey.getSerializedPublic().toByteArray();
     return deriveKemSharedSecret(
         dhSharedSecret, encapsulatedKey, recipientPublicKey, senderPublicKey);
