148657be76a4d692e9e9e056a4a20503178660fa
Juerg Wullschleger <juerg@google.com>
2024-10-30 05:14:02 -0700

    Use Conscrypt implementation of X25519 in HPKE, if it is available.
    Android uses Conscrypt and has support since API level 31. This increases the performance by a factor of 3-5, and also lowers the memory usage.
    
    PiperOrigin-RevId: 691378379
    Change-Id: Ic1e86a0be97d84655479258ea4dae4976e5fe452

--- src/main/java/com/google/crypto/tink/hybrid/internal/X25519HpkeKem.java
+++ src/main/java/com/google/crypto/tink/hybrid/internal/X25519HpkeKem.java
@@ -17,19 +17,51 @@
 package com.google.crypto.tink.hybrid.internal;
 
 import com.google.crypto.tink.subtle.Bytes;
-import com.google.crypto.tink.subtle.X25519;
 import com.google.errorprone.annotations.Immutable;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 
-/** Diffie-Hellman-based X25519-HKDF HPKE KEM variant. */
+/**
+ * Diffie-Hellman-based X25519-HKDF HPKE KEM variant.
+ *
+ * <p>It uses the Conscrypt implementation if available. If not, it uses Tink's subtle
+ * implementation.
+ *
+ * <p>On Android and on Java since JDK 11, the Conscrypt implementation is available by default.
+ */
 @Immutable
 final class X25519HpkeKem implements HpkeKem {
   private final HkdfHpkeKdf hkdf;
 
+  private final X25519 x25519;
+
+  /** Implementation of the X25519 interface using Tink's own pure Java implementation. */
+  @Immutable
+  private static final class X25519Java implements X25519 {
+    @Override
+    public X25519.KeyPair generateKeyPair() throws GeneralSecurityException {
+      byte[] privateKey = com.google.crypto.tink.subtle.X25519.generatePrivateKey();
+      byte[] publicKey = com.google.crypto.tink.subtle.X25519.publicFromPrivate(privateKey);
+      return new X25519.KeyPair(privateKey, publicKey);
+    }
+
+    @Override
+    public byte[] computeSharedSecret(byte[] privateKey, byte[] publicKey)
+        throws GeneralSecurityException {
+      return com.google.crypto.tink.subtle.X25519.computeSharedSecret(privateKey, publicKey);
+    }
+  }
+
   /** Construct X25519-HKDF HPKE KEM using {@code hkdf}. */
   X25519HpkeKem(HkdfHpkeKdf hkdf) {
     this.hkdf = hkdf;
+    X25519 x25519 = null;
+    try {
+      x25519 = X25519Conscrypt.create();
+    } catch (GeneralSecurityException e) {
+      x25519 = new X25519Java();
+    }
+    this.x25519 = x25519;
   }
 
   private byte[] deriveKemSharedSecret(
@@ -66,7 +98,7 @@ final class X25519HpkeKem implements HpkeKem {
   HpkeKemEncapOutput encapsulateWithFixedEphemeralKey(
       byte[] recipientPublicKey, byte[] ephemeralPrivateKey, byte[] ephemeralPublicKey)
       throws GeneralSecurityException {
-    byte[] dhSharedSecret = X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey);
+    byte[] dhSharedSecret = x25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey);
     byte[] kemSharedSecret =
         deriveKemSharedSecret(dhSharedSecret, ephemeralPublicKey, recipientPublicKey);
     return new HpkeKemEncapOutput(kemSharedSecret, ephemeralPublicKey);
@@ -74,10 +106,9 @@ final class X25519HpkeKem implements HpkeKem {
 
   @Override
   public HpkeKemEncapOutput encapsulate(byte[] recipientPublicKey) throws GeneralSecurityException {
-    byte[] ephemeralPrivateKey = X25519.generatePrivateKey();
-    byte[] ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    X25519.KeyPair ephemeral = x25519.generateKeyPair();
     return encapsulateWithFixedEphemeralKey(
-        recipientPublicKey, ephemeralPrivateKey, ephemeralPublicKey);
+        recipientPublicKey, ephemeral.privateKey, ephemeral.publicKey);
   }
 
   /** Helper function factored out to facilitate unit testing. */
@@ -89,8 +120,8 @@ final class X25519HpkeKem implements HpkeKem {
       throws GeneralSecurityException {
     byte[] dhSharedSecret =
         Bytes.concat(
-            X25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey),
-            X25519.computeSharedSecret(
+            x25519.computeSharedSecret(ephemeralPrivateKey, recipientPublicKey),
+            x25519.computeSharedSecret(
                 senderPrivateKey.getSerializedPrivate().toByteArray(), recipientPublicKey));
     byte[] senderPublicKey = senderPrivateKey.getSerializedPublic().toByteArray();
     byte[] kemSharedSecret =
@@ -103,17 +134,16 @@ final class X25519HpkeKem implements HpkeKem {
   public HpkeKemEncapOutput authEncapsulate(
       byte[] recipientPublicKey, HpkeKemPrivateKey senderPrivateKey)
       throws GeneralSecurityException {
-    byte[] ephemeralPrivateKey = X25519.generatePrivateKey();
-    byte[] ephemeralPublicKey = X25519.publicFromPrivate(ephemeralPrivateKey);
+    X25519.KeyPair ephemeral = x25519.generateKeyPair();
     return authEncapsulateWithFixedEphemeralKey(
-        recipientPublicKey, ephemeralPrivateKey, ephemeralPublicKey, senderPrivateKey);
+        recipientPublicKey, ephemeral.privateKey, ephemeral.publicKey, senderPrivateKey);
   }
 
   @Override
   public byte[] decapsulate(byte[] encapsulatedKey, HpkeKemPrivateKey recipientPrivateKey)
       throws GeneralSecurityException {
     byte[] dhSharedSecret =
-        X25519.computeSharedSecret(
+        x25519.computeSharedSecret(
             recipientPrivateKey.getSerializedPrivate().toByteArray(), encapsulatedKey);
     return deriveKemSharedSecret(
         dhSharedSecret, encapsulatedKey, recipientPrivateKey.getSerializedPublic().toByteArray());
@@ -126,8 +156,8 @@ final class X25519HpkeKem implements HpkeKem {
     byte[] privateKey = recipientPrivateKey.getSerializedPrivate().toByteArray();
     byte[] dhSharedSecret =
         Bytes.concat(
-            X25519.computeSharedSecret(privateKey, encapsulatedKey),
-            X25519.computeSharedSecret(privateKey, senderPublicKey));
+            x25519.computeSharedSecret(privateKey, encapsulatedKey),
+            x25519.computeSharedSecret(privateKey, senderPublicKey));
     byte[] recipientPublicKey = recipientPrivateKey.getSerializedPublic().toByteArray();
     return deriveKemSharedSecret(
         dhSharedSecret, encapsulatedKey, recipientPublicKey, senderPublicKey);
