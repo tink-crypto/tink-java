95e1c60bff033ecf6626926127daf347fe091836
Liza Tretyakova <lizatretyakova@google.com>
2024-11-15 08:38:24 -0800

    DeterministicAeadConfigurationV0 for Tink Java.
    PiperOrigin-RevId: 696899020
    Change-Id: Ic106e5ed828c4d8dfaf91b1dc7217dbd17da9226

---
+++ src/main/java/com/google/crypto/tink/daead/DeterministicAeadConfigurationV0.java
@@ -0,0 +1,82 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.daead;
+
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.DeterministicAead;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.InternalConfiguration;
+import com.google.crypto.tink.internal.PrimitiveConstructor;
+import com.google.crypto.tink.internal.PrimitiveRegistry;
+import com.google.crypto.tink.subtle.AesSiv;
+import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+
+/**
+ * DeterministicAeadConfigurationV0 contains the following algorithms for DeterministicAEAD:
+ *
+ * <ul>
+ *   <li>AesSiv
+ * </ul>
+ */
+/* Placeholder for internally public; DO NOT CHANGE. */ class DeterministicAeadConfigurationV0 {
+  private DeterministicAeadConfigurationV0() {}
+
+  private static final InternalConfiguration INTERNAL_CONFIGURATION = create();
+
+  private static InternalConfiguration create() {
+    try {
+      PrimitiveRegistry.Builder builder = PrimitiveRegistry.builder();
+
+      // Register DeterministicAead wrapper and concrete primitives.
+      DeterministicAeadWrapper.registerToInternalPrimitiveRegistry(builder);
+      builder.registerPrimitiveConstructor(
+          PrimitiveConstructor.create(
+              DeterministicAeadConfigurationV0::createDeterministicAead,
+              AesSivKey.class,
+              DeterministicAead.class));
+
+      return InternalConfiguration.createFromPrimitiveRegistry(builder.build());
+    } catch (GeneralSecurityException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static Configuration get() throws GeneralSecurityException {
+    if (TinkFipsUtil.useOnlyFips()) {
+      throw new GeneralSecurityException(
+          "Cannot use non-FIPS-compliant DeterministicAeadConfigurationV0 in FIPS mode");
+    }
+    return INTERNAL_CONFIGURATION;
+  }
+
+  // We only allow 64-byte keys for AesSiv.
+  private static final int KEY_SIZE_IN_BYTES = 64;
+
+  private static DeterministicAead createDeterministicAead(AesSivKey key)
+      throws GeneralSecurityException {
+    if (key.getParameters().getKeySizeBytes() != KEY_SIZE_IN_BYTES) {
+      throw new InvalidAlgorithmParameterException(
+          "invalid key size: "
+              + key.getParameters().getKeySizeBytes()
+              + ". Valid keys must have "
+              + KEY_SIZE_IN_BYTES
+              + " bytes.");
+    }
+    return AesSiv.create(key);
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/daead/DeterministicAeadConfigurationV0Test.java
@@ -0,0 +1,89 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.daead;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.DeterministicAead;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.daead.AesSivParameters.Variant;
+import com.google.crypto.tink.daead.internal.AesSivProtoSerialization;
+import com.google.crypto.tink.util.SecretBytes;
+import java.security.GeneralSecurityException;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DeterministicAeadConfigurationV0Test {
+  @Test
+  public void config_throwsIfInFipsMode() throws Exception {
+    Assume.assumeTrue(TinkFipsUtil.useOnlyFips());
+
+    assertThrows(GeneralSecurityException.class, DeterministicAeadConfigurationV0::get);
+  }
+
+  @Test
+  public void config_containsAesSivForDeterministicAead() throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+
+    AesSivProtoSerialization.register();
+    AesSivParameters parameters =
+        AesSivParameters.builder().setKeySizeBytes(64).setVariant(Variant.NO_PREFIX).build();
+    AesSivKey key =
+        AesSivKey.builder()
+            .setParameters(parameters)
+            .setKeyBytes(SecretBytes.randomBytes(64))
+            .build();
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
+            .build();
+
+    assertThat(
+            keysetHandle.getPrimitive(
+                DeterministicAeadConfigurationV0.get(), DeterministicAead.class))
+        .isNotNull();
+  }
+
+  @Test
+  public void config_disallowsNon64ByteKeyForAesSiv() throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+
+    AesSivProtoSerialization.register();
+    AesSivParameters parameters =
+        AesSivParameters.builder().setKeySizeBytes(32).setVariant(Variant.NO_PREFIX).build();
+    AesSivKey key =
+        AesSivKey.builder()
+            .setParameters(parameters)
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
+            .build();
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            keysetHandle.getPrimitive(
+                DeterministicAeadConfigurationV0.get(), DeterministicAead.class));
+  }
+}
