e906eb47a2f1225373e691e817e3750021444cd8
Daniel Woffinden <daw@google.com>
2024-12-10 07:01:31 -0800

    Allow `@AccessesPartialKey` to apply to fields and local variables as well
    This allows you to apply it at the narrowest possible scope, rather than on the enclosing method/class, e.g.
    
    ```
    @AccessesPartialKey
    var key =
        AesGcmKey.builder()
            .setParameters(...)
            .setKeyBytes(...)
            .build();
    ```
    
    PiperOrigin-RevId: 704693926
    Change-Id: I4b5520f3ba71b4d8560cebb7c1cd647caf72a755

--- src/main/java/com/google/crypto/tink/AccessesPartialKey.java
+++ src/main/java/com/google/crypto/tink/AccessesPartialKey.java
@@ -15,7 +15,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 package com.google.crypto.tink;
 
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -61,6 +65,6 @@ import java.lang.annotation.Target;
  *   }
  * </pre>
  */
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({TYPE, METHOD, FIELD, LOCAL_VARIABLE})
 @Retention(RetentionPolicy.CLASS)
 public @interface AccessesPartialKey {}
