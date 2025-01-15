d226faed953637f803569e0e3e4e859d69a83eca
Thomas Holenstein <tholenst@google.com>
2025-01-15 01:24:36 -0800

    Add some test vectors for AES GCM HKDF.
    PiperOrigin-RevId: 715696531
    Change-Id: I2ee11fe6f7127a5b20c221fb5b3ccef88e4687b7

--- src/main/java/com/google/crypto/tink/streamingaead/internal/testing/AesGcmHkdfStreamingTestUtil.java
+++ src/main/java/com/google/crypto/tink/streamingaead/internal/testing/AesGcmHkdfStreamingTestUtil.java
@@ -66,11 +66,179 @@ public final class AesGcmHkdfStreamingTestUtil {
     return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
   }
 
+  // Empty plaintext, empty aad.
+  private static StreamingAeadTestVector createTestVector1() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(16)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA1)
+            .setCiphertextSegmentSizeBytes(64)
+            .setDerivedAesGcmKeySizeBytes(16)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("6eb56cdc726dfbe5d57f2fcdc6e9345b"), InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "18ec99b2c3884194023bd41bb9bff309205354c750fa0cb3c0d02609abf71f88eaeaa48d7deca27f");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // SHA256
+  private static StreamingAeadTestVector createTestVector2() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(16)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA256)
+            .setCiphertextSegmentSizeBytes(64)
+            .setDerivedAesGcmKeySizeBytes(16)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("6eb56cdc726dfbe5d57f2fcdc6e9345b"), InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "18be78bce0cab6fd38a9041eea8ab0ffc4cf17e3c00b661d3927f5f79069f41a210fedc40b25648c");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // SHA512
+  private static StreamingAeadTestVector createTestVector3() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(16)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA512)
+            .setCiphertextSegmentSizeBytes(64)
+            .setDerivedAesGcmKeySizeBytes(16)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("6eb56cdc726dfbe5d57f2fcdc6e9345b"), InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "18fec8139ccb406e0f140d7e2bcc41e1b899bc1b121574cc31d1c78ed2ca1cdf324665b8dce21095");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // 32 byte key
+  private static StreamingAeadTestVector createTestVector4() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(32)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA1)
+            .setCiphertextSegmentSizeBytes(64)
+            .setDerivedAesGcmKeySizeBytes(16)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("00112233445566778899aabbccddeeff6eb56cdc726dfbe5d57f2fcdc6e9345b"),
+                InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "180880e85a24207a7b72cefde26306ed09d7d76e58104b751d005a3ffd72dd6151f0d9c8ccf163f7");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // DerivedAesGcmKeySize = 32
+  private static StreamingAeadTestVector createTestVector5() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(32)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA1)
+            .setCiphertextSegmentSizeBytes(64)
+            .setDerivedAesGcmKeySizeBytes(32)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("00112233445566778899aabbccddeeff6eb56cdc726dfbe5d57f2fcdc6e9345b"),
+                InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "28fae780c70a82d1256c8303a1261ac6a98cf557b4e173967a62f3c149517ba5b2431da93b3c6e90f508b34dc14bfda59bab05681c6ad3fa");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // CiphertextSegmentSize = 128 (note: we can use the same ciphertext as for the above)
+  private static StreamingAeadTestVector createTestVector6() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(32)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA1)
+            .setCiphertextSegmentSizeBytes(128)
+            .setDerivedAesGcmKeySizeBytes(32)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("00112233445566778899aabbccddeeff6eb56cdc726dfbe5d57f2fcdc6e9345b"),
+                InsecureSecretKeyAccess.get()));
+    byte[] plaintext = new byte[0];
+    byte[] aad = new byte[0];
+    byte[] ciphertext =
+        Hex.decode(
+            "28fae780c70a82d1256c8303a1261ac6a98cf557b4e173967a62f3c149517ba5b2431da93b3c6e90f508b34dc14bfda59bab05681c6ad3fa");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
+  // CiphertextSegmentSize = 120, long plaintext (length 65).
+  // Note: len(header) == 40, hence len(M0), first message block length, is 120 - 40 - 16 = 64
+  // (see https://developers.google.com/tink/streaming-aead/aes_gcm_hkdf_streaming)
+  private static StreamingAeadTestVector createTestVector7() throws GeneralSecurityException {
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(32)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA1)
+            .setCiphertextSegmentSizeBytes(120)
+            .setDerivedAesGcmKeySizeBytes(32)
+            .build();
+    AesGcmHkdfStreamingKey key =
+        AesGcmHkdfStreamingKey.create(
+            parameters,
+            SecretBytes.copyFrom(
+                Hex.decode("00112233445566778899aabbccddeeff6eb56cdc726dfbe5d57f2fcdc6e9345b"),
+                InsecureSecretKeyAccess.get()));
+    byte[] plaintext =
+        "BLOCK 1.BLOCK 2.BLOCK 3.BLOCK 4.BLOCK 5.BLOCK 6.BLOCK 7.BLOCK 8.".getBytes(UTF_8);
+    byte[] aad = new byte[] {0, 1, 2, 3};
+    byte[] ciphertext =
+        Hex.decode(
+            "28782994617714a80fa085e15051a16854522330d6d0f26c049b2192f09cdd98b2eb90c753bfff277a29e54fa4afb15d648c28477eb07f012535c767f5fdce24ffffae318480c1d37357d6d3c511159318afa09ef38aa3f5456fd7817c5c02dd6e1a7fef174c8bd24e38b5982ac105497c0101ac5581fe5b");
+    return new StreamingAeadTestVector(key, plaintext, aad, ciphertext);
+  }
+
   public static StreamingAeadTestVector[] createAesGcmHkdfTestVectors() {
     return exceptionIsBug(
         () ->
             new StreamingAeadTestVector[] {
               createTestVector0(),
+              createTestVector1(),
+              createTestVector2(),
+              createTestVector3(),
+              createTestVector4(),
+              createTestVector5(),
+              createTestVector6(),
+              createTestVector7(),
             });
   }
 
