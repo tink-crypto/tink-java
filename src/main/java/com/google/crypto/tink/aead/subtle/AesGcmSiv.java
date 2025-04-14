f2b378185f3fcf7726cd91550d4533df9fd14ce1
Juerg Wullschleger <juerg@google.com>
2025-04-14 08:59:04 -0700

    Avoid copying the whole ciphertext to add or remove the output prefix in AesGcmSiv.
    - Also rename some variables that caused lint warnings.
    - Fix some comments.
    
    PiperOrigin-RevId: 747439821
    Change-Id: I37cbb03c129b6f16316f0883ae353d3259471435

--- src/main/java/com/google/crypto/tink/aead/subtle/AesGcmSiv.java
+++ src/main/java/com/google/crypto/tink/aead/subtle/AesGcmSiv.java
@@ -31,6 +31,7 @@ import com.google.crypto.tink.subtle.Validators;
 import java.security.GeneralSecurityException;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
+import javax.annotation.Nullable;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
@@ -50,22 +51,22 @@ import javax.crypto.spec.SecretKeySpec;
 public final class AesGcmSiv implements Aead {
 
   // Test vector from https://www.rfc-editor.org/rfc/rfc8452.html#appendix-C.1
-  private static final byte[] TEST_PLAINTEXT = Hex.decode("7a806c");
-  private static final byte[] TEST_AAD = Hex.decode("46bb91c3c5");
-  private static final byte[] TEST_KEY = Hex.decode("36864200e0eaf5284d884a0e77d31646");
-  private static final byte[] TEST_NOUNCE = Hex.decode("bae8e37fc83441b16034566b");
-  private static final byte[] TEST_RESULT = Hex.decode("af60eb711bd85bc1e4d3e0a462e074eea428a8");
+  private static final byte[] testPlaintext = Hex.decode("7a806c");
+  private static final byte[] testAad = Hex.decode("46bb91c3c5");
+  private static final byte[] testKey = Hex.decode("36864200e0eaf5284d884a0e77d31646");
+  private static final byte[] testNounce = Hex.decode("bae8e37fc83441b16034566b");
+  private static final byte[] testResult = Hex.decode("af60eb711bd85bc1e4d3e0a462e074eea428a8");
 
   // On Android API version 29 and older, the security provider returns an AES GCM cipher instead
   // an AES GCM SIV cipher. This function tests if we have a correct cipher.
   private static boolean isAesGcmSivCipher(Cipher cipher) {
     try {
       // Use test vector to validate that cipher implements AES GCM SIV.
-      AlgorithmParameterSpec params = getParams(TEST_NOUNCE);
-      cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(TEST_KEY, "AES"), params);
-      cipher.updateAAD(TEST_AAD);
-      byte[] output = cipher.doFinal(TEST_RESULT, 0, TEST_RESULT.length);
-      return Bytes.equal(output, TEST_PLAINTEXT);
+      AlgorithmParameterSpec params = getParams(testNounce);
+      cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(testKey, "AES"), params);
+      cipher.updateAAD(testAad);
+      byte[] output = cipher.doFinal(testResult, 0, testResult.length);
+      return Bytes.equal(output, testPlaintext);
     } catch (GeneralSecurityException ex) {
       return false;
     }
@@ -75,6 +76,7 @@ public final class AesGcmSiv implements Aead {
   // AES GCM SIV cipher.
   private static final ThreadLocal<Cipher> localAesGcmSivCipher =
       new ThreadLocal<Cipher>() {
+        @Nullable
         @Override
         protected Cipher initialValue() {
           try {
@@ -121,32 +123,45 @@ public final class AesGcmSiv implements Aead {
     return cipher;
   }
 
-  private byte[] rawEncrypt(final byte[] plaintext, final byte[] associatedData)
+  /**
+   * On Android KitKat (API level 19) this method does not support non null or non empty {@code
+   * associatedData}. It might not work at all in older versions.
+   */
+  @Override
+  public byte[] encrypt(final byte[] plaintext, final byte[] associatedData)
       throws GeneralSecurityException {
     Cipher cipher = getAesGcmSivCipher();
     // Check that ciphertext is not longer than the max. size of a Java array.
-    if (plaintext.length > Integer.MAX_VALUE - IV_SIZE_IN_BYTES - TAG_SIZE_IN_BYTES) {
+    if (plaintext.length
+        > Integer.MAX_VALUE - IV_SIZE_IN_BYTES - TAG_SIZE_IN_BYTES - outputPrefix.length) {
       throw new GeneralSecurityException("plaintext too long");
     }
-    byte[] ciphertext = new byte[IV_SIZE_IN_BYTES + plaintext.length + TAG_SIZE_IN_BYTES];
+
+    int ciphertextLen =
+        outputPrefix.length + IV_SIZE_IN_BYTES + plaintext.length + TAG_SIZE_IN_BYTES;
+    byte[] ciphertext = Arrays.copyOf(outputPrefix, ciphertextLen);
     byte[] iv = Random.randBytes(IV_SIZE_IN_BYTES);
-    System.arraycopy(iv, 0, ciphertext, 0, IV_SIZE_IN_BYTES);
+    System.arraycopy(
+        /* src= */ iv,
+        /* srcPos= */ 0,
+        /* dest= */ ciphertext,
+        /* destPos= */ outputPrefix.length,
+        /* length= */ IV_SIZE_IN_BYTES);
 
     AlgorithmParameterSpec params = getParams(iv);
     cipher.init(Cipher.ENCRYPT_MODE, keySpec, params);
     if (associatedData != null && associatedData.length != 0) {
       cipher.updateAAD(associatedData);
     }
-    int written = cipher.doFinal(plaintext, 0, plaintext.length, ciphertext, IV_SIZE_IN_BYTES);
-    // For security reasons, AES-GCM encryption must always use tag of TAG_SIZE_IN_BYTES bytes. If
-    // so, written must be equal to plaintext.length + TAG_SIZE_IN_BYTES.
-
+    int written =
+        cipher.doFinal(
+            plaintext, 0, plaintext.length, ciphertext, outputPrefix.length + IV_SIZE_IN_BYTES);
+    // AES-GCM-SIV always adds a tag of length TAG_SIZE_IN_BYTES.
     if (written != plaintext.length + TAG_SIZE_IN_BYTES) {
-      // The tag is shorter than expected.
       int actualTagSize = written - plaintext.length;
       throw new GeneralSecurityException(
           String.format(
-              "encryption failed; GCM tag must be %s bytes, but got only %s bytes",
+              "encryption failed; AES-GCM-SIV tag must be %s bytes, but got only %s bytes",
               TAG_SIZE_IN_BYTES, actualTagSize));
     }
     return ciphertext;
@@ -157,55 +172,30 @@ public final class AesGcmSiv implements Aead {
    * associatedData}. It might not work at all in older versions.
    */
   @Override
-  public byte[] encrypt(final byte[] plaintext, final byte[] associatedData)
-      throws GeneralSecurityException {
-    // Check that ciphertext is not longer than the max. size of a Java array.
-    byte[] ciphertext = rawEncrypt(plaintext, associatedData);
-    if (outputPrefix.length == 0) {
-      return ciphertext;
-    }
-    return Bytes.concat(outputPrefix, ciphertext);
-  }
-
-  private byte[] rawDecrypt(final byte[] ciphertext, final byte[] associatedData)
-      throws GeneralSecurityException {
-    Cipher cipher = getAesGcmSivCipher();
-    if (ciphertext.length < IV_SIZE_IN_BYTES + TAG_SIZE_IN_BYTES) {
-      throw new GeneralSecurityException("ciphertext too short");
-    }
-
-    AlgorithmParameterSpec params = getParams(ciphertext, 0, IV_SIZE_IN_BYTES);
-    cipher.init(Cipher.DECRYPT_MODE, keySpec, params);
-    if (associatedData != null && associatedData.length != 0) {
-      cipher.updateAAD(associatedData);
-    }
-    return cipher.doFinal(ciphertext, IV_SIZE_IN_BYTES, ciphertext.length - IV_SIZE_IN_BYTES);
-  }
-
-  /**
-   * On Android KitKat (API level 19) this method does not support non null or non empty {@code
-   * associatedData}. It might not work at all in older versions.
-   */
-  @Override
   public byte[] decrypt(final byte[] ciphertext, final byte[] associatedData)
       throws GeneralSecurityException {
-    if (outputPrefix.length == 0) {
-      return rawDecrypt(ciphertext, associatedData);
+    if (ciphertext.length < outputPrefix.length + IV_SIZE_IN_BYTES + TAG_SIZE_IN_BYTES) {
+      throw new GeneralSecurityException("ciphertext too short");
     }
     if (!isPrefix(outputPrefix, ciphertext)) {
       throw new GeneralSecurityException("Decryption failed (OutputPrefix mismatch).");
     }
-    byte[] copiedCiphertext =
-        Arrays.copyOfRange(ciphertext, outputPrefix.length, ciphertext.length);
-    return rawDecrypt(copiedCiphertext, associatedData);
+    Cipher cipher = getAesGcmSivCipher();
+    AlgorithmParameterSpec params = getParams(ciphertext, outputPrefix.length, IV_SIZE_IN_BYTES);
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, params);
+    if (associatedData != null && associatedData.length != 0) {
+      cipher.updateAAD(associatedData);
+    }
+    int offset = outputPrefix.length + IV_SIZE_IN_BYTES;
+    int len = ciphertext.length - outputPrefix.length - IV_SIZE_IN_BYTES;
+    return cipher.doFinal(ciphertext, offset, len);
   }
 
-  private static AlgorithmParameterSpec getParams(final byte[] iv) throws GeneralSecurityException {
+  private static AlgorithmParameterSpec getParams(final byte[] iv) {
     return getParams(iv, 0, iv.length);
   }
 
-  private static AlgorithmParameterSpec getParams(final byte[] buf, int offset, int len)
-      throws GeneralSecurityException {
+  private static AlgorithmParameterSpec getParams(final byte[] buf, int offset, int len) {
     return new GCMParameterSpec(8 * TAG_SIZE_IN_BYTES, buf, offset, len);
   }
 }
