230f661e8722ed8417e88b04202f894e4c805e4a
Juerg Wullschleger <juerg@google.com>
2025-06-17 22:08:29 -0700

    Simplify AES-EAX.
    The OMAC used in AES-EAX is the same as AES-CMAC, where
    OMAC(tag, data) = AES-CMAC(tag || data).
    
    So we can simplify the AES-EAX implementation by re-using the CMAC implementation.
    
    Unfortunately, this simplification results in an additional copy of the input. Avoiding this would require us to make the implementation and the interface of PRF-AES-CMAC more complicated, which we think is not worth the effort, since the impact on the overall performance of AES-EAX is small (it get about %5 slower), and AES-EAX is usually not used for high performance applications.
    
    Also, remove the old and outdated section about our plans for AES-EAX.
    
    PiperOrigin-RevId: 772761236
    Change-Id: If38419310c5448fe1cd0a183ef2a9df487038588

--- src/main/java/com/google/crypto/tink/subtle/AesEaxJce.java
+++ src/main/java/com/google/crypto/tink/subtle/AesEaxJce.java
@@ -23,13 +23,11 @@ import com.google.crypto.tink.Aead;
 import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.aead.AesEaxKey;
 import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.prf.Prf;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import javax.crypto.AEADBadTagException;
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -47,28 +45,10 @@ import javax.crypto.spec.SecretKeySpec;
  *   <li>The tag size is always 16 bytes
  *   <li>Nonces are chosen by the implementation at random. Their size is 12 or 16 bytes.
  * </ul>
- *
- * <p>Plans: The current implementation is slow since it uses JCA and only assumes that the
- * encryption modes "AES/ECB/NOPADDING" and "AES/CTR/NOPADDING" are implemented. Our plan is to
- * implement a native version of EAX.
- *
- * @since 1.0.0
  */
 public final class AesEaxJce implements Aead {
   public static final TinkFipsUtil.AlgorithmFipsCompatibility FIPS =
       TinkFipsUtil.AlgorithmFipsCompatibility.ALGORITHM_NOT_FIPS;
-
-  private static final ThreadLocal<Cipher> localEcbCipher =
-      new ThreadLocal<Cipher>() {
-        @Override
-        protected Cipher initialValue() {
-          try {
-            return EngineFactory.CIPHER.getInstance("AES/ECB/NOPADDING");
-          } catch (GeneralSecurityException ex) {
-            throw new IllegalStateException(ex);
-          }
-        }
-      };
 
   private static final ThreadLocal<Cipher> localCtrCipher =
       new ThreadLocal<Cipher>() {
@@ -85,12 +65,9 @@ public final class AesEaxJce implements Aead {
   static final int BLOCK_SIZE_IN_BYTES = 16;
   static final int TAG_SIZE_IN_BYTES = 16;
 
-  // The constants B and P derived from the key. These constants are used for computing an OMAC.
-  private final byte[] b;
-  private final byte[] p;
-
   private final byte[] outputPrefix;
 
+  private final Prf cmac;
   private final SecretKeySpec keySpec;
   private final int ivSizeInBytes;
 
@@ -110,6 +87,11 @@ public final class AesEaxJce implements Aead {
         key.getOutputPrefix().toByteArray());
   }
 
+  @AccessesPartialKey
+  private static Prf createCmac(byte[] key) throws GeneralSecurityException {
+    return new PrfAesCmac(key);
+  }
+
   private AesEaxJce(final byte[] key, int ivSizeInBytes, byte[] outputPrefix)
       throws GeneralSecurityException {
     if (!FIPS.isCompatible()) {
@@ -122,11 +104,7 @@ public final class AesEaxJce implements Aead {
     this.ivSizeInBytes = ivSizeInBytes;
     Validators.validateAesKeySize(key.length);
     keySpec = new SecretKeySpec(key, "AES");
-    Cipher ecb = localEcbCipher.get();
-    ecb.init(Cipher.ENCRYPT_MODE, keySpec);
-    byte[] block = ecb.doFinal(new byte[BLOCK_SIZE_IN_BYTES]);
-    b = multiplyByX(block);
-    p = multiplyByX(b);
+    cmac = createCmac(key);
     this.outputPrefix = outputPrefix;
   }
 
@@ -135,102 +113,28 @@ public final class AesEaxJce implements Aead {
   }
 
   /**
-   * Computes the xor of two byte arrays of equal size.
-   *
-   * <p>The output is stored in the first array.
-   */
-  private static void xor(final byte[] x, final byte[] y) {
-    int len = x.length;
-    for (int i = 0; i < len; i++) {
-      x[i] = (byte) (x[i] ^ y[i]);
-    }
-  }
-
-  /**
-   * Multiplies an element of the field GF(2)[x]/(x^128+x^7+x^2+x+1) by x.
-   *
-   * @param block a 16 byte block representing an element of the field using big endian order.
-   */
-  private static byte[] multiplyByX(final byte[] block) {
-    byte[] res = new byte[BLOCK_SIZE_IN_BYTES];
-    for (int i = 0; i < BLOCK_SIZE_IN_BYTES - 1; i++) {
-      // Shifts byte array by 1 bit (this is ugly because bytes in Java are signed)
-      res[i] = (byte) (((block[i] << 1) ^ ((block[i + 1] & 0xff) >>> 7)) & 0xff);
-    }
-    // Shifts the least significant block by 1 bit and reduces the msb modulo the polynomial.
-    res[BLOCK_SIZE_IN_BYTES - 1] =
-        (byte) ((block[BLOCK_SIZE_IN_BYTES - 1] << 1) ^ ((block[0] >> 7) & 0x87));
-    return res;
-  }
-
-  /**
-   * Pads the last block for OMAC. If the last block is smaller than 16 bytes then a bitstring
-   * starting with 1 and followed by 0's is appended and the result is XORed with p. If the last
-   * block is 16 bytes long then the last block is XORed with b.
-   */
-  private byte[] pad(final byte[] data, int lastBlockFrom, int lastBlockTo) {
-    byte[] lastBlock = Arrays.copyOfRange(data, lastBlockFrom, lastBlockTo);
-    if (lastBlock.length == BLOCK_SIZE_IN_BYTES) {
-      xor(lastBlock, b);
-      return lastBlock;
-    } else {
-      byte[] res = Arrays.copyOf(p, BLOCK_SIZE_IN_BYTES);
-      for (int i = 0; i < lastBlock.length; i++) {
-        res[i] ^= lastBlock[i];
-      }
-      res[lastBlock.length] = (byte) (res[lastBlock.length] ^ 0x80);
-      return res;
-    }
-  }
-
-  /**
    * Computes an OMAC.
    *
-   * @param ecb A cipher initialized with the key of this class using AES/ECB/NOPadding and
-   *     encryption mode.
+   * <p>OMAC (or OMAC1) is the same as to CMAC, where the tag is prepended to the data.
+   *
    * @param tag The OMAC tag (0 for nonce, 1 for aad, 2 for ciphertext)
    * @param data The array containing the data to MAC.
    * @param offset The start of the data to MAC.
    * @param length The length of the data to MAC.
    * @return The 16 byte long OMAC
-   * @throws IllegalBlockSizeException, BadPaddingException This should not happen.
+   * @throws GeneralSecurityException This should not happen.
    */
-  private byte[] omac(Cipher ecb, int tag, final byte[] data, int offset, int length)
-      throws IllegalBlockSizeException, BadPaddingException, ShortBufferException {
-    assert length >= 0;
-    assert 0 <= tag && tag <= 3;
-    byte[] block = new byte[BLOCK_SIZE_IN_BYTES];
-    block[BLOCK_SIZE_IN_BYTES - 1] = (byte) tag;
-    if (length == 0) {
-      xor(block, b);
-      return ecb.doFinal(block);
-    }
-    byte[] buffer = new byte[BLOCK_SIZE_IN_BYTES];
-
-    // Note that
-    // {@code ecb.doFinal(block, 0, BLOCK_SIZE_IN_BYTES, /* output= */ block);} also works,
-    // but the performance of doing this is bad. It seems that it detects that input and output
-    // are the same, and then allocate some temporary memory, and copies the result back.
-    ecb.doFinal(block, 0, BLOCK_SIZE_IN_BYTES, /* output= */ buffer);
-    byte[] temp = block; // re-use unused block as buffer.
-    block = buffer;
-    buffer = temp;
-
-    int position = 0;
-    while (length - position > BLOCK_SIZE_IN_BYTES) {
-      for (int i = 0; i < BLOCK_SIZE_IN_BYTES; i++) {
-        block[i] ^= data[offset + position + i];
-      }
-      ecb.doFinal(block, 0, BLOCK_SIZE_IN_BYTES, /* output= */ buffer);
-      temp = block; // block is not needed anymore, we re-use it as buffer.
-      block = buffer;
-      buffer = temp;
-      position += BLOCK_SIZE_IN_BYTES;
-    }
-    byte[] padded = pad(data, offset + position, offset + length);
-    xor(block, padded);
-    ecb.doFinal(block, 0, BLOCK_SIZE_IN_BYTES, /* output= */ buffer);
-    return buffer;
+  private byte[] omac(int tag, final byte[] data, int offset, int length)
+      throws GeneralSecurityException {
+    byte[] input = new byte[length + BLOCK_SIZE_IN_BYTES];
+    input[BLOCK_SIZE_IN_BYTES - 1] = (byte) tag;
+    System.arraycopy(
+        /* src= */ data,
+        /* srcPos= */ offset,
+        /* dest= */ input,
+        /* destPos= */ BLOCK_SIZE_IN_BYTES,
+        /* length= */ length);
+    return cmac.compute(input, BLOCK_SIZE_IN_BYTES);
   }
 
   @SuppressWarnings("InsecureCryptoUsage")
@@ -253,18 +157,16 @@ public final class AesEaxJce implements Aead {
         /* dest= */ ciphertext,
         /* destPos= */ outputPrefix.length,
         /* length= */ ivSizeInBytes);
-    Cipher ecb = localEcbCipher.get();
-    ecb.init(Cipher.ENCRYPT_MODE, keySpec);
-    byte[] n = omac(ecb, 0, iv, 0, iv.length);
+    byte[] n = omac(0, iv, 0, iv.length);
     byte[] aad = associatedData;
     if (aad == null) {
       aad = new byte[0];
     }
-    byte[] h = omac(ecb, 1, aad, 0, aad.length);
+    byte[] h = omac(1, aad, 0, aad.length);
     Cipher ctr = localCtrCipher.get();
     ctr.init(Cipher.ENCRYPT_MODE, keySpec, new IvParameterSpec(n));
     ctr.doFinal(plaintext, 0, plaintext.length, ciphertext, outputPrefix.length + ivSizeInBytes);
-    byte[] t = omac(ecb, 2, ciphertext, outputPrefix.length + ivSizeInBytes, plaintext.length);
+    byte[] t = omac(2, ciphertext, outputPrefix.length + ivSizeInBytes, plaintext.length);
     int offset = outputPrefix.length + plaintext.length + ivSizeInBytes;
     for (int i = 0; i < TAG_SIZE_IN_BYTES; i++) {
       ciphertext[offset + i] = (byte) (h[i] ^ n[i] ^ t[i]);
@@ -284,15 +186,13 @@ public final class AesEaxJce implements Aead {
     if (!isPrefix(outputPrefix, ciphertext)) {
       throw new GeneralSecurityException("Decryption failed (OutputPrefix mismatch).");
     }
-    Cipher ecb = localEcbCipher.get();
-    ecb.init(Cipher.ENCRYPT_MODE, keySpec);
-    byte[] n = omac(ecb, 0, ciphertext, outputPrefix.length, ivSizeInBytes);
+    byte[] n = omac(0, ciphertext, outputPrefix.length, ivSizeInBytes);
     byte[] aad = associatedData;
     if (aad == null) {
       aad = new byte[0];
     }
-    byte[] h = omac(ecb, 1, aad, 0, aad.length);
-    byte[] t = omac(ecb, 2, ciphertext, outputPrefix.length + ivSizeInBytes, plaintextLength);
+    byte[] h = omac(1, aad, 0, aad.length);
+    byte[] t = omac(2, ciphertext, outputPrefix.length + ivSizeInBytes, plaintextLength);
     byte res = 0;
     int offset = ciphertext.length - TAG_SIZE_IN_BYTES;
     for (int i = 0; i < TAG_SIZE_IN_BYTES; i++) {
