13dec54c1fcc165ea3628faf5a4b79b4bb6194e0
Thomas Holenstein <tholenst@google.com>
2025-09-24 03:29:04 -0700

    Fix the path to publish_javadoc_to_github_pages.
    PiperOrigin-RevId: 810807877
    Change-Id: Iba07db475b71dd7c20a5a9503ae49327e1d8c84b

--- maven/maven_deploy_library.sh
+++ maven/maven_deploy_library.sh
@@ -263,7 +263,7 @@ main() {
     mkdir -p  "${KOKORO_ARTIFACTS_DIR}/kokoro_upload_dir/release"
     cp "maven_bundle_${LIBRARY_NAME}.zip" "${KOKORO_ARTIFACTS_DIR}/kokoro_upload_dir/release"
   fi
-  ./publish_javadoc_to_github_pages \
+  ./maven/publish_javadoc_to_github_pages \
     "${GIT_URL}" \
     "${LIBRARY_NAME}"\
     "${ARTIFACT_VERSION}" \
