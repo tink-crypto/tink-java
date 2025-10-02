4c20578275c293a59db652d61524da77ecb2255d
Thomas Holenstein <tholenst@google.com>
2025-10-02 03:37:21 -0700

    Change tink/.../maven/run_tests.sh to use the build_maven_bundle script.
    PiperOrigin-RevId: 814160278
    Change-Id: I13b0861fea48ce7e7a15ab4653ddb40c2516721e

--- kokoro/gcp_ubuntu/maven/run_tests.sh
+++ kokoro/gcp_ubuntu/maven/run_tests.sh
@@ -32,116 +32,44 @@ echo "==========================================================================
 
 set -eEuox pipefail
 
-IS_KOKORO="false"
-if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]] ; then
-  IS_KOKORO="true"
-fi
-readonly IS_KOKORO
-
-RUN_COMMAND_ARGS=()
-if [[ "${IS_KOKORO}" == "true" ]] ; then
+if [[ ! -v TINK_BASE_DIR ]] ; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+fi
+readonly TINK_BASE_DIR
+echo "TINK_BASE_DIR is ${TINK_BASE_DIR}"
+if [[ ! -v CONTAINER_IMAGE ]] ; then
   source \
     "${TINK_BASE_DIR}/tink_java/kokoro/testutils/java_test_container_images.sh"
   CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
-  RUN_COMMAND_ARGS+=( -k "${TINK_GCR_SERVICE_KEY}" )
 fi
-
-: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
-readonly TINK_BASE_DIR
 readonly CONTAINER_IMAGE
-# If running from the tink_java folder this has no effect.
+echo "CONTAINER_IMAGE is ${CONTAINER_IMAGE}"
+
 cd "${TINK_BASE_DIR}/tink_java"
 
-if [[ -n "${CONTAINER_IMAGE:-}" ]]; then
-  RUN_COMMAND_ARGS+=( -c "${CONTAINER_IMAGE}" )
-fi
-
-# File that stores environment variables to pass to the container.
-readonly ENV_VARIABLES_FILE="/tmp/env_variables.txt"
-
-if [[ -n "${TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET:-}" ]]; then
+if [[ -v TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET ]]; then
   cp "${TINK_REMOTE_BAZEL_CACHE_SERVICE_KEY}" ./cache_key
-  cat <<EOF > "${ENV_VARIABLES_FILE}"
+  cat <<EOF > /tmp/env_variables.txt
 BAZEL_REMOTE_CACHE_NAME=${TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET}/bazel/${TINK_JAVA_BASE_IMAGE_HASH}
 EOF
-  RUN_COMMAND_ARGS+=( -e "${ENV_VARIABLES_FILE}" )
+else
+  touch /tmp/env_variables.txt
 fi
 
-cat <<'EOF' > _do_run_test.sh
-set -euo pipefail
-
-export ANDROID_HOME=/android-sdk-30
-
-# Compare the dependencies of the ":tink" target with the declared dependencies.
-# These should match the dependencies declared in tink-java.pom.xml, since
-# since these are the dependencies which are declared on maven.
-./kokoro/testutils/check_maven_bazel_deps_consistency.sh "//:tink" \
-  "maven/tink-java.pom.xml"
-
-# For Android, we compare the unshaded version -- trial and error revealed that
-# this is the target that has the correct dependencies.
-./kokoro/testutils/check_maven_bazel_deps_consistency.sh \
-  -e com.google.protobuf:protobuf-javalite \
-  "//:tink-android-unshaded" "maven/tink-java-android.pom.xml"
-
-MAVEN_DEPLOY_LIBRARY_OPTS=()
-if [[ -n "${BAZEL_REMOTE_CACHE_NAME:-}" ]]; then
-  MAVEN_DEPLOY_LIBRARY_OPTS+=( -c "${BAZEL_REMOTE_CACHE_NAME}" )
+if [[ ! -z "${TINK_GCR_SERVICE_KEY:-}" ]]; then
+  gcloud auth activate-service-account --key-file="${TINK_GCR_SERVICE_KEY}"
+  gcloud config set project tink-test-infrastructure
+  gcloud auth configure-docker us-docker.pkg.dev --quiet
 fi
-readonly MAVEN_DEPLOY_LIBRARY_OPTS
 
-# Install the latest snapshot for tink-java and tink-android locally.
-./maven/maven_deploy_library.sh "${MAVEN_DEPLOY_LIBRARY_OPTS[@]}" install \
-  tink maven/tink-java.pom.xml HEAD
-./maven/maven_deploy_library.sh "${MAVEN_DEPLOY_LIBRARY_OPTS[@]}" install \
-  tink-android maven/tink-java-android.pom.xml HEAD
+echo "-- PULLING DOCKER IMAGE"
+time docker pull "${CONTAINER_IMAGE}"
 
-# TODO(tholenst): find a good way to test these jar files.
-# TODO: b/433476142 - Re-enable this test
-# ./examples/android/helloworld/gradlew -PmavenLocation=local \
-#   -p ./examples/android/helloworld build
-EOF
-chmod +x _do_run_test.sh
-
-# Run cleanup on EXIT.
-trap cleanup EXIT
-
-cleanup() {
-  rm -rf _do_run_test.sh "${ENV_VARIABLES_FILE}"
-}
-
-./kokoro/testutils/docker_execute.sh "${RUN_COMMAND_ARGS[@]}" ./_do_run_test.sh
-
-readonly GITHUB_JOB_NAME="tink/github/java/gcp_ubuntu/maven/continuous"
-
-if [[ "${IS_KOKORO}" == "true" \
-      && "${KOKORO_JOB_NAME}" == "${GITHUB_JOB_NAME}" ]]; then
-  # Disable logging of commands when accessing key material.
-  set +x
-  echo "Currently handling key material. Command logging temporarily disabled"
-  # GITHUB_ACCESS_TOKEN is populated by Kokoro.
-  readonly GIT_CREDENTIALS="ise-crypto:${GITHUB_ACCESS_TOKEN}"
-  readonly GITHUB_URL="https://${GIT_CREDENTIALS}@github.com/tink-crypto/tink-java.git"
-
-  # Share the required env variables with the container to allow publishing the
-  # snapshot on Sonatype.
-  cat <<EOF >> "${ENV_VARIABLES_FILE}"
-SONATYPE_USERNAME
-SONATYPE_PASSWORD
-EOF
-
-  set -x
-  MAVEN_DEPLOY_LIBRARY_OPTS=( -u "${GITHUB_URL}" )
-  if [[ -n "${BAZEL_REMOTE_CACHE_NAME:-}" ]]; then
-    MAVEN_DEPLOY_LIBRARY_OPTS+=( -c "${BAZEL_REMOTE_CACHE_NAME}" )
-  fi
-  readonly MAVEN_DEPLOY_LIBRARY_OPTS
-
-  ./kokoro/testutils/docker_execute.sh "${RUN_COMMAND_ARGS[@]}" \
-    ./maven/maven_deploy_library.sh "${MAVEN_DEPLOY_LIBRARY_OPTS[@]}" snapshot \
-    tink maven/tink-java.pom.xml HEAD
-  ./kokoro/testutils/docker_execute.sh "${RUN_COMMAND_ARGS[@]}" \
-    ./maven/maven_deploy_library.sh "${MAVEN_DEPLOY_LIBRARY_OPTS[@]}" snapshot \
-    tink-android maven/tink-java-android.pom.xml HEAD
-fi
+echo "-- RUNNING DOCKER"
+docker run \
+  --network="host" \
+  --mount type=bind,src="${TINK_BASE_DIR}",dst=/tink_orig_dir \
+  --env-file /tmp/env_variables.txt \
+  --rm \
+  "${CONTAINER_IMAGE}" \
+  bash -c "/tink_orig_dir/tink_java/kokoro/testutils/build_maven_bundle.sh"
