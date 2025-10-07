d2ce245305637ccd17f52f24747ffcb8df67ba3b
Thomas Holenstein <tholenst@google.com>
2025-10-07 05:26:40 -0700

    Update the release_maven script.
    PiperOrigin-RevId: 816160985
    Change-Id: I8b696e48f3d198edeae9758c1599ed56422feed2

--- kokoro/release_maven.sh
+++ kokoro/release_maven.sh
@@ -14,7 +14,12 @@
 # limitations under the License.
 ################################################################################
 
-set -euo pipefail
+# Generated with openssl rand -hex 10
+echo "================================================================================"
+echo "Tink Script ID: b3b0a021d3ee098bbe8a (to quickly find the script from logs)"
+echo "================================================================================"
+
+set -eEuo pipefail
 
 # Fail if RELEASE_VERSION is not set.
 if [[ -z "${RELEASE_VERSION:-}" ]]; then
@@ -22,74 +27,66 @@ if [[ -z "${RELEASE_VERSION:-}" ]]; then
   exit 1
 fi
 
-readonly TINK_JAVA_GITHUB_URL="github.com/tink-crypto/tink-java"
-IS_KOKORO="false"
-if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
-  IS_KOKORO="true"
-fi
-readonly IS_KOKORO
-
 # WARNING: Setting this environment varialble to "true" will cause this script
 # to actually perform a release.
 : "${DO_MAKE_RELEASE:="false"}"
-# By default, release both tink and tink-android.
-: "${TINK_JAVA_ARTIFACT:="all"}"
 
 if [[ ! "${DO_MAKE_RELEASE}" =~ ^(false|true)$ ]]; then
   echo "DO_MAKE_RELEASE must be either \"true\" or \"false\"" >&2
   exit 1
 fi
 
-#######################################
-# Create a Maven release on Sonatype.
-#
-# Globals:
-#   GITHUB_ACCESS_TOKEN (optional from Kokoro)
-#   IS_KOKORO
-#   RELEASE_VERSION
-#   TINK_JAVA_GITHUB_URL
-#
-#######################################
-create_maven_release() {
-  local gitub_protocol_and_auth="ssh://git"
-  if [[ "${IS_KOKORO}" == "true" ]] ; then
-    gitub_protocol_and_auth="https://ise-crypto:${GITHUB_ACCESS_TOKEN}"
-  fi
-  readonly gitub_protocol_and_auth
-  local -r github_url="${gitub_protocol_and_auth}@${TINK_JAVA_GITHUB_URL}"
+if [[ ! -v TINK_BASE_DIR ]] ; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+fi
+readonly TINK_BASE_DIR
+echo "TINK_BASE_DIR is ${TINK_BASE_DIR}"
 
-  local maven_deploy_library_options=( -u "${github_url}" )
-  readonly maven_deploy_library_options
+cp "${KOKORO_KEYSTORE_DIR}/70968_tink_dev_maven_pgp_passphrase" \
+   "${TINK_BASE_DIR}/tink_orig_dir/tink-java/gpg_pin.txt"
+cp "${KOKORO_KEYSTORE_DIR}/70968_tink_dev_maven_pgp_secret_key" \
+   "${TINK_BASE_DIR}/tink_orig_dir/tink-java/gpg_key.asc"
 
-  if [[ "${IS_KOKORO}" == "true" ]]; then
-    # Import the PGP signing key and make the passphrase available as an env
-    # variable.
-    gpg --import --pinentry-mode loopback \
-      --passphrase-file \
-      "${KOKORO_KEYSTORE_DIR}/70968_tink_dev_maven_pgp_passphrase" \
-      --batch "${KOKORO_KEYSTORE_DIR}/70968_tink_dev_maven_pgp_secret_key"
-    export TINK_DEV_MAVEN_PGP_PASSPHRASE="$(cat \
-      "${KOKORO_KEYSTORE_DIR}/70968_tink_dev_maven_pgp_passphrase")"
-  fi
+if [[ ! -v CONTAINER_IMAGE ]] ; then
+  source \
+    "${TINK_BASE_DIR}/tink_java/kokoro/testutils/java_test_container_images.sh"
+  CONTAINER_IMAGE="${TINK_JAVA_BASE_IMAGE}"
+fi
+readonly CONTAINER_IMAGE
+echo "CONTAINER_IMAGE is ${CONTAINER_IMAGE}"
 
-  if [[ "${TINK_JAVA_ARTIFACT}" == "tink" \
-        || "${TINK_JAVA_ARTIFACT}" == "all" ]]; then
-    ./maven/maven_deploy_library.sh "${maven_deploy_library_options[@]}" \
-      release tink maven/tink-java.pom.xml "${RELEASE_VERSION}"
-  fi
-  if [[ "${TINK_JAVA_ARTIFACT}" == "tink-android" \
-        || "${TINK_JAVA_ARTIFACT}" == "all" ]]; then
-    ./maven/maven_deploy_library.sh "${maven_deploy_library_options[@]}" \
-      release tink-android maven/tink-java-android.pom.xml "${RELEASE_VERSION}"
-  fi
-}
+cd "${TINK_BASE_DIR}/tink_java"
 
-main() {
-  if [[ "${IS_KOKORO}" == "true" ]] ; then
-    readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
-    cd "${TINK_BASE_DIR}/tink_java"
-  fi
-  create_maven_release
-}
+if [[ -v TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET ]]; then
+  cp "${TINK_REMOTE_BAZEL_CACHE_SERVICE_KEY}" ./cache_key
+  cat <<EOF > /tmp/env_variables.txt
+BAZEL_REMOTE_CACHE_NAME=${TINK_REMOTE_BAZEL_CACHE_GCS_BUCKET}/bazel/${TINK_JAVA_BASE_IMAGE_HASH}
+EOF
+else
+  touch /tmp/env_variables.txt
+fi
 
-main "$@"
+if [[ ! -z "${TINK_GCR_SERVICE_KEY:-}" ]]; then
+  gcloud auth activate-service-account --key-file="${TINK_GCR_SERVICE_KEY}"
+  gcloud config set project tink-test-infrastructure
+  gcloud auth configure-docker us-docker.pkg.dev --quiet
+fi
+
+echo "-- PULLING DOCKER IMAGE"
+time docker pull "${CONTAINER_IMAGE}"
+
+echo "-- RUNNING DOCKER"
+docker run \
+  --network="host" \
+  --mount type=bind,src="${TINK_BASE_DIR}",dst=/tink_orig_dir \
+  --env-file /tmp/env_variables.txt \
+  --rm \
+  "${CONTAINER_IMAGE}" \
+  bash -c "/tink_orig_dir/tink_java/kokoro/testutils/build_maven_bundle.sh"
+
+cd "${KOKORO_ARTIFACTS_DIR}"
+mkdir -p kokoro_upload_dir/release
+cp "${TINK_BASE_DIR}"/kokoro_upload_dir/* kokoro_upload_dir/release
+
+# TODO: Upload the correct release artifacts from kokoro_upload_dir/release/
+# by using https://central.sonatype.org/publish/publish-portal-api/
