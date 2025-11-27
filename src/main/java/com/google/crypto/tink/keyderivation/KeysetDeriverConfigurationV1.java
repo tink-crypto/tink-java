7f1e3fae0174710687e092b87aef784f23647fca
Liza Tretyakova <lizatretyakova@google.com>
2025-11-27 08:47:26 -0800

    Introduce KeysetDeriverConfigurationV1 (no reparsing).
    PiperOrigin-RevId: 837545521
    Change-Id: Ib29181e29c84720ab367eb877a3eadd3dd38b340

---
+++ src/main/java/com/google/crypto/tink/keyderivation/KeysetDeriverConfigurationV1.java
@@ -0,0 +1,95 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.keyderivation;
+
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.InternalConfiguration;
+import com.google.crypto.tink.internal.PrimitiveConstructor;
+import com.google.crypto.tink.internal.PrimitiveRegistry;
+import com.google.crypto.tink.keyderivation.internal.KeyDeriver;
+import com.google.crypto.tink.keyderivation.internal.KeysetDeriverWrapper;
+import com.google.crypto.tink.keyderivation.internal.PrfBasedKeyDeriver;
+import com.google.crypto.tink.prf.HkdfPrfKey;
+import com.google.crypto.tink.subtle.prf.HkdfStreamingPrf;
+import com.google.crypto.tink.subtle.prf.StreamingPrf;
+import java.security.GeneralSecurityException;
+
+/**
+ * KeysetDeriverConfigurationV1 contains the following algorithms for KeysetDeriver:
+ *
+ * <ul>
+ *   <li>HKDF-PRF-based key derivation
+ * </ul>
+ *
+ * TODO(b/463439649): replace the usage of the global MutableKeyDerivationRegistry by a
+ * localised solution.
+ */
+/* Placeholder for internally public; DO NOT CHANGE. */ class KeysetDeriverConfigurationV1 {
+  private KeysetDeriverConfigurationV1() {}
+
+  private static final InternalConfiguration INTERNAL_CONFIGURATION = create();
+  private static final PrimitiveRegistry PRF_REGISTRY = createPrfRegistry();
+
+  private static InternalConfiguration create() {
+    try {
+      PrimitiveRegistry.Builder builder = PrimitiveRegistry.builder();
+
+      KeysetDeriverWrapper.registerToInternalPrimitiveRegistry(builder);
+
+      // HKDF-PRF-based key derivation
+      builder.registerPrimitiveConstructor(
+          PrimitiveConstructor.create(
+              KeysetDeriverConfigurationV1::createHkdfPrfBasedKeyDeriver,
+              PrfBasedKeyDerivationKey.class,
+              KeyDeriver.class));
+
+      return InternalConfiguration.createFromPrimitiveRegistry(builder.build());
+    } catch (GeneralSecurityException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private static PrimitiveRegistry createPrfRegistry() {
+    try {
+      return PrimitiveRegistry.builder()
+          .registerPrimitiveConstructor(
+              PrimitiveConstructor.create(
+                  HkdfStreamingPrf::create, HkdfPrfKey.class, StreamingPrf.class))
+          .build();
+    } catch (GeneralSecurityException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static Configuration get() throws GeneralSecurityException {
+    if (TinkFipsUtil.useOnlyFips()) {
+      throw new GeneralSecurityException(
+          "Cannot use non-FIPS-compliant KeysetDeriverConfigurationV1 in FIPS mode");
+    }
+    return INTERNAL_CONFIGURATION;
+  }
+
+  private static KeyDeriver createHkdfPrfBasedKeyDeriver(PrfBasedKeyDerivationKey key)
+      throws GeneralSecurityException {
+    // TODO(b/463439649): create the object that would make use of a local
+    //   KeyDerivationRegistry.
+    KeyDeriver deriver = PrfBasedKeyDeriver.createWithPrfPrimitiveRegistry(PRF_REGISTRY, key);
+    Object unused = deriver.deriveKey(new byte[] {1});
+    return deriver;
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/keyderivation/KeysetDeriverConfigurationV1Test.java
@@ -0,0 +1,141 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.keyderivation;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.RegistryConfiguration;
+import com.google.crypto.tink.aead.AesGcmKeyManager;
+import com.google.crypto.tink.aead.AesGcmParameters;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.LegacyProtoKey;
+import com.google.crypto.tink.internal.MutableSerializationRegistry;
+import com.google.crypto.tink.internal.ProtoKeySerialization;
+import com.google.crypto.tink.keyderivation.internal.PrfBasedKeyDerivationKeyProtoSerialization;
+import com.google.crypto.tink.prf.HkdfPrfKey;
+import com.google.crypto.tink.prf.HkdfPrfParameters;
+import com.google.crypto.tink.prf.internal.HkdfPrfProtoSerialization;
+import com.google.crypto.tink.subtle.Hex;
+import com.google.crypto.tink.util.SecretBytes;
+import java.security.GeneralSecurityException;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@SuppressWarnings("RestrictedApi")
+public class KeysetDeriverConfigurationV1Test {
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    HkdfPrfProtoSerialization.register();
+    PrfBasedKeyDerivationKeyProtoSerialization.register();
+    AesGcmKeyManager.register(true);
+  }
+
+  @Test
+  public void config_throwsIfInFipsMode() throws Exception {
+    Assume.assumeTrue(TinkFipsUtil.useOnlyFips());
+
+    assertThrows(GeneralSecurityException.class, KeysetDeriverConfigurationV1::get);
+  }
+
+  @Test
+  public void config_containsPrfBasedKeyDerivation() throws Exception {
+    HkdfPrfParameters hkdfPrfParameters =
+        HkdfPrfParameters.builder()
+            .setKeySizeBytes(32)
+            .setHashType(HkdfPrfParameters.HashType.SHA256)
+            .build();
+    HkdfPrfKey prfKey =
+        HkdfPrfKey.builder()
+            .setParameters(hkdfPrfParameters)
+            .setKeyBytes(
+                SecretBytes.copyFrom(
+                    // Value from PrfBasedKeyDerivationKeyTest.java
+                    Hex.decode("0102030405060708091011121314151617181920212123242526272829303132"),
+                    InsecureSecretKeyAccess.get()))
+            .build();
+    AesGcmParameters derivedKeyParameters =
+        AesGcmParameters.builder()
+            .setKeySizeBytes(16)
+            .setIvSizeBytes(12)
+            .setTagSizeBytes(16)
+            .setVariant(AesGcmParameters.Variant.NO_PREFIX)
+            .build();
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(prfKey.getParameters())
+            .setDerivedKeyParameters(derivedKeyParameters)
+            .build();
+    PrfBasedKeyDerivationKey key = PrfBasedKeyDerivationKey.create(parameters, prfKey, null);
+    KeysetHandle handle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withFixedId(123).makePrimary())
+            .build();
+
+    assertThat(handle.getPrimitive(KeysetDeriverConfigurationV1.get(), KeysetDeriver.class))
+        .isNotNull();
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> handle.getPrimitive(RegistryConfiguration.get(), KeysetDeriver.class));
+  }
+
+  @Test
+  public void config_handlesLegacyKey() throws Exception {
+    HkdfPrfKey prfKey =
+        HkdfPrfKey.builder()
+            .setParameters(
+                HkdfPrfParameters.builder()
+                    .setKeySizeBytes(32)
+                    .setHashType(HkdfPrfParameters.HashType.SHA256)
+                    .build())
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    PrfBasedKeyDerivationKey key =
+        PrfBasedKeyDerivationKey.create(
+            PrfBasedKeyDerivationParameters.builder()
+                .setPrfParameters(prfKey.getParameters())
+                .setDerivedKeyParameters(
+                    AesGcmParameters.builder()
+                        .setKeySizeBytes(16)
+                        .setIvSizeBytes(12)
+                        .setTagSizeBytes(16)
+                        .setVariant(AesGcmParameters.Variant.NO_PREFIX)
+                        .build())
+                .build(),
+            prfKey,
+            null);
+    ProtoKeySerialization serialization =
+        MutableSerializationRegistry.globalInstance()
+            .serializeKey(key, ProtoKeySerialization.class, InsecureSecretKeyAccess.get());
+    LegacyProtoKey legacyKey = new LegacyProtoKey(serialization, InsecureSecretKeyAccess.get());
+    KeysetHandle handle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(legacyKey).withFixedId(123).makePrimary())
+            .build();
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> handle.getPrimitive(KeysetDeriverConfigurationV1.get(), KeysetDeriver.class));
+  }
+}
