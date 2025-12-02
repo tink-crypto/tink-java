47d37db78cd32545bbdbaac128cababdf5b2e143
Liza Tretyakova <lizatretyakova@google.com>
2025-12-02 02:43:30 -0800

    Introduce SlhDsaSign/VerifyConscrypt to Tink Java.
    PiperOrigin-RevId: 839173530
    Change-Id: Ifb3ddcbf261bf8e25df7f6de3b6b85996c4f52ad

---
+++ src/main/java/com/google/crypto/tink/signature/internal/SlhDsaSignConscrypt.java
@@ -0,0 +1,156 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature.internal;
+
+import static com.google.crypto.tink.signature.internal.SlhDsaVerifyConscrypt.SLH_DSA_SHA2_128S_ALGORITHM;
+import static com.google.crypto.tink.signature.internal.SlhDsaVerifyConscrypt.SLH_DSA_SHA2_128S_SIG_LENGTH;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.PublicKeySign;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.config.internal.TinkFipsUtil.AlgorithmFipsCompatibility;
+import com.google.crypto.tink.internal.ConscryptUtil;
+import com.google.crypto.tink.signature.SlhDsaParameters;
+import com.google.crypto.tink.signature.SlhDsaParameters.HashType;
+import com.google.crypto.tink.signature.SlhDsaParameters.SignatureType;
+import com.google.crypto.tink.signature.SlhDsaPrivateKey;
+import com.google.crypto.tink.signature.internal.SlhDsaVerifyConscrypt.RawKeySpec;
+import com.google.errorprone.annotations.Immutable;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.Provider;
+import java.security.Signature;
+
+/** SLH-DSA signing with Conscypt. */
+@Immutable
+public class SlhDsaSignConscrypt implements PublicKeySign {
+  public static final TinkFipsUtil.AlgorithmFipsCompatibility FIPS =
+      AlgorithmFipsCompatibility.ALGORITHM_NOT_FIPS;
+
+  private static final String TEST_WORKLOAD = "test workload";
+
+  @SuppressWarnings("Immutable") // We do not change the output prefix
+  private final byte[] outputPrefix;
+
+  @SuppressWarnings("Immutable") // We do not change the private key
+  private final PrivateKey privateKey;
+
+  private final String algorithm;
+  private final int signatureLength;
+
+  @SuppressWarnings("Immutable") // We do not change the provider
+  private final Provider provider;
+
+  public SlhDsaSignConscrypt(
+      byte[] outputPrefix,
+      PrivateKey privateKey,
+      String algorithm,
+      int signatureLength,
+      Provider provider) {
+    this.outputPrefix = outputPrefix;
+    this.privateKey = privateKey;
+    this.algorithm = algorithm;
+    this.signatureLength = signatureLength;
+    this.provider = provider;
+  }
+
+  @AccessesPartialKey
+  public static PublicKeySign create(SlhDsaPrivateKey slhDsaPrivateKey)
+      throws GeneralSecurityException {
+    if (!FIPS.isCompatible()) {
+      throw new GeneralSecurityException(
+          "Can not use SLH-DSA in FIPS-mode, as it is not yet certified in Conscrypt.");
+    }
+
+    Provider provider = ConscryptUtil.providerOrNull();
+    if (provider == null) {
+      throw new GeneralSecurityException("Obtaining Conscrypt provider failed");
+    }
+
+    SlhDsaParameters parameters = slhDsaPrivateKey.getParameters();
+    if (parameters.getPrivateKeySize() != SlhDsaParameters.SLH_DSA_128_PRIVATE_KEY_SIZE_BYTES
+        || parameters.getHashType() != HashType.SHA2
+        || parameters.getSignatureType() != SignatureType.SMALL_SIGNATURE) {
+      throw new GeneralSecurityException("Unsupported SLH-DSA parameters");
+    }
+
+    PrivateKey privateKey =
+        KeyFactory.getInstance(SLH_DSA_SHA2_128S_ALGORITHM, provider)
+            .generatePrivate(
+                new RawKeySpec(
+                    slhDsaPrivateKey
+                        .getPrivateKeyBytes()
+                        .toByteArray(InsecureSecretKeyAccess.get())));
+
+    // Verify that the public key and the private key match by creating and verifying a dummy
+    // signature. This is subomptimal to do it this way, but at this time we prefer to not have our
+    // own key derivation implementation, and some check is better than nothing.
+    byte[] testSignature =
+        signInternal(
+            TEST_WORKLOAD.getBytes(UTF_8),
+            slhDsaPrivateKey.getOutputPrefix().toByteArray(),
+            privateKey,
+            SLH_DSA_SHA2_128S_ALGORITHM,
+            SLH_DSA_SHA2_128S_SIG_LENGTH,
+            provider);
+    SlhDsaVerifyConscrypt verifier =
+        (SlhDsaVerifyConscrypt) SlhDsaVerifyConscrypt.create(slhDsaPrivateKey.getPublicKey());
+    verifier.verify(testSignature, TEST_WORKLOAD.getBytes(UTF_8));
+
+    // If verified successfully, proceed with the primitive creation.
+    return new SlhDsaSignConscrypt(
+        slhDsaPrivateKey.getOutputPrefix().toByteArray(),
+        privateKey,
+        SLH_DSA_SHA2_128S_ALGORITHM,
+        SLH_DSA_SHA2_128S_SIG_LENGTH,
+        provider);
+  }
+
+  /** Returns true if the Conscrypt is available and supports SLH-DSA-SHA2-128S. */
+  public static boolean isSupported() {
+    return SlhDsaVerifyConscrypt.isSupported();
+  }
+
+  @Override
+  public byte[] sign(final byte[] data) throws GeneralSecurityException {
+    return signInternal(data, outputPrefix, privateKey, algorithm, signatureLength, provider);
+  }
+
+  private static byte[] signInternal(
+      byte[] data,
+      byte[] outputPrefix,
+      PrivateKey privateKey,
+      String algorithm,
+      int signatureLength,
+      Provider provider)
+      throws GeneralSecurityException {
+    Signature signer = Signature.getInstance(algorithm, provider);
+    signer.initSign(privateKey);
+    signer.update(data);
+    byte[] signature = new byte[outputPrefix.length + signatureLength];
+    if (outputPrefix.length > 0) {
+      System.arraycopy(outputPrefix, 0, signature, 0, outputPrefix.length);
+    }
+    // Use this interface instead of plain sign() to avoid an extra copy of 7MB of signature.
+    // Resets the Signature object into its initial initialized state.
+    signer.sign(signature, outputPrefix.length, signatureLength);
+    return signature;
+  }
+}
---
+++ src/main/java/com/google/crypto/tink/signature/internal/SlhDsaVerifyConscrypt.java
@@ -0,0 +1,151 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature.internal;
+
+import static com.google.crypto.tink.internal.Util.isPrefix;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.PublicKeyVerify;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.config.internal.TinkFipsUtil.AlgorithmFipsCompatibility;
+import com.google.crypto.tink.internal.ConscryptUtil;
+import com.google.crypto.tink.signature.SlhDsaParameters;
+import com.google.crypto.tink.signature.SlhDsaParameters.HashType;
+import com.google.crypto.tink.signature.SlhDsaParameters.SignatureType;
+import com.google.crypto.tink.signature.SlhDsaPublicKey;
+import com.google.errorprone.annotations.Immutable;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.Provider;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.EncodedKeySpec;
+
+/** SLH-DSA verification with Conscypt. */
+@Immutable
+public class SlhDsaVerifyConscrypt implements PublicKeyVerify {
+  public static final TinkFipsUtil.AlgorithmFipsCompatibility FIPS =
+      AlgorithmFipsCompatibility.ALGORITHM_NOT_FIPS;
+
+  static final int SLH_DSA_SHA2_128S_SIG_LENGTH = 7856;
+  static final String SLH_DSA_SHA2_128S_ALGORITHM = "SLH-DSA-SHA2-128S";
+
+  @SuppressWarnings("Immutable") // We do not change the output prefix
+  private final byte[] outputPrefix;
+
+  @SuppressWarnings("Immutable") // We do not change the private key
+  private final PublicKey publicKey;
+
+  private final String algorithm;
+  private final int signatureLength;
+
+  @SuppressWarnings("Immutable") // We do not change the provider
+  private final Provider provider;
+
+  public SlhDsaVerifyConscrypt(
+      byte[] outputPrefix,
+      PublicKey publicKey,
+      String algorithm,
+      int signatureLength,
+      Provider provider) {
+    this.outputPrefix = outputPrefix;
+    this.publicKey = publicKey;
+    this.algorithm = algorithm;
+    this.signatureLength = signatureLength;
+    this.provider = provider;
+  }
+
+  @AccessesPartialKey
+  public static PublicKeyVerify create(SlhDsaPublicKey slhDsaPublicKey)
+      throws GeneralSecurityException {
+    if (!FIPS.isCompatible()) {
+      throw new GeneralSecurityException(
+          "Can not use ML-DSA in FIPS-mode, as it is not yet certified in Conscrypt.");
+    }
+
+    Provider provider = ConscryptUtil.providerOrNull();
+    if (provider == null) {
+      throw new GeneralSecurityException("Obtaining Conscrypt provider failed");
+    }
+
+    SlhDsaParameters parameters = slhDsaPublicKey.getParameters();
+    if (parameters.getPrivateKeySize() != SlhDsaParameters.SLH_DSA_128_PRIVATE_KEY_SIZE_BYTES
+        || parameters.getHashType() != HashType.SHA2
+        || parameters.getSignatureType() != SignatureType.SMALL_SIGNATURE) {
+      throw new GeneralSecurityException("Unsupported SLH-DSA parameters");
+    }
+
+    PublicKey publicKey =
+        KeyFactory.getInstance(SLH_DSA_SHA2_128S_ALGORITHM, provider)
+            .generatePublic(new RawKeySpec(slhDsaPublicKey.getSerializedPublicKey().toByteArray()));
+
+    return new SlhDsaVerifyConscrypt(
+        slhDsaPublicKey.getOutputPrefix().toByteArray(),
+        publicKey,
+        SLH_DSA_SHA2_128S_ALGORITHM,
+        SLH_DSA_SHA2_128S_SIG_LENGTH,
+        provider);
+  }
+
+  @Override
+  public void verify(byte[] signature, byte[] data) throws GeneralSecurityException {
+    if (!isPrefix(outputPrefix, signature)) {
+      throw new GeneralSecurityException("Invalid signature (output prefix mismatch)");
+    }
+    if (signature.length != outputPrefix.length + signatureLength) {
+      throw new GeneralSecurityException("Invalid signature length");
+    }
+    Signature verifier = Signature.getInstance(algorithm, provider);
+    verifier.initVerify(publicKey);
+    verifier.update(data);
+    if (!verifier.verify(signature, outputPrefix.length, signatureLength)) {
+      throw new GeneralSecurityException("Invalid signature");
+    }
+  }
+
+  /** Returns true if we're not in FIPS, and Conscrypt is available and supports SLH-DSA-SHA2-128S. */
+  public static boolean isSupported() {
+    if (!FIPS.isCompatible()) {
+      return false;
+    }
+
+    Provider provider = ConscryptUtil.providerOrNull();
+    if (provider == null) {
+      return false;
+    }
+
+    try {
+      KeyFactory unusedKeyFactory = KeyFactory.getInstance(SLH_DSA_SHA2_128S_ALGORITHM, provider);
+      Signature unusedSignature = Signature.getInstance(SLH_DSA_SHA2_128S_ALGORITHM, provider);
+      return true;
+    } catch (GeneralSecurityException e) {
+      return false;
+    }
+  }
+
+  /** Representation of the raw keys for interoperability with Conscrypt. */
+  public static final class RawKeySpec extends EncodedKeySpec {
+    public RawKeySpec(byte[] encoded) {
+      super(encoded);
+    }
+
+    @Override
+    public String getFormat() {
+      return "raw";
+    }
+  }
+}
