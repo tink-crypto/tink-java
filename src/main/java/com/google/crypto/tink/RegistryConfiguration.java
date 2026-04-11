fb12127c85969bb5aad0668bf839f02431e3e6c3
Thomas Holenstein <tholenst@google.com>
2026-04-11 02:50:11 -0700

    Remove the class "internal.RegistryConfiguration".
    Since we have a public class, there isn't really a need for keeping the only internal one anymore.
    
    We move the implementation to the public one.
    
    PiperOrigin-RevId: 898126428
    Change-Id: Iad8a15bdcdc8d7499a29b7280bf5b0c9898c1206

--- src/main/java/com/google/crypto/tink/RegistryConfiguration.java
+++ src/main/java/com/google/crypto/tink/RegistryConfiguration.java
@@ -16,16 +16,86 @@
 
 package com.google.crypto.tink;
 
+import com.google.crypto.tink.internal.MutablePrimitiveRegistry;
+import com.google.crypto.tink.internal.MutableSerializationRegistry;
+import com.google.crypto.tink.internal.ProtoParametersSerialization;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.ExtensionRegistryLite;
+import com.google.protobuf.InvalidProtocolBufferException;
 import java.security.GeneralSecurityException;
+import javax.annotation.Nullable;
 
 /**
  * RegistryConfiguration is a proxy Configuration that forwards all calls to the global Registry.
  */
 public class RegistryConfiguration {
-  private RegistryConfiguration() {}
-
-  /** get returns a Configuration forwarding all calls to the global Registry. */
+  // Returns the singleton instance of RegistryConfiguration.
   public static Configuration get() throws GeneralSecurityException {
-    return com.google.crypto.tink.internal.RegistryConfiguration.get();
+    return CONFIG;
   }
+
+  private static class RegistryProtoKeySerializer implements ProtoKeySerializer {
+    @Override
+    public Key parseKey(
+        com.google.crypto.tink.ProtoKeySerialization protoKeySerialization,
+        @Nullable SecretKeyAccess access)
+        throws GeneralSecurityException {
+      return MutableSerializationRegistry.globalInstance()
+          .parseKey(
+              com.google.crypto.tink.internal.ProtoKeySerialization.createFromPublic(
+                  protoKeySerialization),
+              access);
+    }
+
+    @Override
+    public com.google.crypto.tink.ProtoKeySerialization serializeKey(
+        Key key, @Nullable SecretKeyAccess access) throws GeneralSecurityException {
+      return MutableSerializationRegistry.globalInstance()
+          .serializeKey(key, com.google.crypto.tink.internal.ProtoKeySerialization.class, access)
+          .toPublic();
+    }
+
+    @Override
+    public ByteString serializeParameters(Parameters parameters) throws GeneralSecurityException {
+      ProtoParametersSerialization serialization =
+          MutableSerializationRegistry.globalInstance()
+              .serializeParameters(parameters, ProtoParametersSerialization.class);
+      return serialization.getKeyTemplate().toByteString();
+    }
+
+    @Override
+    @SuppressWarnings("UnnecessarilyFullyQualified") // We fully specify KeyTemplate in Tink.
+    public Parameters parseParameters(ByteString serialization) throws GeneralSecurityException {
+      try {
+        com.google.crypto.tink.proto.KeyTemplate template =
+            com.google.crypto.tink.proto.KeyTemplate.parseFrom(
+                serialization, ExtensionRegistryLite.getEmptyRegistry());
+        return MutableSerializationRegistry.globalInstance()
+            .parseParameters(ProtoParametersSerialization.create(template));
+      } catch (InvalidProtocolBufferException e) {
+        throw new GeneralSecurityException("Problem parsing the parameters", e);
+      }
+    }
+  }
+
+  private static final RegistryProtoKeySerializer SERIALIZER = new RegistryProtoKeySerializer();
+  private static final Configuration CONFIG =
+      new Configuration() {
+        @Override
+        public <P> P createPrimitive(KeysetHandleInterface keysetHandle, Class<P> clazz)
+            throws GeneralSecurityException {
+          return MutablePrimitiveRegistry.globalInstance().wrap(keysetHandle, clazz);
+        }
+
+        @Override
+        public <P> P get(Class<P> clazz) throws GeneralSecurityException {
+          if (clazz.equals(ProtoKeySerializer.class)) {
+            return clazz.cast(SERIALIZER);
+          }
+          throw new GeneralSecurityException(
+              "RegistryConfiguration does not support get for " + clazz);
+        }
+      };
+
+  private RegistryConfiguration() {}
 }
