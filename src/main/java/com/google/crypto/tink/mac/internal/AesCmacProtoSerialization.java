906ce3c1dfb2192de04f2ffc1433af681b5dfeb3
William Conner <wconner@google.com>
2026-05-04 04:21:06 -0700

    Enable `AES-CMAC` proto serialization registration with a registry builder.
    PiperOrigin-RevId: 909945987
    Change-Id: Ic80c690d27303d8f674dc6879f9221594c5cb36e

--- src/main/java/com/google/crypto/tink/mac/internal/AesCmacProtoSerialization.java
+++ src/main/java/com/google/crypto/tink/mac/internal/AesCmacProtoSerialization.java
@@ -27,6 +27,7 @@ import com.google.crypto.tink.internal.ParametersParser;
 import com.google.crypto.tink.internal.ParametersSerializer;
 import com.google.crypto.tink.internal.ProtoKeySerialization;
 import com.google.crypto.tink.internal.ProtoParametersSerialization;
+import com.google.crypto.tink.internal.SerializationRegistry;
 import com.google.crypto.tink.mac.AesCmacKey;
 import com.google.crypto.tink.mac.AesCmacParameters;
 import com.google.crypto.tink.proto.KeyData.KeyMaterialType;
@@ -223,6 +224,14 @@ public final class AesCmacProtoSerialization {
     registry.registerParametersParser(PARAMETERS_PARSER);
     registry.registerKeySerializer(KEY_SERIALIZER);
     registry.registerKeyParser(KEY_PARSER);
+  }
+
+  public static void register(SerializationRegistry.Builder registryBuilder)
+      throws GeneralSecurityException {
+    registryBuilder.registerParametersSerializer(PARAMETERS_SERIALIZER);
+    registryBuilder.registerParametersParser(PARAMETERS_PARSER);
+    registryBuilder.registerKeySerializer(KEY_SERIALIZER);
+    registryBuilder.registerKeyParser(KEY_PARSER);
   }
 
   private AesCmacProtoSerialization() {}
