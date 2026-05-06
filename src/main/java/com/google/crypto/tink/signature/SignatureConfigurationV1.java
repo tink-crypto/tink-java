688858f0a4ec6c6be27b8b8f64b08b52f773b561
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-05-06 06:23:06 -0700

    Add SignatureConfiguration2026 structure for Java.
    PiperOrigin-RevId: 911299181
    Change-Id: I397f6415642fb2b460094eb403cfd49c707c2216

--- src/main/java/com/google/crypto/tink/signature/SignatureConfigurationV1.java
+++ src/main/java/com/google/crypto/tink/signature/SignatureConfigurationV1.java
@@ -17,119 +17,17 @@
 package com.google.crypto.tink.signature;
 
 import com.google.crypto.tink.Configuration;
-import com.google.crypto.tink.Key;
-import com.google.crypto.tink.KeysetHandleInterface;
-import com.google.crypto.tink.PublicKeySign;
-import com.google.crypto.tink.PublicKeyVerify;
-import com.google.crypto.tink.config.internal.TinkFipsUtil;
-import com.google.crypto.tink.signature.internal.MlDsaSignConscrypt;
-import com.google.crypto.tink.signature.internal.MlDsaVerifyConscrypt;
-import com.google.crypto.tink.signature.internal.SlhDsaSignConscrypt;
-import com.google.crypto.tink.signature.internal.SlhDsaVerifyConscrypt;
-import com.google.crypto.tink.subtle.EcdsaSignJce;
-import com.google.crypto.tink.subtle.EcdsaVerifyJce;
-import com.google.crypto.tink.subtle.Ed25519Sign;
-import com.google.crypto.tink.subtle.Ed25519Verify;
-import com.google.crypto.tink.subtle.RsaSsaPkcs1SignJce;
-import com.google.crypto.tink.subtle.RsaSsaPkcs1VerifyJce;
-import com.google.crypto.tink.subtle.RsaSsaPssSignJce;
-import com.google.crypto.tink.subtle.RsaSsaPssVerifyJce;
 import java.security.GeneralSecurityException;
 
 /**
- * SignatureConfigurationV1 contains the following algorithms for PublicKeySign/Verify:
- *
- * <ul>
- *   <li>Ecdsa
- *   <li>RsaSsaPss
- *   <li>RsaSsaPkcs1
- *   <li>Ed25519
- *   <li>MlDsa65
- * </ul>
+ * SignatureConfigurationV1 is a wrapper around SignatureConfig2026 for backward compatibility.
+ * It is not exposed to OSS.
  */
 /* Placeholder for internally public; DO NOT CHANGE. */ class SignatureConfigurationV1 {
   private SignatureConfigurationV1() {}
 
-  private static final PublicKeySignWrapper PUBLIC_KEY_SIGN_WRAPPER = new PublicKeySignWrapper();
-  private static final PublicKeyVerifyWrapper PUBLIC_KEY_VERIFY_WRAPPER =
-      new PublicKeyVerifyWrapper();
-  private static final Configuration CONFIGURATION = create();
-
-  private static Configuration create() {
-    return new Configuration() {
-      @Override
-      public <P> P createPrimitive(KeysetHandleInterface keysetHandle, Class<P> clazz)
-          throws GeneralSecurityException {
-        if (clazz == PublicKeySign.class) {
-          return clazz.cast(
-              PUBLIC_KEY_SIGN_WRAPPER.wrap(
-                  keysetHandle, SignatureConfigurationV1::createPublicKeySign));
-        }
-        if (clazz == PublicKeyVerify.class) {
-          return clazz.cast(
-              PUBLIC_KEY_VERIFY_WRAPPER.wrap(
-                  keysetHandle, SignatureConfigurationV1::createPublicKeyVerify));
-        }
-        throw new GeneralSecurityException(
-            "SignatureConfigurationV1 can only create PublicKeySign and PublicKeyVerify");
-      }
-    };
-  }
-
   /** Returns an instance of the {@code SignatureConfigurationV1}. */
   public static Configuration get() throws GeneralSecurityException {
-    if (TinkFipsUtil.useOnlyFips()) {
-      throw new GeneralSecurityException(
-          "Cannot use non-FIPS-compliant SignatureConfigurationV1 in FIPS mode");
-    }
-    return CONFIGURATION;
-  }
-
-  private static PublicKeySign createPublicKeySign(KeysetHandleInterface.Entry entry)
-      throws GeneralSecurityException {
-    Key key = entry.getKey();
-    if (key instanceof EcdsaPrivateKey) {
-      return EcdsaSignJce.create((EcdsaPrivateKey) key);
-    }
-    if (key instanceof RsaSsaPssPrivateKey) {
-      return RsaSsaPssSignJce.create((RsaSsaPssPrivateKey) key);
-    }
-    if (key instanceof RsaSsaPkcs1PrivateKey) {
-      return RsaSsaPkcs1SignJce.create((RsaSsaPkcs1PrivateKey) key);
-    }
-    if (key instanceof Ed25519PrivateKey) {
-      return Ed25519Sign.create((Ed25519PrivateKey) key);
-    }
-    if (key instanceof MlDsaPrivateKey) {
-      return MlDsaSignConscrypt.create((MlDsaPrivateKey) key);
-    }
-    if (key instanceof SlhDsaPrivateKey) {
-      return SlhDsaSignConscrypt.create((SlhDsaPrivateKey) key);
-    }
-    throw new GeneralSecurityException("Unknown key class: " + key.getClass());
-  }
-
-  private static PublicKeyVerify createPublicKeyVerify(KeysetHandleInterface.Entry entry)
-      throws GeneralSecurityException {
-    Key key = entry.getKey();
-    if (key instanceof EcdsaPublicKey) {
-      return EcdsaVerifyJce.create((EcdsaPublicKey) key);
-    }
-    if (key instanceof RsaSsaPssPublicKey) {
-      return RsaSsaPssVerifyJce.create((RsaSsaPssPublicKey) key);
-    }
-    if (key instanceof RsaSsaPkcs1PublicKey) {
-      return RsaSsaPkcs1VerifyJce.create((RsaSsaPkcs1PublicKey) key);
-    }
-    if (key instanceof Ed25519PublicKey) {
-      return Ed25519Verify.create((Ed25519PublicKey) key);
-    }
-    if (key instanceof MlDsaPublicKey) {
-      return MlDsaVerifyConscrypt.create((MlDsaPublicKey) key);
-    }
-    if (key instanceof SlhDsaPublicKey) {
-      return SlhDsaVerifyConscrypt.create((SlhDsaPublicKey) key);
-    }
-    throw new GeneralSecurityException("Unknown key class: " + key.getClass());
+    return SignatureConfig2026.get();
   }
 }
---
+++ src/test/java/com/google/crypto/tink/signature/SignatureConfig2026Test.java
@@ -0,0 +1,244 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.signature;
+
+import static java.util.Arrays.stream;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.PublicKeySign;
+import com.google.crypto.tink.PublicKeyVerify;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.Util;
+import com.google.crypto.tink.signature.internal.EcdsaProtoSerialization;
+import com.google.crypto.tink.signature.internal.Ed25519ProtoSerialization;
+import com.google.crypto.tink.signature.internal.MlDsaProtoSerialization;
+import com.google.crypto.tink.signature.internal.MlDsaVerifyConscrypt;
+import com.google.crypto.tink.signature.internal.RsaSsaPkcs1ProtoSerialization;
+import com.google.crypto.tink.signature.internal.RsaSsaPssProtoSerialization;
+import com.google.crypto.tink.signature.internal.SlhDsaProtoSerialization;
+import com.google.crypto.tink.signature.internal.SlhDsaVerifyConscrypt;
+import com.google.crypto.tink.signature.internal.testing.EcdsaTestUtil;
+import com.google.crypto.tink.signature.internal.testing.Ed25519TestUtil;
+import com.google.crypto.tink.signature.internal.testing.MlDsaTestUtil;
+import com.google.crypto.tink.signature.internal.testing.RsaSsaPkcs1TestUtil;
+import com.google.crypto.tink.signature.internal.testing.RsaSsaPssTestUtil;
+import com.google.crypto.tink.signature.internal.testing.SignatureTestVector;
+import com.google.crypto.tink.signature.internal.testing.SlhDsaTestUtil;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import javax.annotation.Nullable;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class SignatureConfig2026Test {
+  @BeforeClass
+  public static void setUp() throws Exception {
+    EcdsaProtoSerialization.register();
+    RsaSsaPssProtoSerialization.register();
+    RsaSsaPkcs1ProtoSerialization.register();
+    Ed25519ProtoSerialization.register();
+    MlDsaProtoSerialization.register();
+    SlhDsaProtoSerialization.register();
+  }
+
+  @Test
+  public void config_throwsIfInFipsMode() throws Exception {
+    Assume.assumeTrue(TinkFipsUtil.useOnlyFips());
+
+    assertThrows(GeneralSecurityException.class, SignatureConfig2026::get);
+  }
+
+  /**
+   * Tests that when using the public API of Tink, signatures in the test vector can be verified.
+   * This additionally ensures that all the expected algorithms (Ecdsa, RsaSsaPss, RsaSsaPkcs1,
+   * Ed25519 for {@code PublicKeyVerify}, as well as MlDsa and SlhDsa if Conscrypt is available and
+   * has support) are present in the SignatureConfig2026.
+   */
+  @Theory
+  public void test_validateSignatureInTestVector(
+      @FromDataPoints("signatureTests") SignatureTestVector testVector) throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    @Nullable Integer apiLevel = Util.getAndroidApiLevel();
+    if (apiLevel != null && apiLevel == 19) {
+      // Android API 19 is slower than the others in this.
+      return;
+    }
+    if ((testVector.getPrivateKey() instanceof MlDsaPrivateKey
+            && !MlDsaVerifyConscrypt.isSupported())
+        || (testVector.getPrivateKey() instanceof SlhDsaPrivateKey
+            && !SlhDsaVerifyConscrypt.isSupported())) {
+      // ML-DSA/SLH-DSA requires Conscrypt to be available. This also captures cases where
+      // ML-DSA/SLH-DSA is not
+      // available even with Conscrypt installed, since 1) an older version of Conscrypt may be in
+      // use and 2) Conscrypt on Android does not support ML-DSA/SLH-DSA yet.
+      return;
+    }
+
+    SignaturePrivateKey key = testVector.getPrivateKey();
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    @Nullable Integer id = key.getIdRequirementOrNull();
+    if (id == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(id);
+    }
+    KeysetHandle handle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    PublicKeyVerify verifier =
+        handle
+            .getPublicKeysetHandle()
+            .getPrimitive(SignatureConfig2026.get(), PublicKeyVerify.class);
+
+    verifier.verify(testVector.getSignature(), testVector.getMessage());
+  }
+
+  /**
+   * Tests that when using the public API of Tink, newly created signatures can be verified. This
+   * additionally ensures that all the expected algorithms (Ecdsa, RsaSsaPss, RsaSsaPkcs1, Ed25519
+   * for {@code PublicKeySign}, as well as MlDsa and SlhDsa if Conscrypt is available and has
+   * support) are present in the SignatureConfig2026.
+   */
+  @Theory
+  public void test_computeAndValidateFreshSignatureWithTestVector(
+      @FromDataPoints("signatureTests") SignatureTestVector testVector) throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    @Nullable Integer apiLevel = Util.getAndroidApiLevel();
+    if (apiLevel != null && apiLevel == 19) {
+      // Android API 19 is slower than the others in this.
+      return;
+    }
+
+    SignaturePrivateKey key = testVector.getPrivateKey();
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    @Nullable Integer id = key.getIdRequirementOrNull();
+    if (id == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(id);
+    }
+    KeysetHandle handle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    if ((testVector.getPrivateKey() instanceof MlDsaPrivateKey
+            && !MlDsaVerifyConscrypt.isSupported())
+        || (testVector.getPrivateKey() instanceof SlhDsaPrivateKey
+            && !SlhDsaVerifyConscrypt.isSupported())) {
+      // ML-DSA/SLH-DSA requires Conscrypt to be available. This also captures cases where
+      // ML-DSA/SLH-DSA is not
+      // available even with Conscrypt installed, since 1) an older version of Conscrypt may be in
+      // use and 2) Conscrypt on Android does not support ML-DSA/SLH-DSA yet.
+      Configuration configuration = SignatureConfig2026.get();
+      assertThrows(
+          GeneralSecurityException.class,
+          () -> handle.getPrimitive(configuration, PublicKeySign.class));
+      KeysetHandle keysetHandle = handle.getPublicKeysetHandle();
+      Configuration configuration2 = SignatureConfig2026.get();
+      assertThrows(
+          GeneralSecurityException.class,
+          () -> keysetHandle.getPrimitive(configuration2, PublicKeyVerify.class));
+      return;
+    }
+
+    PublicKeySign signer = handle.getPrimitive(SignatureConfig2026.get(), PublicKeySign.class);
+    byte[] signature = signer.sign(testVector.getMessage());
+    PublicKeyVerify verifier =
+        handle
+            .getPublicKeysetHandle()
+            .getPrimitive(SignatureConfig2026.get(), PublicKeyVerify.class);
+
+    verifier.verify(signature, testVector.getMessage());
+  }
+
+  private static byte[] modifyInput(byte[] message) {
+    if (message.length == 0) {
+      return new byte[] {1};
+    }
+    byte[] copy = Arrays.copyOf(message, message.length);
+    copy[0] ^= 1;
+    return copy;
+  }
+
+  /**
+   * Ensures correctness in the faulty scenario of the Sign/Verify primitives obtained through the
+   * public API of Tink.
+   */
+  @Theory
+  public void test_computeFreshSignatureWithTestVector_throwsWithWrongMessage(
+      @FromDataPoints("signatureTests") SignatureTestVector testVector) throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    @Nullable Integer apiLevel = Util.getAndroidApiLevel();
+    if (apiLevel != null && apiLevel == 19) {
+      // Android API 19 is slower than the others in this.
+      return;
+    }
+    if ((testVector.getPrivateKey() instanceof MlDsaPrivateKey
+            && !MlDsaVerifyConscrypt.isSupported())
+        || (testVector.getPrivateKey() instanceof SlhDsaPrivateKey
+            && !SlhDsaVerifyConscrypt.isSupported())) {
+      // ML-DSA/SLH-DSA requires Conscrypt to be available. This also captures cases where
+      // ML-DSA/SLH-DSA is not
+      // available even with Conscrypt installed, since 1) an older version of Conscrypt may be in
+      // use and 2) Conscrypt on Android does not support ML-DSA/SLH-DSA yet.
+      return;
+    }
+
+    SignaturePrivateKey key = testVector.getPrivateKey();
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    @Nullable Integer id = key.getIdRequirementOrNull();
+    if (id == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(id);
+    }
+    KeysetHandle handle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    PublicKeySign signer = handle.getPrimitive(SignatureConfig2026.get(), PublicKeySign.class);
+    byte[] signature = signer.sign(testVector.getMessage());
+    PublicKeyVerify verifier =
+        handle
+            .getPublicKeysetHandle()
+            .getPrimitive(SignatureConfig2026.get(), PublicKeyVerify.class);
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () -> verifier.verify(signature, modifyInput(testVector.getMessage())));
+  }
+
+  @DataPoints("signatureTests")
+  public static final SignatureTestVector[] signatureTestVectors =
+      Stream.concat(
+              Stream.concat(
+                  Stream.concat(
+                      stream(EcdsaTestUtil.createEcdsaTestVectors()),
+                      stream(RsaSsaPssTestUtil.createRsaPssTestVectors())),
+                  Stream.concat(
+                      stream(RsaSsaPkcs1TestUtil.createRsaSsaPkcs1TestVectors()),
+                      stream(Ed25519TestUtil.createEd25519TestVectors()))),
+              Stream.concat(
+                  MlDsaTestUtil.createMlDsa65ValidSignatureTestVectors(),
+                  SlhDsaTestUtil.createSlhDsaValidSignatureTestVectors()))
+          .toArray(SignatureTestVector[]::new);
+}
