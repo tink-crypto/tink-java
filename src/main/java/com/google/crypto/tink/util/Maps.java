c4ac941ba34149846c18af22520de225f06dc8c9
Clayton Walker <clayton.m.walker@gmail.com>
2026-05-21 09:54:32 -0600

    Specify return as unmodifyable

--- src/main/java/com/google/crypto/tink/util/Maps.java
+++ src/main/java/com/google/crypto/tink/util/Maps.java
@@ -1,14 +1,20 @@
 package com.google.crypto.tink.util;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+
 import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public final class Maps {
   @SafeVarargs
   public static <K, V> Map<K, V> ofEntries(Map.Entry<K, V>... entries) {
-    return Arrays.stream(entries).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    return Arrays.stream(entries)
+        .collect(
+            collectingAndThen(
+                toMap(Map.Entry::getKey, Map.Entry::getValue), Collections::unmodifiableMap));
   }
 
   public static <K, V> Map.Entry<K, V> entry(K k, V v) {
