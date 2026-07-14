daf51041e6d442802a5fb8a6a831d2cec8826e09
Thomas Holenstein <tholenst@google.com>
2026-07-14 07:31:52 -0700

    Add primitive creation support to ProtoBasedConfigurationBuilder.
    PiperOrigin-RevId: 947672725
    Change-Id: I0c073f8420fa387b6079682b1533ed5ddd15d473

--- src/main/java/com/google/crypto/tink/internal/ProtoBasedConfigurationBuilder.java
+++ src/main/java/com/google/crypto/tink/internal/ProtoBasedConfigurationBuilder.java
@@ -18,22 +18,76 @@ package com.google.crypto.tink.internal;
 
 import com.google.crypto.tink.Configuration;
 import com.google.crypto.tink.Key;
+import com.google.crypto.tink.KeysetHandleInterface;
 import com.google.crypto.tink.Parameters;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.Nullable;
 
-/** ProtoBasedConfigurationBuilder builds an internal {@link Configuration} instance. */
+/**
+ * ProtoBasedConfigurationBuilder helps create {@link Configuration} instances.
+ *
+ * <p>It is especially optimized to create standard configurations where serialization and parsing
+ * is based on the Protobuf serialization which Tink uses.
+ */
 public final class ProtoBasedConfigurationBuilder {
+
+  /** Functional interface to wrap a keyset into a primitive. */
+  @FunctionalInterface
+  public interface WrapFunction<B, P> {
+    P wrap(KeysetHandleInterface keysetHandle, PrimitiveWrapper.PrimitiveFactory<B> factory)
+        throws GeneralSecurityException;
+  }
+
+  /**
+   * A pair (Class<? extends Key>, Class<?>) used to index into a map for primitive constructors.
+   */
+  private static final class PrimitiveConstructorKey {
+    private final Class<? extends Key> keyClass;
+    private final Class<?> primitiveClass;
+
+    private PrimitiveConstructorKey(Class<? extends Key> keyClass, Class<?> primitiveClass) {
+      this.keyClass = keyClass;
+      this.primitiveClass = primitiveClass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof PrimitiveConstructorKey)) {
+        return false;
+      }
+      PrimitiveConstructorKey other = (PrimitiveConstructorKey) o;
+      return other.keyClass.equals(keyClass) && other.primitiveClass.equals(primitiveClass);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(keyClass, primitiveClass);
+    }
+
+    @Override
+    public String toString() {
+      return keyClass.getSimpleName() + " with primitive type: " + primitiveClass.getSimpleName();
+    }
+  }
 
   private static final class InternalProtoBasedConfiguration implements Configuration {
     private final Map<Class<? extends Parameters>, KeyCreator<?>> keyCreators;
+    private final Map<PrimitiveConstructorKey, PrimitiveConstructor<?, ?>> primitiveConstructors;
+    private final Map<Class<?>, WrapFunctionAndInputPrimitiveClass<?, ?>> wrappers;
 
-    InternalProtoBasedConfiguration(Map<Class<? extends Parameters>, KeyCreator<?>> keyCreators) {
+    InternalProtoBasedConfiguration(
+        Map<Class<? extends Parameters>, KeyCreator<?>> keyCreators,
+        Map<PrimitiveConstructorKey, PrimitiveConstructor<?, ?>> primitiveConstructors,
+        Map<Class<?>, WrapFunctionAndInputPrimitiveClass<?, ?>> wrappers) {
       this.keyCreators = Collections.unmodifiableMap(new HashMap<>(keyCreators));
+      this.primitiveConstructors =
+          Collections.unmodifiableMap(new HashMap<>(primitiveConstructors));
+      this.wrappers = Collections.unmodifiableMap(new HashMap<>(wrappers));
     }
 
     @Override
@@ -47,12 +101,72 @@ public final class ProtoBasedConfigurationBuilder {
       }
       return creator.createKey(parameters, idRequirement);
     }
+
+    @Override
+    public <P> P createPrimitive(KeysetHandleInterface keysetHandle, Class<P> clazz)
+        throws GeneralSecurityException {
+      WrapFunctionAndInputPrimitiveClass<?, ?> wrapFunctionAndInputPrimitiveClass =
+          wrappers.get(clazz);
+      if (wrapFunctionAndInputPrimitiveClass == null) {
+        throw new GeneralSecurityException(
+            "No wrapper registered for primitive class: " + clazz.getName());
+      }
+      @SuppressWarnings("unchecked") // We create the map so that it satisfies this.
+      WrapFunctionAndInputPrimitiveClass<?, P> typedRegistration =
+          (WrapFunctionAndInputPrimitiveClass<?, P>) wrapFunctionAndInputPrimitiveClass;
+      return wrapHelper(typedRegistration, keysetHandle);
+    }
+
+    private <B, P> P wrapHelper(
+        WrapFunctionAndInputPrimitiveClass<B, P> registration, KeysetHandleInterface keysetHandle)
+        throws GeneralSecurityException {
+      return registration.wrapFunction.wrap(
+          keysetHandle, entry -> createInputPrimitive(entry, registration.inputPrimitiveClass));
+    }
+
+    private <B> B createInputPrimitive(
+        KeysetHandleInterface.Entry entry, Class<B> inputPrimitiveClass)
+        throws GeneralSecurityException {
+      Key key = entry.getKey();
+      PrimitiveConstructorKey keyPair =
+          new PrimitiveConstructorKey((Class<? extends Key>) key.getClass(), inputPrimitiveClass);
+      @SuppressWarnings("unchecked") // We create the map so that it satisfies this.
+      PrimitiveConstructor<Key, B> constructor =
+          (PrimitiveConstructor<Key, B>) primitiveConstructors.get(keyPair);
+      if (constructor == null) {
+        throw new GeneralSecurityException(
+            "No PrimitiveConstructor registered for key class "
+                + key.getClass().getName()
+                + " and primitive "
+                + inputPrimitiveClass.getName());
+      }
+      return constructor.constructPrimitive(key);
+    }
+  }
+
+  /**
+   * A pair (inputPrimitiveClass, wrapFunction). The wrap function knows how to create primitives of
+   * type P given a primitive creator for type B.
+   */
+  private static final class WrapFunctionAndInputPrimitiveClass<B, P> {
+    private final Class<B> inputPrimitiveClass;
+    private final WrapFunction<B, P> wrapFunction;
+
+    private WrapFunctionAndInputPrimitiveClass(
+        Class<B> inputPrimitiveClass, WrapFunction<B, P> wrapFunction) {
+      this.inputPrimitiveClass = inputPrimitiveClass;
+      this.wrapFunction = wrapFunction;
+    }
   }
 
   private final Map<Class<? extends Parameters>, KeyCreator<?>> keyCreators = new HashMap<>();
+  private final Map<PrimitiveConstructorKey, PrimitiveConstructor<?, ?>> primitiveConstructors =
+      new HashMap<>();
+  private final Map<Class<?>, WrapFunctionAndInputPrimitiveClass<?, ?>> wrappers = new HashMap<>();
 
   public ProtoBasedConfigurationBuilder() {}
 
+  /** Add a method to create keys from the given parameters. */
   @CanIgnoreReturnValue
   public <P extends Parameters> ProtoBasedConfigurationBuilder addKeyCreator(
       Class<P> parametersClass, KeyCreator<P> creator) {
@@ -63,7 +177,43 @@ public final class ProtoBasedConfigurationBuilder {
     return this;
   }
 
+  /** Add a method to create (single key) primitives from a given key. */
+  @CanIgnoreReturnValue
+  public <KeyT extends Key, PrimitiveT> ProtoBasedConfigurationBuilder addPrimitiveConstructor(
+      PrimitiveConstructor.PrimitiveConstructionFunction<KeyT, PrimitiveT> constructionFunction,
+      Class<KeyT> keyClass,
+      Class<PrimitiveT> primitiveClass) {
+    PrimitiveConstructorKey key = new PrimitiveConstructorKey(keyClass, primitiveClass);
+    if (primitiveConstructors.containsKey(key)) {
+      throw new IllegalArgumentException(
+          "PrimitiveConstructor for "
+              + keyClass
+              + " and primitive "
+              + primitiveClass
+              + " already added.");
+    }
+    primitiveConstructors.put(
+        key, PrimitiveConstructor.create(constructionFunction, keyClass, primitiveClass));
+    return this;
+  }
+
+  /** Add a method to create a primitive from a keyset. */
+  @CanIgnoreReturnValue
+  public <InputPrimitiveT, WrapperPrimitiveT> ProtoBasedConfigurationBuilder addPrimitiveWrapper(
+      Class<WrapperPrimitiveT> primitiveClass,
+      Class<InputPrimitiveT> inputPrimitiveClass,
+      WrapFunction<InputPrimitiveT, WrapperPrimitiveT> wrapFunction) {
+    if (wrappers.containsKey(primitiveClass)) {
+      throw new IllegalArgumentException(
+          "PrimitiveWrapper for " + primitiveClass + " already added.");
+    }
+    wrappers.put(
+        primitiveClass,
+        new WrapFunctionAndInputPrimitiveClass<>(inputPrimitiveClass, wrapFunction));
+    return this;
+  }
+
   public Configuration build() {
-    return new InternalProtoBasedConfiguration(keyCreators);
+    return new InternalProtoBasedConfiguration(keyCreators, primitiveConstructors, wrappers);
   }
 }
--- src/test/java/com/google/crypto/tink/internal/ProtoBasedConfigurationBuilderTest.java
+++ src/test/java/com/google/crypto/tink/internal/ProtoBasedConfigurationBuilderTest.java
@@ -21,11 +21,22 @@ import static org.junit.Assert.assertThrows;
 
 import com.google.crypto.tink.Configuration;
 import com.google.crypto.tink.Key;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.Mac;
 import com.google.crypto.tink.Parameters;
 import com.google.crypto.tink.aead.ChaCha20Poly1305Key;
 import com.google.crypto.tink.aead.ChaCha20Poly1305Parameters;
 import com.google.crypto.tink.aead.XChaCha20Poly1305Key;
 import com.google.crypto.tink.aead.XChaCha20Poly1305Parameters;
+import com.google.crypto.tink.mac.AesCmacKey;
+import com.google.crypto.tink.mac.AesCmacParameters;
+import com.google.crypto.tink.mac.ChunkedMac;
+import com.google.crypto.tink.mac.HmacKey;
+import com.google.crypto.tink.mac.HmacParameters;
+import com.google.crypto.tink.mac.internal.ChunkedHmacImpl;
+import com.google.crypto.tink.mac.internal.WrappedChunkedMac;
+import com.google.crypto.tink.mac.internal.WrappedMac;
+import com.google.crypto.tink.subtle.PrfMac;
 import com.google.crypto.tink.util.SecretBytes;
 import java.security.GeneralSecurityException;
 import org.junit.Test;
@@ -113,5 +124,154 @@ public final class ProtoBasedConfigurationBuilderTest {
     assertThrows(
         GeneralSecurityException.class,
         () -> config.createKey(ChaCha20Poly1305Parameters.create(), null));
+  }
+
+  @Test
+  public void testCreatePrimitive_actualMac_works() throws Exception {
+    Configuration config =
+        new ProtoBasedConfigurationBuilder()
+            .addPrimitiveConstructor(PrfMac::create, HmacKey.class, Mac.class)
+            .addPrimitiveWrapper(Mac.class, Mac.class, WrappedMac::create)
+            .build();
+
+    HmacParameters parameters =
+        HmacParameters.builder()
+            .setTagSizeBytes(16)
+            .setKeySizeBytes(32)
+            .setHashType(HmacParameters.HashType.SHA256)
+            .setVariant(HmacParameters.Variant.NO_PREFIX)
+            .build();
+    HmacKey key =
+        HmacKey.builder()
+            .setParameters(parameters)
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
+            .build();
+
+    Mac mac = keysetHandle.getPrimitive(config, Mac.class);
+    assertThat(mac).isNotNull();
+
+    byte[] data = new byte[0];
+    byte[] tag = mac.computeMac(data);
+    mac.verifyMac(tag, data); // Should not throw
+  }
+
+  @Test
+  public void testRegisterPrimitiveConstructor_twice_throws() throws Exception {
+    ProtoBasedConfigurationBuilder builder =
+        new ProtoBasedConfigurationBuilder()
+            .addPrimitiveConstructor(PrfMac::create, HmacKey.class, Mac.class);
+
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> builder.addPrimitiveConstructor(PrfMac::create, HmacKey.class, Mac.class));
+  }
+
+  @Test
+  public void testRegisterPrimitiveWrapper_twice_throws() throws Exception {
+    ProtoBasedConfigurationBuilder builder =
+        new ProtoBasedConfigurationBuilder()
+            .addPrimitiveWrapper(Mac.class, Mac.class, WrappedMac::create);
+
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> builder.addPrimitiveWrapper(Mac.class, Mac.class, WrappedMac::create));
+  }
+
+  @Test
+  public void testCreatePrimitive_multiplePrimitives_works() throws Exception {
+    Configuration config =
+        new ProtoBasedConfigurationBuilder()
+            .addPrimitiveConstructor(PrfMac::create, HmacKey.class, Mac.class)
+            .addPrimitiveConstructor(ChunkedHmacImpl::new, HmacKey.class, ChunkedMac.class)
+            .addPrimitiveWrapper(Mac.class, Mac.class, WrappedMac::create)
+            .addPrimitiveWrapper(ChunkedMac.class, ChunkedMac.class, WrappedChunkedMac::create)
+            .build();
+
+    HmacParameters parameters =
+        HmacParameters.builder()
+            .setTagSizeBytes(16)
+            .setKeySizeBytes(32)
+            .setHashType(HmacParameters.HashType.SHA256)
+            .setVariant(HmacParameters.Variant.NO_PREFIX)
+            .build();
+    HmacKey key =
+        HmacKey.builder()
+            .setParameters(parameters)
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
+            .build();
+
+    // Verify we can get Mac
+    Mac mac = keysetHandle.getPrimitive(config, Mac.class);
+    assertThat(mac).isNotNull();
+    byte[] data = new byte[0];
+    byte[] tag = mac.computeMac(data);
+    mac.verifyMac(tag, data); // Should not throw
+
+    // Verify we can get ChunkedMac
+    ChunkedMac chunkedMac = keysetHandle.getPrimitive(config, ChunkedMac.class);
+    assertThat(chunkedMac).isNotNull();
+  }
+
+  @Test
+  public void testCreatePrimitive_multipleKeyTypes_works() throws Exception {
+    Configuration config =
+        new ProtoBasedConfigurationBuilder()
+            .addPrimitiveConstructor(PrfMac::create, HmacKey.class, Mac.class)
+            .addPrimitiveConstructor(PrfMac::create, AesCmacKey.class, Mac.class)
+            .addPrimitiveWrapper(Mac.class, Mac.class, WrappedMac::create)
+            .build();
+
+    // 1. Test with HmacKey
+    HmacParameters hmacParameters =
+        HmacParameters.builder()
+            .setTagSizeBytes(16)
+            .setKeySizeBytes(32)
+            .setHashType(HmacParameters.HashType.SHA256)
+            .setVariant(HmacParameters.Variant.NO_PREFIX)
+            .build();
+    HmacKey hmacKey =
+        HmacKey.builder()
+            .setParameters(hmacParameters)
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    KeysetHandle hmacKeysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(hmacKey).withRandomId().makePrimary())
+            .build();
+
+    Mac hmac = hmacKeysetHandle.getPrimitive(config, Mac.class);
+    assertThat(hmac).isNotNull();
+    byte[] tag1 = hmac.computeMac(new byte[0]);
+    hmac.verifyMac(tag1, new byte[0]);
+
+    // 2. Test with AesCmacKey
+    AesCmacParameters aesCmacParameters =
+        AesCmacParameters.builder()
+            .setKeySizeBytes(32)
+            .setTagSizeBytes(16)
+            .setVariant(AesCmacParameters.Variant.NO_PREFIX)
+            .build();
+    AesCmacKey aesCmacKey =
+        AesCmacKey.builder()
+            .setParameters(aesCmacParameters)
+            .setAesKeyBytes(SecretBytes.randomBytes(32))
+            .build();
+    KeysetHandle aesCmacKeysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(aesCmacKey).withRandomId().makePrimary())
+            .build();
+
+    Mac aesCmac = aesCmacKeysetHandle.getPrimitive(config, Mac.class);
+    assertThat(aesCmac).isNotNull();
+    byte[] tag2 = aesCmac.computeMac(new byte[0]);
+    aesCmac.verifyMac(tag2, new byte[0]);
   }
 }
