9eca1604c828a9ab30dab9f8f87655fb040968e5
Thomas Holenstein <tholenst@google.com>
2026-07-22 06:03:14 -0700

    Refactor DeterministicAeadConfig2026 to use ProtoBasedConfigurationBuilder.
    PiperOrigin-RevId: 952069593
    Change-Id: I972223f7c43fb7b17f1d5eb4b87716b488311cd9

--- src/main/java/com/google/crypto/tink/daead/DeterministicAeadConfig2026.java
+++ src/main/java/com/google/crypto/tink/daead/DeterministicAeadConfig2026.java
@@ -19,14 +19,12 @@ package com.google.crypto.tink.daead;
 import com.google.crypto.tink.AccessesPartialKey;
 import com.google.crypto.tink.Configuration;
 import com.google.crypto.tink.DeterministicAead;
-import com.google.crypto.tink.Key;
-import com.google.crypto.tink.KeysetHandleInterface;
-import com.google.crypto.tink.Parameters;
-import com.google.crypto.tink.ProtoKeySerializer;
+import com.google.crypto.tink.LowLevelCryptoCaller;
 import com.google.crypto.tink.config.internal.TinkFipsUtil;
-import com.google.crypto.tink.daead.internal.AesSivProtoSerialization;
-import com.google.crypto.tink.internal.SerializationRegistry;
-import com.google.crypto.tink.subtle.AesSiv;
+import com.google.crypto.tink.daead.internal.WrappedDeterministicAead;
+import com.google.crypto.tink.daead.subtle.AesSivDeterministicAead;
+import com.google.crypto.tink.daead.subtle.AesSivProtoSerialization;
+import com.google.crypto.tink.internal.ProtoBasedConfigurationBuilder;
 import com.google.crypto.tink.util.SecretBytes;
 import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
@@ -43,10 +41,7 @@ import javax.annotation.Nullable;
 public class DeterministicAeadConfig2026 {
   private DeterministicAeadConfig2026() {}
 
-  private static final DeterministicAeadWrapper DETERMINISTIC_AEAD_WRAPPER =
-      new DeterministicAeadWrapper();
   private static final Configuration CONFIGURATION = create();
-  private static final ProtoKeySerializer SERIALIZER = createProtoKeySerializer();
 
   /** Returns the {@link Configuration} instance. */
   public static Configuration get() throws GeneralSecurityException {
@@ -57,60 +52,46 @@ public class DeterministicAeadConfig2026 {
     return CONFIGURATION;
   }
 
+  private static final String AES_SIV_TYPE_URL =
+      "type.googleapis.com/google.crypto.tink.AesSivKey";
+
+  @LowLevelCryptoCaller
   private static Configuration create() {
-    return new Configuration() {
-      @Override
-      public <P> P createPrimitive(KeysetHandleInterface keysetHandle, Class<P> clazz)
-          throws GeneralSecurityException {
-        if (clazz.equals(DeterministicAead.class)) {
-          return clazz.cast(
-              DETERMINISTIC_AEAD_WRAPPER.wrap(
-                  keysetHandle, DeterministicAeadConfig2026::createDeterministicAead));
-        }
-        throw new GeneralSecurityException(
-            "DeterministicAeadConfig2026 can only create DeterministicAead primitive");
-      }
-
-      @Override
-      @AccessesPartialKey
-      public Key createKey(Parameters parameters, @Nullable Integer idRequirement)
-          throws GeneralSecurityException {
-        if (parameters instanceof AesSivParameters) {
-          AesSivParameters aesSivParameters = (AesSivParameters) parameters;
-          return AesSivKey.builder()
-              .setParameters(aesSivParameters)
-              .setKeyBytes(SecretBytes.randomBytes(aesSivParameters.getKeySizeBytes()))
-              .setIdRequirement(idRequirement)
-              .build();
-        }
-        throw new GeneralSecurityException(
-            "Unrecognized parameters for DeterministicAeadConfig2026: " + parameters);
-      }
-
-      @Override
-      public <P> P getOrNull(Class<P> clazz) {
-        if (clazz.equals(ProtoKeySerializer.class)) {
-          return clazz.cast(SERIALIZER);
-        }
-        return null;
-      }
-    };
+    return new ProtoBasedConfigurationBuilder()
+        .addPrimitiveWrapper(
+            DeterministicAead.class,
+            DeterministicAead.class,
+            WrappedDeterministicAead::create)
+        .addKeyCreator(AesSivParameters.class, DeterministicAeadConfig2026::createAesSivKey)
+        .addPrimitiveConstructor(
+            DeterministicAeadConfig2026::createAesSivPrimitive,
+            AesSivKey.class,
+            DeterministicAead.class)
+        .addKeySerializer(AesSivKey.class, AesSivProtoSerialization::serializeKey)
+        .addParametersSerializer(
+            AesSivParameters.class, AesSivProtoSerialization::serializeParameters)
+        .addKeyParser(AES_SIV_TYPE_URL, AesSivProtoSerialization::parseKey)
+        .addParametersParser(AES_SIV_TYPE_URL, AesSivProtoSerialization::parseParameters)
+        .build();
   }
 
-  private static DeterministicAead createDeterministicAead(KeysetHandleInterface.Entry entry)
+  @AccessesPartialKey
+  private static AesSivKey createAesSivKey(
+      AesSivParameters parameters, @Nullable Integer idRequirement)
       throws GeneralSecurityException {
-    Key key = entry.getKey();
-    if (key instanceof AesSivKey) {
-      return createAesSiv((AesSivKey) key);
-    }
-    throw new GeneralSecurityException("Unknown key class: " + key.getClass());
+    return AesSivKey.builder()
+        .setParameters(parameters)
+        .setKeyBytes(SecretBytes.randomBytes(parameters.getKeySizeBytes()))
+        .setIdRequirement(idRequirement)
+        .build();
   }
 
   // We only allow 64-byte keys for AesSiv, because 32-byte keys might not provide 128-bit security
   // level in multi-user setting.
   private static final int KEY_SIZE_IN_BYTES = 64;
 
-  private static DeterministicAead createAesSiv(AesSivKey key)
+  @LowLevelCryptoCaller
+  private static DeterministicAead createAesSivPrimitive(AesSivKey key)
       throws GeneralSecurityException {
     if (key.getParameters().getKeySizeBytes() != KEY_SIZE_IN_BYTES) {
       throw new InvalidAlgorithmParameterException(
@@ -120,16 +101,6 @@ public class DeterministicAeadConfig2026 {
               + KEY_SIZE_IN_BYTES
               + " bytes.");
     }
-    return AesSiv.create(key);
-  }
-
-  private static ProtoKeySerializer createProtoKeySerializer() {
-    try {
-      SerializationRegistry.Builder builder = new SerializationRegistry.Builder();
-      AesSivProtoSerialization.register(builder);
-      return builder.build();
-    } catch (GeneralSecurityException e) {
-      throw new IllegalStateException(e);
-    }
+    return AesSivDeterministicAead.create(key);
   }
 }
