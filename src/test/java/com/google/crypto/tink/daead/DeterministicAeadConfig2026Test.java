1423887709cd537e79bffe1d531bdf5bf3c25d01
Thomas Holenstein <tholenst@google.com>
2026-07-22 08:44:14 -0700

    Refactor DeterministicAeadConfig2026Test using JUnit Theories.
    PiperOrigin-RevId: 952143675
    Change-Id: Idde8a0d81fed46fda14c5d9c703a6bd1da8c58bf

--- src/test/java/com/google/crypto/tink/daead/DeterministicAeadConfig2026Test.java
+++ src/test/java/com/google/crypto/tink/daead/DeterministicAeadConfig2026Test.java
@@ -17,6 +17,7 @@
 package com.google.crypto.tink.daead;
 
 import static com.google.common.truth.Truth.assertThat;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertThrows;
 
 import com.google.crypto.tink.Configuration;
@@ -25,199 +26,225 @@ import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.Key;
 import com.google.crypto.tink.KeysetHandle;
 import com.google.crypto.tink.Parameters;
-import com.google.crypto.tink.ProtoKeySerialization;
-import com.google.crypto.tink.ProtoKeySerializer;
-import com.google.crypto.tink.ProtoParametersSerialization;
+import com.google.crypto.tink.TinkProtoKeysetFormat;
+import com.google.crypto.tink.TinkProtoParametersFormat;
 import com.google.crypto.tink.config.internal.TinkFipsUtil;
-import com.google.crypto.tink.daead.AesSivParameters.Variant;
-import com.google.crypto.tink.daead.internal.AesSivProtoSerialization;
 import com.google.crypto.tink.util.SecretBytes;
 import java.security.GeneralSecurityException;
 import org.junit.Assume;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+/** Tests for {@link DeterministicAeadConfig2026}. * */
+@RunWith(Theories.class)
 public class DeterministicAeadConfig2026Test {
-  @Test
-  public void config_throwsIfInFipsMode() throws Exception {
-    Assume.assumeTrue(TinkFipsUtil.useOnlyFips());
+  /**
+   * A list of Keys which behaves common for this config. For these keys we can
+   *
+   * <ul>
+   *   <li>create primitives
+   *   <li>create new keys with the same parameters
+   *   <li>serialize and parse the keys
+   *   <li>serialize and parse the parameters.
+   */
+  @DataPoints public static final Key[] keys = createKeys();
 
-    assertThrows(GeneralSecurityException.class, DeterministicAeadConfig2026::get);
-  }
-
-  @Test
-  public void config_containsAesSivForDeterministicAead() throws Exception {
-    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
-
-    AesSivProtoSerialization.register();
-    AesSivParameters parameters =
-        AesSivParameters.builder().setKeySizeBytes(64).setVariant(Variant.NO_PREFIX).build();
-    AesSivKey key =
+  private static Key[] createKeys() {
+    try {
+      return new Key[] {
         AesSivKey.builder()
-            .setParameters(parameters)
+            .setParameters(
+                AesSivParameters.builder()
+                    .setKeySizeBytes(64)
+                    .setVariant(AesSivParameters.Variant.TINK)
+                    .build())
             .setKeyBytes(SecretBytes.randomBytes(64))
-            .build();
-    KeysetHandle keysetHandle =
-        KeysetHandle.newBuilder()
-            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
-            .build();
-
-    assertThat(
-            keysetHandle.getPrimitive(
-                DeterministicAeadConfig2026.get(), DeterministicAead.class))
-        .isNotNull();
+            .setIdRequirement(1234)
+            .build(),
+        AesSivKey.builder()
+            .setParameters(
+                AesSivParameters.builder()
+                    .setKeySizeBytes(64)
+                    .setVariant(AesSivParameters.Variant.NO_PREFIX)
+                    .build())
+            .setKeyBytes(SecretBytes.randomBytes(64))
+            .build(),
+      };
+    } catch (GeneralSecurityException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  @Test
-  public void config_disallowsNon64ByteKeyForAesSiv() throws Exception {
-    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+  @Theory
+  public void createKey_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      // Skip this if we are fips only (Theory doesn't allow Assume here).
+      return;
+    }
+    Key createdKey =
+        DeterministicAeadConfig2026.get()
+            .createKey(key.getParameters(), key.getIdRequirementOrNull());
 
-    AesSivProtoSerialization.register();
-    AesSivParameters parameters =
-        AesSivParameters.builder().setKeySizeBytes(32).setVariant(Variant.NO_PREFIX).build();
-    AesSivKey key =
+    assertThat(createdKey.getParameters()).isEqualTo(key.getParameters());
+    assertThat(createdKey.getIdRequirementOrNull()).isEqualTo(key.getIdRequirementOrNull());
+  }
+
+  @Theory
+  public void serializeAndParseKey_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      // Skip this if we are fips only (Theory doesn't allow Assume here).
+      return;
+    }
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    Configuration config = DeterministicAeadConfig2026.get();
+    byte[] serialized =
+        TinkProtoKeysetFormat.serializeKeyset(keysetHandle, InsecureSecretKeyAccess.get(), config);
+    KeysetHandle parsed =
+        TinkProtoKeysetFormat.parseKeyset(serialized, InsecureSecretKeyAccess.get(), config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
+  }
+
+  @Theory
+  public void serializeAndParseParameters_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      // Skip this if we are fips only (Theory doesn't allow Assume here).
+      return;
+    }
+    Parameters parameters = key.getParameters();
+    Configuration config = DeterministicAeadConfig2026.get();
+    byte[] serialized = TinkProtoParametersFormat.serialize(parameters, config);
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, config);
+
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Theory
+  public void getPrimitive_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      // Skip this if we are fips only (Theory doesn't allow Assume here).
+      return;
+    }
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    DeterministicAead daead =
+        keysetHandle.getPrimitive(DeterministicAeadConfig2026.get(), DeterministicAead.class);
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+    byte[] ciphertext = daead.encryptDeterministically(plaintext, associatedData);
+    byte[] decrypted = daead.decryptDeterministically(ciphertext, associatedData);
+
+    assertThat(decrypted).isEqualTo(plaintext);
+  }
+
+  // The remaining tests are specialized for certain keys only. For example, for DeterministicAead
+  // we don't allow GetPrimitive for 32 byte keys (though we still allow creating and
+  // parsing/serializing them). We tests these in single tests without @Theory.
+  @Test
+  public void smallKey_getPrimitive_throws() throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    Key keyWithSize32 =
         AesSivKey.builder()
-            .setParameters(parameters)
+            .setParameters(
+                AesSivParameters.builder()
+                    .setKeySizeBytes(32)
+                    .setVariant(AesSivParameters.Variant.TINK)
+                    .build())
             .setKeyBytes(SecretBytes.randomBytes(32))
+            .setIdRequirement(1234)
             .build();
-    KeysetHandle keysetHandle =
-        KeysetHandle.newBuilder()
-            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
-            .build();
-
-    Configuration config = DeterministicAeadConfig2026.get();
+    KeysetHandle.Builder.Entry entry =
+        KeysetHandle.importKey(keyWithSize32).makePrimary().withFixedId(1234);
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+    Configuration configuration = DeterministicAeadConfig2026.get();
     assertThrows(
         GeneralSecurityException.class,
-        () -> keysetHandle.getPrimitive(config, DeterministicAead.class));
+        () -> keysetHandle.getPrimitive(configuration, DeterministicAead.class));
   }
 
   @Test
-  public void createKey_aesSivParameters() throws Exception {
-    AesSivParameters parameters =
-        AesSivParameters.builder()
-            .setKeySizeBytes(64)
-            .setVariant(AesSivParameters.Variant.TINK)
-            .build();
-    Key key = DeterministicAeadConfig2026.get().createKey(parameters, 42);
-    assertThat(key).isInstanceOf(AesSivKey.class);
-    AesSivKey aesSivKey = (AesSivKey) key;
-    assertThat(aesSivKey.getParameters()).isEqualTo(parameters);
-    assertThat(aesSivKey.getIdRequirementOrNull()).isEqualTo(42);
-  }
-
-  @Test
-  public void createKey_unrecognizedParameters_throws() throws Exception {
-    Parameters parameters =
-        new Parameters() {
-          @Override
-          public boolean hasIdRequirement() {
-            return false;
-          }
-        };
-    Configuration config = DeterministicAeadConfig2026.get();
-    assertThrows(GeneralSecurityException.class, () -> config.createKey(parameters, null));
-  }
-
-  private static interface DummyPrimitive {}
-
-  @Test
-  public void createPrimitive_unsupportedPrimitiveClass_throws() throws Exception {
+  public void smallKey_parseAndSerializeKey_works() throws Exception {
     Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
-
-    AesSivProtoSerialization.register();
-    AesSivParameters parameters =
-        AesSivParameters.builder()
-            .setKeySizeBytes(64)
-            .setVariant(AesSivParameters.Variant.NO_PREFIX)
-            .build();
-    AesSivKey key =
+    Key keyWithSize32 =
         AesSivKey.builder()
-            .setParameters(parameters)
-            .setKeyBytes(SecretBytes.randomBytes(64))
+            .setParameters(
+                AesSivParameters.builder()
+                    .setKeySizeBytes(32)
+                    .setVariant(AesSivParameters.Variant.TINK)
+                    .build())
+            .setKeyBytes(SecretBytes.randomBytes(32))
+            .setIdRequirement(1234)
             .build();
-    KeysetHandle keysetHandle =
-        KeysetHandle.newBuilder()
-            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
-            .build();
-
+    KeysetHandle.Builder.Entry entry =
+        KeysetHandle.importKey(keyWithSize32).makePrimary().withFixedId(1234);
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
     Configuration config = DeterministicAeadConfig2026.get();
-    assertThrows(
-        GeneralSecurityException.class,
-        () -> keysetHandle.getPrimitive(config, DummyPrimitive.class));
+    byte[] serialized =
+        TinkProtoKeysetFormat.serializeKeyset(keysetHandle, InsecureSecretKeyAccess.get(), config);
+    KeysetHandle parsed =
+        TinkProtoKeysetFormat.parseKeyset(serialized, InsecureSecretKeyAccess.get(), config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
   }
 
   @Test
-  public void createKey_aesSivParametersWithoutIdRequirement_works() throws Exception {
-    AesSivParameters parameters =
+  public void smallParameters_parseAndSerializeKey_works() throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    Parameters parametersWithSize32 =
         AesSivParameters.builder()
-            .setKeySizeBytes(64)
-            .setVariant(AesSivParameters.Variant.NO_PREFIX)
-            .build();
-    Key key = DeterministicAeadConfig2026.get().createKey(parameters, null);
-    assertThat(key).isInstanceOf(AesSivKey.class);
-    AesSivKey aesSivKey = (AesSivKey) key;
-    assertThat(aesSivKey.getParameters()).isEqualTo(parameters);
-    assertThat(aesSivKey.getIdRequirementOrNull()).isNull();
-  }
-
-  @Test
-  public void createKey_aesSivParametersWithIdRequirementButPassedNull_throws() throws Exception {
-    AesSivParameters parameters =
-        AesSivParameters.builder()
-            .setKeySizeBytes(64)
+            .setKeySizeBytes(32)
             .setVariant(AesSivParameters.Variant.TINK)
             .build();
+
     Configuration config = DeterministicAeadConfig2026.get();
-    assertThrows(GeneralSecurityException.class, () -> config.createKey(parameters, null));
+    byte[] serialized = TinkProtoParametersFormat.serialize(parametersWithSize32, config);
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, config);
+
+    assertThat(parsed).isEqualTo(parametersWithSize32);
   }
 
   @Test
-  public void getOrNull_protoKeySerializer_serializeAndParseParameters() throws Exception {
-    ProtoKeySerializer serializer =
-        DeterministicAeadConfig2026.get().getOrNull(ProtoKeySerializer.class);
-    assertThat(serializer).isNotNull();
+  public void smallKey_createNewKey_works() throws Exception {
+    Assume.assumeFalse(TinkFipsUtil.useOnlyFips());
+    Key createdKey =
+        DeterministicAeadConfig2026.get()
+            .createKey(
+                AesSivParameters.builder()
+                    .setKeySizeBytes(32)
+                    .setVariant(AesSivParameters.Variant.TINK)
+                    .build(),
+                1234);
 
-    AesSivParameters parameters =
-        AesSivParameters.builder()
-            .setKeySizeBytes(64)
-            .setVariant(AesSivParameters.Variant.NO_PREFIX)
-            .build();
-
-    ProtoParametersSerialization serializedParameters =
-        serializer.serializeParameters(parameters);
-    assertThat(serializer.parseParameters(serializedParameters)).isEqualTo(parameters);
+    assertThat(createdKey.getParameters())
+        .isEqualTo(
+            AesSivParameters.builder()
+                .setKeySizeBytes(32)
+                .setVariant(AesSivParameters.Variant.TINK)
+                .build());
+    assertThat(createdKey.getIdRequirementOrNull()).isEqualTo(1234);
   }
 
   @Test
-  public void getOrNull_protoKeySerializer_serializeAndParseKey() throws Exception {
-    ProtoKeySerializer serializer =
-        DeterministicAeadConfig2026.get().getOrNull(ProtoKeySerializer.class);
-    assertThat(serializer).isNotNull();
-
-    AesSivParameters parameters =
-        AesSivParameters.builder()
-            .setKeySizeBytes(64)
-            .setVariant(AesSivParameters.Variant.NO_PREFIX)
-            .build();
-    AesSivKey key =
-        AesSivKey.builder()
-            .setParameters(parameters)
-            .setKeyBytes(SecretBytes.randomBytes(64))
-            .build();
-
-    ProtoKeySerialization serializedKey =
-        serializer.serializeKey(key, InsecureSecretKeyAccess.get());
-    assertThat(
-            serializer
-                .parseKey(serializedKey, InsecureSecretKeyAccess.get())
-                .equalsKey(key))
-        .isTrue();
-  }
-
-  @Test
-  public void getOrNull_unsupportedClass_returnsNull() throws Exception {
-    assertThat(DeterministicAeadConfig2026.get().getOrNull(String.class)).isNull();
+  public void getThrowsIfFips() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      assertThrows(GeneralSecurityException.class, () -> DeterministicAeadConfig2026.get());
+    }
   }
 }
+
