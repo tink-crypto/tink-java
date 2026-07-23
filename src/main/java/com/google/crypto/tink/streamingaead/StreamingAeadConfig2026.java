73d8311579d0fcb02614e1be294d6e29b26b9d4e
Thomas Holenstein <tholenst@google.com>
2026-07-23 07:49:29 -0700

    Add StreamingAeadConfig2026.
    PiperOrigin-RevId: 952757009
    Change-Id: Ib85351c933b37400c382d411135c6078d265a9cc

---
+++ src/main/java/com/google/crypto/tink/streamingaead/StreamingAeadConfig2026.java
@@ -0,0 +1,120 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.streamingaead;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.LowLevelCryptoCaller;
+import com.google.crypto.tink.StreamingAead;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.ProtoBasedConfigurationBuilder;
+import com.google.crypto.tink.streamingaead.internal.WrappedStreamingAead;
+import com.google.crypto.tink.streamingaead.subtle.AesCtrHmacStreamingAead;
+import com.google.crypto.tink.streamingaead.subtle.AesCtrHmacStreamingProtoSerialization;
+import com.google.crypto.tink.streamingaead.subtle.AesGcmHkdfStreamingAead;
+import com.google.crypto.tink.streamingaead.subtle.AesGcmHkdfStreamingProtoSerialization;
+import com.google.crypto.tink.util.SecretBytes;
+import java.security.GeneralSecurityException;
+import javax.annotation.Nullable;
+
+/**
+ * StreamingAeadConfig2026 contains the following primitives and algorithms for {@link
+ * StreamingAead}:
+ *
+ * <ul>
+ *   <li>AesCtrHmacStreaming
+ *   <li>AesGcmHkdfStreaming
+ * </ul>
+ */
+public class StreamingAeadConfig2026 {
+  private StreamingAeadConfig2026() {}
+
+  private static final Configuration CONFIGURATION = create();
+
+  /** Returns the {@link Configuration} instance. */
+  public static Configuration get() throws GeneralSecurityException {
+    if (TinkFipsUtil.useOnlyFips()) {
+      throw new GeneralSecurityException(
+          "Cannot use non-FIPS-compliant StreamingAeadConfig2026 in FIPS mode");
+    }
+    return CONFIGURATION;
+  }
+
+  private static final String AES_CTR_HMAC_STREAMING_TYPE_URL =
+      "type.googleapis.com/google.crypto.tink.AesCtrHmacStreamingKey";
+  private static final String AES_GCM_HKDF_STREAMING_TYPE_URL =
+      "type.googleapis.com/google.crypto.tink.AesGcmHkdfStreamingKey";
+
+  @LowLevelCryptoCaller
+  private static Configuration create() {
+    return new ProtoBasedConfigurationBuilder()
+        .addPrimitiveWrapper(StreamingAead.class, StreamingAead.class, WrappedStreamingAead::wrap)
+        // AesCtrHmacStreaming
+        .addKeyCreator(
+            AesCtrHmacStreamingParameters.class,
+            StreamingAeadConfig2026::createAesCtrHmacStreamingKey)
+        .addPrimitiveConstructor(
+            AesCtrHmacStreamingAead::create, AesCtrHmacStreamingKey.class, StreamingAead.class)
+        .addKeySerializer(
+            AesCtrHmacStreamingKey.class, AesCtrHmacStreamingProtoSerialization::serializeKey)
+        .addParametersSerializer(
+            AesCtrHmacStreamingParameters.class,
+            AesCtrHmacStreamingProtoSerialization::serializeParameters)
+        .addKeyParser(
+            AES_CTR_HMAC_STREAMING_TYPE_URL, AesCtrHmacStreamingProtoSerialization::parseKey)
+        .addParametersParser(
+            AES_CTR_HMAC_STREAMING_TYPE_URL, AesCtrHmacStreamingProtoSerialization::parseParameters)
+        // AesGcmHkdfStreaming
+        .addKeyCreator(
+            AesGcmHkdfStreamingParameters.class,
+            StreamingAeadConfig2026::createAesGcmHkdfStreamingKey)
+        .addPrimitiveConstructor(
+            AesGcmHkdfStreamingAead::create, AesGcmHkdfStreamingKey.class, StreamingAead.class)
+        .addKeySerializer(
+            AesGcmHkdfStreamingKey.class, AesGcmHkdfStreamingProtoSerialization::serializeKey)
+        .addParametersSerializer(
+            AesGcmHkdfStreamingParameters.class,
+            AesGcmHkdfStreamingProtoSerialization::serializeParameters)
+        .addKeyParser(
+            AES_GCM_HKDF_STREAMING_TYPE_URL, AesGcmHkdfStreamingProtoSerialization::parseKey)
+        .addParametersParser(
+            AES_GCM_HKDF_STREAMING_TYPE_URL, AesGcmHkdfStreamingProtoSerialization::parseParameters)
+        .build();
+  }
+
+  @AccessesPartialKey
+  private static AesCtrHmacStreamingKey createAesCtrHmacStreamingKey(
+      AesCtrHmacStreamingParameters parameters, @Nullable Integer idRequirement)
+      throws GeneralSecurityException {
+    if (idRequirement != null) {
+      throw new GeneralSecurityException("idRequirement must be null");
+    }
+    return AesCtrHmacStreamingKey.create(
+        parameters, SecretBytes.randomBytes(parameters.getKeySizeBytes()));
+  }
+
+  @AccessesPartialKey
+  private static AesGcmHkdfStreamingKey createAesGcmHkdfStreamingKey(
+      AesGcmHkdfStreamingParameters parameters, @Nullable Integer idRequirement)
+      throws GeneralSecurityException {
+    if (idRequirement != null) {
+      throw new GeneralSecurityException("idRequirement must be null");
+    }
+    return AesGcmHkdfStreamingKey.create(
+        parameters, SecretBytes.randomBytes(parameters.getKeySizeBytes()));
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/streamingaead/StreamingAeadConfig2026Test.java
@@ -0,0 +1,203 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.streamingaead;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.Parameters;
+import com.google.crypto.tink.StreamingAead;
+import com.google.crypto.tink.TinkProtoKeysetFormat;
+import com.google.crypto.tink.TinkProtoParametersFormat;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.util.SecretBytes;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.security.GeneralSecurityException;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/** Tests for {@link StreamingAeadConfig2026}. */
+@RunWith(Theories.class)
+public class StreamingAeadConfig2026Test {
+  /**
+   * A list of Keys which behave common for this config. For these keys we can
+   *
+   * <ul>
+   *   <li>create primitives
+   *   <li>create new keys with the same parameters
+   *   <li>serialize and parse the keys
+   *   <li>serialize and parse the parameters.
+   * </ul>
+   */
+  @DataPoints public static final Key[] keys = createKeys();
+
+  private static Key[] createKeys() {
+    try {
+      return new Key[] {
+        AesCtrHmacStreamingKey.create(
+            AesCtrHmacStreamingParameters.builder()
+                .setKeySizeBytes(48)
+                .setDerivedKeySizeBytes(32)
+                .setHkdfHashType(AesCtrHmacStreamingParameters.HashType.SHA256)
+                .setHmacHashType(AesCtrHmacStreamingParameters.HashType.SHA256)
+                .setHmacTagSizeBytes(16)
+                .setCiphertextSegmentSizeBytes(60)
+                .build(),
+            SecretBytes.randomBytes(48)),
+        AesGcmHkdfStreamingKey.create(
+            AesGcmHkdfStreamingParameters.builder()
+                .setKeySizeBytes(32)
+                .setDerivedAesGcmKeySizeBytes(32)
+                .setCiphertextSegmentSizeBytes(100)
+                .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA256)
+                .build(),
+            SecretBytes.randomBytes(32)),
+      };
+    } catch (GeneralSecurityException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Theory
+  public void createKey_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    Key createdKey =
+        StreamingAeadConfig2026.get().createKey(key.getParameters(), key.getIdRequirementOrNull());
+
+    assertThat(createdKey.getParameters()).isEqualTo(key.getParameters());
+    assertThat(createdKey.getIdRequirementOrNull()).isEqualTo(key.getIdRequirementOrNull());
+  }
+
+  @Theory
+  public void serializeAndParseKey_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    Configuration config = StreamingAeadConfig2026.get();
+    byte[] serialized =
+        TinkProtoKeysetFormat.serializeKeyset(keysetHandle, InsecureSecretKeyAccess.get(), config);
+    KeysetHandle parsed =
+        TinkProtoKeysetFormat.parseKeyset(serialized, InsecureSecretKeyAccess.get(), config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
+  }
+
+  @Theory
+  public void serializeAndParseParameters_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    Parameters parameters = key.getParameters();
+    Configuration config = StreamingAeadConfig2026.get();
+    byte[] serialized = TinkProtoParametersFormat.serialize(parameters, config);
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, config);
+
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Theory
+  public void getPrimitive_works(Key key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    StreamingAead streamingAead =
+        keysetHandle.getPrimitive(StreamingAeadConfig2026.get(), StreamingAead.class);
+
+    byte[] plaintext = "plaintext".getBytes(UTF_8);
+    byte[] associatedData = "associatedData".getBytes(UTF_8);
+
+    ByteArrayOutputStream ciphertextStream = new ByteArrayOutputStream();
+    try (WritableByteChannel encryptingChannel =
+        streamingAead.newEncryptingChannel(Channels.newChannel(ciphertextStream), associatedData)) {
+      ByteBuffer buffer = ByteBuffer.wrap(plaintext);
+      while (buffer.hasRemaining()) {
+        encryptingChannel.write(buffer);
+      }
+    }
+
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(ciphertextStream.toByteArray());
+    ByteArrayOutputStream decryptedStream = new ByteArrayOutputStream();
+    try (ReadableByteChannel decryptingChannel =
+        streamingAead.newDecryptingChannel(Channels.newChannel(inputStream), associatedData)) {
+      ByteBuffer buffer = ByteBuffer.allocate(100);
+      int read;
+      while ((read = decryptingChannel.read(buffer)) != -1) {
+        buffer.flip();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        decryptedStream.write(bytes);
+        buffer.clear();
+      }
+    }
+
+    assertThat(decryptedStream.toByteArray()).isEqualTo(plaintext);
+  }
+
+  @Test
+  public void createKey_withNonNullIdRequirement_throws() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    AesGcmHkdfStreamingParameters parameters =
+        AesGcmHkdfStreamingParameters.builder()
+            .setKeySizeBytes(32)
+            .setDerivedAesGcmKeySizeBytes(32)
+            .setCiphertextSegmentSizeBytes(100)
+            .setHkdfHashType(AesGcmHkdfStreamingParameters.HashType.SHA256)
+            .build();
+    Configuration config = StreamingAeadConfig2026.get();
+    assertThrows(GeneralSecurityException.class, () -> config.createKey(parameters, 1234));
+  }
+
+  @Test
+  public void get_throwsInFipsMode() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      assertThrows(GeneralSecurityException.class, StreamingAeadConfig2026::get);
+    }
+  }
+}
