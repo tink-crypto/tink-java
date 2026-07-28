186498420ca893ae7dfa81d7244146d0aed236d4
Thomas Holenstein <tholenst@google.com>
2026-07-28 02:15:36 -0700

    Create JwtConfig2026 for Tink Java.
    PiperOrigin-RevId: 955126922
    Change-Id: I1dbfee09d3d7a4ca68cb85e52503cd5858f58fb2

---
+++ src/main/java/com/google/crypto/tink/jwt/JwtConfig2026.java
@@ -0,0 +1,160 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.jwt;
+
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.LowLevelCryptoCaller;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.internal.ProtoBasedConfigurationBuilder;
+import com.google.crypto.tink.jwt.subtle.JwtEcdsaProtoSerialization;
+import com.google.crypto.tink.jwt.subtle.JwtEcdsaPublicKeySign;
+import com.google.crypto.tink.jwt.subtle.JwtEcdsaPublicKeyVerify;
+import com.google.crypto.tink.jwt.subtle.JwtHmac;
+import com.google.crypto.tink.jwt.subtle.JwtHmacProtoSerialization;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPkcs1ProtoSerialization;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPkcs1PublicKeySign;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPkcs1PublicKeyVerify;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPssProtoSerialization;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPssPublicKeySign;
+import com.google.crypto.tink.jwt.subtle.JwtRsaSsaPssPublicKeyVerify;
+import java.security.GeneralSecurityException;
+
+/**
+ * JwtConfig2026 contains the following primitives and algorithms for JWT:
+ *
+ * <ul>
+ *   <li>JwtMac
+ *       <ul>
+ *         <li>JwtHmac
+ *       </ul>
+ *   <li>JwtPublicKeySign / JwtPublicKeyVerify
+ *       <ul>
+ *         <li>JwtEcdsa
+ *         <li>JwtRsaSsaPkcs1
+ *         <li>JwtRsaSsaPss
+ *       </ul>
+ * </ul>
+ */
+public class JwtConfig2026 {
+
+  private JwtConfig2026() {}
+
+  private static final Configuration CONFIG = buildConfiguration();
+
+  @LowLevelCryptoCaller
+  private static Configuration buildConfiguration() {
+    return new ProtoBasedConfigurationBuilder()
+        // =========== JwtMac
+        .addPrimitiveWrapper(
+            JwtMac.class,
+            JwtMac.class,
+            (handle, factory) -> new JwtMacWrapper().wrap(handle, factory))
+        // JwtHmacMac
+        .addPrimitiveConstructor(JwtHmac::create, JwtHmacKey.class, JwtMac.class)
+        .addKeySerializer(JwtHmacKey.class, JwtHmacProtoSerialization::serializeKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtHmacKey",
+            JwtHmacProtoSerialization::parseKey)
+        .addParametersSerializer(
+            JwtHmacParameters.class, JwtHmacProtoSerialization::serializeParameters)
+        .addParametersParser(
+            "type.googleapis.com/google.crypto.tink.JwtHmacKey",
+            JwtHmacProtoSerialization::parseParameters)
+        // =========== JwtPublicKeySign & JwtPublicKeyVerify
+        .addPrimitiveWrapper(
+            JwtPublicKeySign.class,
+            JwtPublicKeySign.class,
+            (handle, factory) -> new JwtPublicKeySignWrapper().wrap(handle, factory))
+        .addPrimitiveWrapper(
+            JwtPublicKeyVerify.class,
+            JwtPublicKeyVerify.class,
+            (handle, factory) -> new JwtPublicKeyVerifyWrapper().wrap(handle, factory))
+        // JwtEcdsa
+        .addPrimitiveConstructor(
+            JwtEcdsaPublicKeySign::create, JwtEcdsaPrivateKey.class, JwtPublicKeySign.class)
+        .addPrimitiveConstructor(
+            JwtEcdsaPublicKeyVerify::create, JwtEcdsaPublicKey.class, JwtPublicKeyVerify.class)
+        .addKeySerializer(JwtEcdsaPrivateKey.class, JwtEcdsaProtoSerialization::serializePrivateKey)
+        .addKeySerializer(JwtEcdsaPublicKey.class, JwtEcdsaProtoSerialization::serializePublicKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtEcdsaPrivateKey",
+            JwtEcdsaProtoSerialization::parsePrivateKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtEcdsaPublicKey",
+            JwtEcdsaProtoSerialization::parsePublicKey)
+        .addParametersSerializer(
+            JwtEcdsaParameters.class, JwtEcdsaProtoSerialization::serializeParameters)
+        .addParametersParser(
+            "type.googleapis.com/google.crypto.tink.JwtEcdsaPrivateKey",
+            JwtEcdsaProtoSerialization::parseParameters)
+        // JwtRsaSsaPkcs1
+        .addPrimitiveConstructor(
+            JwtRsaSsaPkcs1PublicKeySign::create,
+            JwtRsaSsaPkcs1PrivateKey.class,
+            JwtPublicKeySign.class)
+        .addPrimitiveConstructor(
+            JwtRsaSsaPkcs1PublicKeyVerify::create,
+            JwtRsaSsaPkcs1PublicKey.class,
+            JwtPublicKeyVerify.class)
+        .addKeySerializer(
+            JwtRsaSsaPkcs1PrivateKey.class, JwtRsaSsaPkcs1ProtoSerialization::serializePrivateKey)
+        .addKeySerializer(
+            JwtRsaSsaPkcs1PublicKey.class, JwtRsaSsaPkcs1ProtoSerialization::serializePublicKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPkcs1PrivateKey",
+            JwtRsaSsaPkcs1ProtoSerialization::parsePrivateKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPkcs1PublicKey",
+            JwtRsaSsaPkcs1ProtoSerialization::parsePublicKey)
+        .addParametersSerializer(
+            JwtRsaSsaPkcs1Parameters.class, JwtRsaSsaPkcs1ProtoSerialization::serializeParameters)
+        .addParametersParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPkcs1PrivateKey",
+            JwtRsaSsaPkcs1ProtoSerialization::parseParameters)
+        // JwtRsaSsaPss
+        .addPrimitiveConstructor(
+            JwtRsaSsaPssPublicKeySign::create, JwtRsaSsaPssPrivateKey.class, JwtPublicKeySign.class)
+        .addPrimitiveConstructor(
+            JwtRsaSsaPssPublicKeyVerify::create,
+            JwtRsaSsaPssPublicKey.class,
+            JwtPublicKeyVerify.class)
+        .addKeySerializer(
+            JwtRsaSsaPssPrivateKey.class, JwtRsaSsaPssProtoSerialization::serializePrivateKey)
+        .addKeySerializer(
+            JwtRsaSsaPssPublicKey.class, JwtRsaSsaPssProtoSerialization::serializePublicKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPssPrivateKey",
+            JwtRsaSsaPssProtoSerialization::parsePrivateKey)
+        .addKeyParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPssPublicKey",
+            JwtRsaSsaPssProtoSerialization::parsePublicKey)
+        .addParametersSerializer(
+            JwtRsaSsaPssParameters.class, JwtRsaSsaPssProtoSerialization::serializeParameters)
+        .addParametersParser(
+            "type.googleapis.com/google.crypto.tink.JwtRsaSsaPssPrivateKey",
+            JwtRsaSsaPssProtoSerialization::parseParameters)
+        .build();
+  }
+
+  public static Configuration get() throws GeneralSecurityException {
+    if (TinkFipsUtil.useOnlyFips()) {
+      throw new GeneralSecurityException(
+          "Cannot use non-FIPS-compliant JwtConfig2026 in FIPS mode");
+    }
+    return CONFIG;
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/jwt/JwtConfig2026Test.java
@@ -0,0 +1,395 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.jwt;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.Parameters;
+import com.google.crypto.tink.TinkProtoKeysetFormat;
+import com.google.crypto.tink.TinkProtoParametersFormat;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.signature.RsaSsaPkcs1PrivateKey;
+import com.google.crypto.tink.signature.RsaSsaPssPrivateKey;
+import com.google.crypto.tink.subtle.Base64;
+import com.google.crypto.tink.testing.TestUtil;
+import com.google.crypto.tink.util.SecretBigInteger;
+import com.google.crypto.tink.util.SecretBytes;
+import java.math.BigInteger;
+import java.security.GeneralSecurityException;
+import java.security.spec.ECPoint;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+/** Tests for {@link JwtConfig2026}. */
+@RunWith(Theories.class)
+public class JwtConfig2026Test {
+
+  private static final ECPoint P256_PUBLIC_POINT =
+      new ECPoint(
+          new BigInteger("60FED4BA255A9D31C961EB74C6356D68C049B8923B61FA6CE669622E60F29FB6", 16),
+          new BigInteger("7903FE1008B8BC99A41AE9E95628BC64F2F1B20C2D7E9F5177A3C294D4462299", 16));
+  private static final BigInteger P256_PRIVATE_VALUE =
+      new BigInteger("C9AFA9D845BA75166B5C215767B1D6934E50C3DB36E89B127B8A622B120F6721", 16);
+
+  private static final BigInteger MODULUS =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "ofgWCuLjybRlzo0tZWJjNiuSfb4p4fAkd_wWJcyQoTbji9k0l8W26mPddx"
+                  + "HmfHQp-Vaw-4qPCJrcS2mJPMEzP1Pt0Bm4d4QlL-yRT-SFd2lZS-pCgNMs"
+                  + "D1W_YpRPEwOWvG6b32690r2jZ47soMZo9wGzjb_7OMg0LOL-bSf63kpaSH"
+                  + "SXndS5z5rexMdbBYUsLA9e-KXBdQOS-UTo7WTBEMa2R2CapHg665xsmtdV"
+                  + "MTBQY4uDZlxvb3qCo5ZwKh9kG4LT6_I5IhlJH7aGhyxXFvUK-DWNmoudF8"
+                  + "NAco9_h9iaGNj8q2ethFkMLs91kzk2PAcDTW9gb54h4FRWyuXpoQ"));
+  private static final BigInteger P =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "4BzEEOtIpmVdVEZNCqS7baC4crd0pqnRH_5IB3jw3bcxGn6QLvnEtfdUdi"
+                  + "YrqBdss1l58BQ3KhooKeQTa9AB0Hw_Py5PJdTJNPY8cQn7ouZ2KKDcmnPG"
+                  + "BY5t7yLc1QlQ5xHdwW1VhvKn-nXqhJTBgIPgtldC-KDV5z-y2XDwGUc"));
+  private static final BigInteger Q =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "uQPEfgmVtjL0Uyyx88GZFF1fOunH3-7cepKmtH4pxhtCoHqpWmT8YAmZxa"
+                  + "ewHgHAjLYsp1ZSe7zFYHj7C6ul7TjeLQeZD_YwD66t62wDmpe_HlB-TnBA"
+                  + "-njbglfIsRLtXlnDzQkv5dTltRJ11BKBBypeeF6689rjcJIDEz9RWdc"));
+  private static final BigInteger D =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "Eq5xpGnNCivDflJsRQBXHx1hdR1k6Ulwe2JZD50LpXyWPEAeP88vLNO97I"
+                  + "jlA7_GQ5sLKMgvfTeXZx9SE-7YwVol2NXOoAJe46sui395IW_GO-pWJ1O0"
+                  + "BkTGoVEn2bKVRUCgu-GjBVaYLU6f3l9kJfFNS3E0QbVdxzubSu3Mkqzjkn"
+                  + "439X0M_V51gfpRLI9JYanrC4D4qAdGcopV_0ZHHzQlBjudU2QvXt4ehNYT"
+                  + "CBr6XCLQUShb1juUO1ZdiYoFaFQT5Tw8bGUl_x_jTj3ccPDVZFD9pIuhLh"
+                  + "BOneufuBiB4cS98l2SR_RQyGWSeWjnczT0QU91p1DhOVRuOopznQ"));
+  private static final BigInteger DP =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "BwKfV3Akq5_MFZDFZCnW-wzl-CCo83WoZvnLQwCTeDv8uzluRSnm71I3Q"
+                  + "CLdhrqE2e9YkxvuxdBfpT_PI7Yz-FOKnu1R6HsJeDCjn12Sk3vmAktV2zb"
+                  + "34MCdy7cpdTh_YVr7tss2u6vneTwrA86rZtu5Mbr1C1XsmvkxHQAdYo0"));
+  private static final BigInteger DQ =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "h_96-mK1R_7glhsum81dZxjTnYynPbZpHziZjeeHcXYsXaaMwkOlODsWa"
+                  + "7I9xXDoRwbKgB719rrmI2oKr6N3Do9U0ajaHF-NKJnwgjMd2w9cjz3_-ky"
+                  + "NlxAr2v4IKhGNpmM5iIgOS1VZnOZ68m6_pbLBSp3nssTdlqvd0tIiTHU"));
+  private static final BigInteger Q_INV =
+      new BigInteger(
+          1,
+          Base64.urlSafeDecode(
+              "IYd7DHOhrWvxkwPQsRM2tOgrjbcrfvtQJipd-DlcxyVuuM9sQLdgjVk2o"
+                  + "y26F0EmpScGLq2MowX7fhd_QJQ3ydy5cY7YIBi87w93IKLEdfnbJtoOPLU"
+                  + "W0ITrJReOgo1cq9SbsxYawBgfp_gh6A5603k2-ZQwVK0JKSHuLFkuQ3U"));
+
+  @AccessesPartialKey
+  public static void createTestKeys() {
+    try {
+      JwtHmacParameters jwtHmacParameters =
+          JwtHmacParameters.builder()
+              .setKeySizeBytes(32)
+              .setKidStrategy(JwtHmacParameters.KidStrategy.IGNORED)
+              .setAlgorithm(JwtHmacParameters.Algorithm.HS256)
+              .build();
+      JwtHmacKey jwtHmacKey =
+          JwtHmacKey.builder()
+              .setParameters(jwtHmacParameters)
+              .setKeyBytes(SecretBytes.randomBytes(32))
+              .build();
+
+      JwtEcdsaParameters jwtEcdsaParameters =
+          JwtEcdsaParameters.builder()
+              .setKidStrategy(JwtEcdsaParameters.KidStrategy.IGNORED)
+              .setAlgorithm(JwtEcdsaParameters.Algorithm.ES256)
+              .build();
+      JwtEcdsaPublicKey jwtEcdsaPublicKey =
+          JwtEcdsaPublicKey.builder()
+              .setParameters(jwtEcdsaParameters)
+              .setPublicPoint(P256_PUBLIC_POINT)
+              .build();
+      JwtEcdsaPrivateKey jwtEcdsaPrivateKey =
+          JwtEcdsaPrivateKey.create(
+              jwtEcdsaPublicKey,
+              SecretBigInteger.fromBigInteger(P256_PRIVATE_VALUE, InsecureSecretKeyAccess.get()));
+
+      JwtEcdsaParameters jwtEcdsaKidParameters =
+          JwtEcdsaParameters.builder()
+              .setKidStrategy(JwtEcdsaParameters.KidStrategy.BASE64_ENCODED_KEY_ID)
+              .setAlgorithm(JwtEcdsaParameters.Algorithm.ES256)
+              .build();
+      JwtEcdsaPublicKey jwtEcdsaKidPublicKey =
+          JwtEcdsaPublicKey.builder()
+              .setParameters(jwtEcdsaKidParameters)
+              .setPublicPoint(P256_PUBLIC_POINT)
+              .setIdRequirement(123)
+              .build();
+      JwtEcdsaPrivateKey jwtEcdsaKidPrivateKey =
+          JwtEcdsaPrivateKey.create(
+              jwtEcdsaKidPublicKey,
+              SecretBigInteger.fromBigInteger(P256_PRIVATE_VALUE, InsecureSecretKeyAccess.get()));
+
+      JwtRsaSsaPkcs1Parameters jwtRsaSsaPkcs1Parameters =
+          JwtRsaSsaPkcs1Parameters.builder()
+              .setModulusSizeBits(2048)
+              .setPublicExponent(JwtRsaSsaPkcs1Parameters.F4)
+              .setKidStrategy(JwtRsaSsaPkcs1Parameters.KidStrategy.IGNORED)
+              .setAlgorithm(JwtRsaSsaPkcs1Parameters.Algorithm.RS256)
+              .build();
+      JwtRsaSsaPkcs1PublicKey jwtRsaSsaPkcs1PublicKey =
+          JwtRsaSsaPkcs1PublicKey.builder()
+              .setParameters(jwtRsaSsaPkcs1Parameters)
+              .setModulus(MODULUS)
+              .build();
+      RsaSsaPkcs1PrivateKey rsaSsaPkcs1PrivateKey =
+          RsaSsaPkcs1PrivateKey.builder()
+              .setPublicKey(jwtRsaSsaPkcs1PublicKey.getRsaSsaPkcs1PublicKey())
+              .setPrimes(
+                  SecretBigInteger.fromBigInteger(P, InsecureSecretKeyAccess.get()),
+                  SecretBigInteger.fromBigInteger(Q, InsecureSecretKeyAccess.get()))
+              .setPrivateExponent(SecretBigInteger.fromBigInteger(D, InsecureSecretKeyAccess.get()))
+              .setPrimeExponents(
+                  SecretBigInteger.fromBigInteger(DP, InsecureSecretKeyAccess.get()),
+                  SecretBigInteger.fromBigInteger(DQ, InsecureSecretKeyAccess.get()))
+              .setCrtCoefficient(
+                  SecretBigInteger.fromBigInteger(Q_INV, InsecureSecretKeyAccess.get()))
+              .build();
+      JwtRsaSsaPkcs1PrivateKey jwtRsaSsaPkcs1PrivateKey =
+          JwtRsaSsaPkcs1PrivateKey.builder()
+              .setPublicKey(jwtRsaSsaPkcs1PublicKey)
+              .setRsaSsaPkcs1PrivateKey(rsaSsaPkcs1PrivateKey)
+              .build();
+
+      JwtRsaSsaPssParameters jwtRsaSsaPssParameters =
+          JwtRsaSsaPssParameters.builder()
+              .setModulusSizeBits(2048)
+              .setPublicExponent(JwtRsaSsaPssParameters.F4)
+              .setKidStrategy(JwtRsaSsaPssParameters.KidStrategy.IGNORED)
+              .setAlgorithm(JwtRsaSsaPssParameters.Algorithm.PS256)
+              .build();
+      JwtRsaSsaPssPublicKey jwtRsaSsaPssPublicKey =
+          JwtRsaSsaPssPublicKey.builder()
+              .setParameters(jwtRsaSsaPssParameters)
+              .setModulus(MODULUS)
+              .build();
+      RsaSsaPssPrivateKey rsaSsaPssPrivateKey =
+          RsaSsaPssPrivateKey.builder()
+              .setPublicKey(jwtRsaSsaPssPublicKey.getRsaSsaPssPublicKey())
+              .setPrimes(
+                  SecretBigInteger.fromBigInteger(P, InsecureSecretKeyAccess.get()),
+                  SecretBigInteger.fromBigInteger(Q, InsecureSecretKeyAccess.get()))
+              .setPrivateExponent(SecretBigInteger.fromBigInteger(D, InsecureSecretKeyAccess.get()))
+              .setPrimeExponents(
+                  SecretBigInteger.fromBigInteger(DP, InsecureSecretKeyAccess.get()),
+                  SecretBigInteger.fromBigInteger(DQ, InsecureSecretKeyAccess.get()))
+              .setCrtCoefficient(
+                  SecretBigInteger.fromBigInteger(Q_INV, InsecureSecretKeyAccess.get()))
+              .build();
+      JwtRsaSsaPssPrivateKey jwtRsaSsaPssPrivateKey =
+          JwtRsaSsaPssPrivateKey.builder()
+              .setPublicKey(jwtRsaSsaPssPublicKey)
+              .setRsaSsaPssPrivateKey(rsaSsaPssPrivateKey)
+              .build();
+
+      jwtPrivateKeys =
+          new JwtSignaturePrivateKey[] {
+            jwtEcdsaPrivateKey,
+            jwtEcdsaKidPrivateKey,
+            jwtRsaSsaPkcs1PrivateKey,
+            jwtRsaSsaPssPrivateKey,
+          };
+      jwtMacKey = jwtHmacKey;
+    } catch (GeneralSecurityException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @SuppressWarnings("NonFinalStaticField") // has to be static because of @DataPoints
+  @DataPoints("jwtPrivateKeys")
+  public static JwtSignaturePrivateKey[] jwtPrivateKeys;
+
+  @SuppressWarnings("NonFinalStaticField")
+  public static JwtHmacKey jwtMacKey;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    createTestKeys();
+  }
+
+  @Test
+  public void get_throwsInFipsMode() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      assertThrows(GeneralSecurityException.class, JwtConfig2026::get);
+    }
+  }
+
+  @Test
+  public void jwtMac_computeVerify_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips() || TestUtil.isTsan()) {
+      return;
+    }
+
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(jwtMacKey).withRandomId().makePrimary())
+            .build();
+
+    JwtMac jwtMac = keysetHandle.getPrimitive(JwtConfig2026.get(), JwtMac.class);
+    JwtValidator validator = JwtValidator.newBuilder().allowMissingExpiration().build();
+
+    RawJwt rawToken = RawJwt.newBuilder().setJwtId("jwtId").withoutExpiration().build();
+    String compact = jwtMac.computeMacAndEncode(rawToken);
+    VerifiedJwt verifiedToken = jwtMac.verifyMacAndDecode(compact, validator);
+
+    assertThat(verifiedToken.getJwtId()).isEqualTo("jwtId");
+  }
+
+  @Theory
+  public void getPrimitive_signVerify_works(
+      @FromDataPoints("jwtPrivateKeys") JwtSignaturePrivateKey key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips() || TestUtil.isTsan()) {
+      return;
+    }
+
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    JwtPublicKeySign signer =
+        keysetHandle.getPrimitive(JwtConfig2026.get(), JwtPublicKeySign.class);
+    JwtPublicKeyVerify verifier =
+        keysetHandle
+            .getPublicKeysetHandle()
+            .getPrimitive(JwtConfig2026.get(), JwtPublicKeyVerify.class);
+    JwtValidator validator = JwtValidator.newBuilder().allowMissingExpiration().build();
+
+    RawJwt rawToken = RawJwt.newBuilder().setJwtId("jwtId").withoutExpiration().build();
+    String signedCompact = signer.signAndEncode(rawToken);
+    VerifiedJwt verifiedToken = verifier.verifyAndDecode(signedCompact, validator);
+
+    assertThat(verifiedToken.getJwtId()).isEqualTo("jwtId");
+  }
+
+  @Theory
+  public void serializeAndParsePrivateKey_works(
+      @FromDataPoints("jwtPrivateKeys") JwtSignaturePrivateKey key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(key).makePrimary();
+    if (key.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(key.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    Configuration config = JwtConfig2026.get();
+    byte[] serialized =
+        TinkProtoKeysetFormat.serializeKeyset(keysetHandle, InsecureSecretKeyAccess.get(), config);
+    KeysetHandle parsed =
+        TinkProtoKeysetFormat.parseKeyset(serialized, InsecureSecretKeyAccess.get(), config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
+  }
+
+  @Theory
+  public void serializeAndParsePublicKey_works(
+      @FromDataPoints("jwtPrivateKeys") JwtSignaturePrivateKey key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    JwtSignaturePublicKey publicKey = key.getPublicKey();
+    KeysetHandle.Builder.Entry entry = KeysetHandle.importKey(publicKey).makePrimary();
+    if (publicKey.getIdRequirementOrNull() == null) {
+      entry.withRandomId();
+    } else {
+      entry.withFixedId(publicKey.getIdRequirementOrNull());
+    }
+    KeysetHandle keysetHandle = KeysetHandle.newBuilder().addEntry(entry).build();
+
+    Configuration config = JwtConfig2026.get();
+    byte[] serialized = TinkProtoKeysetFormat.serializeKeysetWithoutSecret(keysetHandle, config);
+    KeysetHandle parsed = TinkProtoKeysetFormat.parseKeysetWithoutSecret(serialized, config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
+  }
+
+  @Theory
+  public void serializeAndParseParameters_works(
+      @FromDataPoints("jwtPrivateKeys") JwtSignaturePrivateKey key) throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    Parameters parameters = key.getParameters();
+    Configuration config = JwtConfig2026.get();
+    byte[] serialized = TinkProtoParametersFormat.serialize(parameters, config);
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, config);
+
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Test
+  public void serializeAndParseJwtHmacKey_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    KeysetHandle keysetHandle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(jwtMacKey).withRandomId().makePrimary())
+            .build();
+
+    Configuration config = JwtConfig2026.get();
+    byte[] serialized =
+        TinkProtoKeysetFormat.serializeKeyset(keysetHandle, InsecureSecretKeyAccess.get(), config);
+    KeysetHandle parsed =
+        TinkProtoKeysetFormat.parseKeyset(serialized, InsecureSecretKeyAccess.get(), config);
+
+    assertThat(parsed.equalsKeyset(keysetHandle)).isTrue();
+  }
+
+  @Test
+  public void serializeAndParseJwtHmacParameters_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    Parameters parameters = jwtMacKey.getParameters();
+    Configuration config = JwtConfig2026.get();
+    byte[] serialized = TinkProtoParametersFormat.serialize(parameters, config);
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, config);
+
+    assertThat(parsed).isEqualTo(parameters);
+  }
+}
