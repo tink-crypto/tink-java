1f21dd400199e0bbff9617a49813acf06ca4d58e
Thomas Holenstein <tholenst@google.com>
2026-08-04 02:14:24 -0700

    Refactor PrfBasedKeyDerivationKeyProtoSerialization to accept Configuration.
    PiperOrigin-RevId: 958890773
    Change-Id: I8714f08e17c751b1d8e84bd1f353240733f1c0f0

--- src/test/java/com/google/crypto/tink/keyderivation/internal/PrfBasedKeyDerivationKeyProtoSerializationTest.java
+++ src/test/java/com/google/crypto/tink/keyderivation/internal/PrfBasedKeyDerivationKeyProtoSerializationTest.java
@@ -21,6 +21,7 @@ import static com.google.crypto.tink.internal.TinkBugException.exceptionIsBug;
 import static com.google.crypto.tink.internal.testing.Asserts.assertEqualWhenValueParsed;
 import static org.junit.Assert.assertThrows;
 
+import com.google.crypto.tink.Configuration;
 import com.google.crypto.tink.InsecureSecretKeyAccess;
 import com.google.crypto.tink.Key;
 import com.google.crypto.tink.Parameters;
@@ -29,14 +30,18 @@ import com.google.crypto.tink.ProtoKeySerialization.KeyMaterialType;
 import com.google.crypto.tink.ProtoKeySerialization.OutputPrefixType;
 import com.google.crypto.tink.ProtoParametersSerialization;
 import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.aead.XChaCha20Poly1305Key;
 import com.google.crypto.tink.aead.XChaCha20Poly1305Parameters;
+import com.google.crypto.tink.aead.subtle.XChaCha20Poly1305ProtoSerialization;
 import com.google.crypto.tink.internal.MutableSerializationRegistry;
+import com.google.crypto.tink.internal.ProtoBasedConfigurationBuilder;
 import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationKey;
 import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationParameters;
 import com.google.crypto.tink.prf.AesCmacPrfKey;
 import com.google.crypto.tink.prf.AesCmacPrfParameters;
 import com.google.crypto.tink.prf.PrfConfig;
 import com.google.crypto.tink.prf.PrfParameters;
+import com.google.crypto.tink.prf.subtle.AesCmacPrfProtoSerialization;
 import com.google.crypto.tink.proto.AesCmacPrfKeyFormat;
 import com.google.crypto.tink.proto.KeyData;
 import com.google.crypto.tink.proto.KeyTemplate;
@@ -626,5 +631,358 @@ public final class PrfBasedKeyDerivationKeyProtoSerializationTest {
         GeneralSecurityException.class, () -> registry.serializeKey(key, /* access= */ null));
     assertThrows(
         GeneralSecurityException.class, () -> registry.parseKey(serialization, /* access= */ null));
+  }
+
+  private static final Configuration TEST_CONFIGURATION =
+      new ProtoBasedConfigurationBuilder()
+          .addKeySerializer(AesCmacPrfKey.class, AesCmacPrfProtoSerialization::serializeKey)
+          .addParametersSerializer(
+              AesCmacPrfParameters.class, AesCmacPrfProtoSerialization::serializeParameters)
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.AesCmacPrfKey",
+              AesCmacPrfProtoSerialization::parseKey)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesCmacPrfKey",
+              AesCmacPrfProtoSerialization::parseParameters)
+          .addKeySerializer(
+              XChaCha20Poly1305Key.class, XChaCha20Poly1305ProtoSerialization::serializeKey)
+          .addParametersSerializer(
+              XChaCha20Poly1305Parameters.class,
+              XChaCha20Poly1305ProtoSerialization::serializeParameters)
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key",
+              XChaCha20Poly1305ProtoSerialization::parseKey)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key",
+              XChaCha20Poly1305ProtoSerialization::parseParameters)
+          .build();
+
+  @Test
+  public void serializeParseParameters_withConfiguration_works() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            OutputPrefixType.RAW,
+            PrfBasedDeriverKeyFormat.newBuilder()
+                .setPrfKeyTemplate(
+                    KeyTemplate.newBuilder()
+                        .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+                        .setValue(PRF_KEY_FORMAT_PROTO.toByteString())
+                        .setOutputPrefixType(com.google.crypto.tink.proto.OutputPrefixType.RAW))
+                .setParams(
+                    PrfBasedDeriverParams.newBuilder()
+                        .setDerivedKeyTemplate(
+                            KeyTemplate.newBuilder()
+                                .setTypeUrl(
+                                    "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                                .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                                .setOutputPrefixType(
+                                    com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+                .build()
+                .toByteString());
+
+    ProtoParametersSerialization serialized =
+        PrfBasedKeyDerivationKeyProtoSerialization.serializeParameters(
+            parameters, TEST_CONFIGURATION);
+    assertEqualWhenValueParsed(PrfBasedDeriverKeyFormat.parser(), serialized, serialization);
+
+    Parameters parsed =
+        PrfBasedKeyDerivationKeyProtoSerialization.parseParameters(
+            serialization, TEST_CONFIGURATION);
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Test
+  public void serializeParseKey_withConfiguration_works() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    PrfBasedKeyDerivationKey key =
+        PrfBasedKeyDerivationKey.create(parameters, PRF_KEY, /* idRequirement= */ null);
+
+    KeyData prfAsKeyData =
+        KeyData.newBuilder()
+            .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+            .setKeyMaterialType(com.google.crypto.tink.proto.KeyData.KeyMaterialType.SYMMETRIC)
+            .setValue(
+                com.google.crypto.tink.proto.AesCmacPrfKey.newBuilder()
+                    .setVersion(0)
+                    .setKeyValue(
+                        ByteString.copyFrom(
+                            PRF_KEY.getKeyBytes().toByteArray(InsecureSecretKeyAccess.get())))
+                    .build()
+                    .toByteString())
+            .build();
+
+    PrfBasedDeriverKey protoKey =
+        PrfBasedDeriverKey.newBuilder()
+            .setPrfKey(prfAsKeyData)
+            .setParams(
+                PrfBasedDeriverParams.newBuilder()
+                    .setDerivedKeyTemplate(
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(
+                                com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+            .build();
+
+    ProtoKeySerialization serialization =
+        ProtoKeySerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            protoKey.toByteString(),
+            KeyMaterialType.SYMMETRIC,
+            OutputPrefixType.RAW,
+            /* idRequirement= */ null);
+
+    ProtoKeySerialization serialized =
+        PrfBasedKeyDerivationKeyProtoSerialization.serializeKey(
+            key, InsecureSecretKeyAccess.get(), TEST_CONFIGURATION);
+    assertEqualWhenValueParsed(PrfBasedDeriverKey.parser(), serialized, serialization);
+
+    Key parsed =
+        PrfBasedKeyDerivationKeyProtoSerialization.parseKey(
+            serialization, InsecureSecretKeyAccess.get(), TEST_CONFIGURATION);
+    assertThat(parsed.equalsKey(key)).isTrue();
+  }
+
+  private static final Configuration CONFIG_WITHOUT_PRF =
+      new ProtoBasedConfigurationBuilder()
+          .addKeySerializer(
+              XChaCha20Poly1305Key.class, XChaCha20Poly1305ProtoSerialization::serializeKey)
+          .addParametersSerializer(
+              XChaCha20Poly1305Parameters.class,
+              XChaCha20Poly1305ProtoSerialization::serializeParameters)
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key",
+              XChaCha20Poly1305ProtoSerialization::parseKey)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key",
+              XChaCha20Poly1305ProtoSerialization::parseParameters)
+          .build();
+
+  private static final Configuration CONFIG_WITHOUT_DERIVED =
+      new ProtoBasedConfigurationBuilder()
+          .addKeySerializer(AesCmacPrfKey.class, AesCmacPrfProtoSerialization::serializeKey)
+          .addParametersSerializer(
+              AesCmacPrfParameters.class, AesCmacPrfProtoSerialization::serializeParameters)
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.AesCmacPrfKey",
+              AesCmacPrfProtoSerialization::parseKey)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesCmacPrfKey",
+              AesCmacPrfProtoSerialization::parseParameters)
+          .build();
+
+  @Test
+  public void serializeParseParameters_withoutPrfKeySerialization_throws() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            OutputPrefixType.RAW,
+            PrfBasedDeriverKeyFormat.newBuilder()
+                .setPrfKeyTemplate(
+                    KeyTemplate.newBuilder()
+                        .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+                        .setValue(PRF_KEY_FORMAT_PROTO.toByteString())
+                        .setOutputPrefixType(com.google.crypto.tink.proto.OutputPrefixType.RAW))
+                .setParams(
+                    PrfBasedDeriverParams.newBuilder()
+                        .setDerivedKeyTemplate(
+                            KeyTemplate.newBuilder()
+                                .setTypeUrl(
+                                    "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                                .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                                .setOutputPrefixType(
+                                    com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+                .build()
+                .toByteString());
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.serializeParameters(
+                parameters, CONFIG_WITHOUT_PRF));
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.parseParameters(
+                serialization, CONFIG_WITHOUT_PRF));
+  }
+
+  @Test
+  public void serializeParseParameters_withoutDerivedKeySerialization_throws() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    ProtoParametersSerialization serialization =
+        ProtoParametersSerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            OutputPrefixType.RAW,
+            PrfBasedDeriverKeyFormat.newBuilder()
+                .setPrfKeyTemplate(
+                    KeyTemplate.newBuilder()
+                        .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+                        .setValue(PRF_KEY_FORMAT_PROTO.toByteString())
+                        .setOutputPrefixType(com.google.crypto.tink.proto.OutputPrefixType.RAW))
+                .setParams(
+                    PrfBasedDeriverParams.newBuilder()
+                        .setDerivedKeyTemplate(
+                            KeyTemplate.newBuilder()
+                                .setTypeUrl(
+                                    "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                                .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                                .setOutputPrefixType(
+                                    com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+                .build()
+                .toByteString());
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.serializeParameters(
+                parameters, CONFIG_WITHOUT_DERIVED));
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.parseParameters(
+                serialization, CONFIG_WITHOUT_DERIVED));
+  }
+
+  @Test
+  public void serializeParseKey_withoutPrfKeySerialization_throws() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    PrfBasedKeyDerivationKey key =
+        PrfBasedKeyDerivationKey.create(parameters, PRF_KEY, /* idRequirement= */ null);
+
+    KeyData prfAsKeyData =
+        KeyData.newBuilder()
+            .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+            .setKeyMaterialType(com.google.crypto.tink.proto.KeyData.KeyMaterialType.SYMMETRIC)
+            .setValue(
+                com.google.crypto.tink.proto.AesCmacPrfKey.newBuilder()
+                    .setVersion(0)
+                    .setKeyValue(
+                        ByteString.copyFrom(
+                            PRF_KEY.getKeyBytes().toByteArray(InsecureSecretKeyAccess.get())))
+                    .build()
+                    .toByteString())
+            .build();
+
+    PrfBasedDeriverKey protoKey =
+        PrfBasedDeriverKey.newBuilder()
+            .setPrfKey(prfAsKeyData)
+            .setParams(
+                PrfBasedDeriverParams.newBuilder()
+                    .setDerivedKeyTemplate(
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(
+                                com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+            .build();
+
+    ProtoKeySerialization serialization =
+        ProtoKeySerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            protoKey.toByteString(),
+            KeyMaterialType.SYMMETRIC,
+            OutputPrefixType.RAW,
+            /* idRequirement= */ null);
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.serializeKey(
+                key, InsecureSecretKeyAccess.get(), CONFIG_WITHOUT_PRF));
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.parseKey(
+                serialization, InsecureSecretKeyAccess.get(), CONFIG_WITHOUT_PRF));
+  }
+
+  @Test
+  public void serializeParseKey_withoutDerivedKeySerialization_throws() throws Exception {
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(PRF_PARAMETERS)
+            .setDerivedKeyParameters(DERIVED_PARAMETERS_RAW)
+            .build();
+
+    PrfBasedKeyDerivationKey key =
+        PrfBasedKeyDerivationKey.create(parameters, PRF_KEY, /* idRequirement= */ null);
+
+    KeyData prfAsKeyData =
+        KeyData.newBuilder()
+            .setTypeUrl("type.googleapis.com/google.crypto.tink.AesCmacPrfKey")
+            .setKeyMaterialType(com.google.crypto.tink.proto.KeyData.KeyMaterialType.SYMMETRIC)
+            .setValue(
+                com.google.crypto.tink.proto.AesCmacPrfKey.newBuilder()
+                    .setVersion(0)
+                    .setKeyValue(
+                        ByteString.copyFrom(
+                            PRF_KEY.getKeyBytes().toByteArray(InsecureSecretKeyAccess.get())))
+                    .build()
+                    .toByteString())
+            .build();
+
+    PrfBasedDeriverKey protoKey =
+        PrfBasedDeriverKey.newBuilder()
+            .setPrfKey(prfAsKeyData)
+            .setParams(
+                PrfBasedDeriverParams.newBuilder()
+                    .setDerivedKeyTemplate(
+                        KeyTemplate.newBuilder()
+                            .setTypeUrl(
+                                "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key")
+                            .setValue(DERIVED_KEY_FORMAT_PROTO.toByteString())
+                            .setOutputPrefixType(
+                                com.google.crypto.tink.proto.OutputPrefixType.RAW)))
+            .build();
+
+    ProtoKeySerialization serialization =
+        ProtoKeySerialization.create(
+            "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+            protoKey.toByteString(),
+            KeyMaterialType.SYMMETRIC,
+            OutputPrefixType.RAW,
+            /* idRequirement= */ null);
+
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.serializeKey(
+                key, InsecureSecretKeyAccess.get(), CONFIG_WITHOUT_DERIVED));
+    assertThrows(
+        GeneralSecurityException.class,
+        () ->
+            PrfBasedKeyDerivationKeyProtoSerialization.parseKey(
+                serialization, InsecureSecretKeyAccess.get(), CONFIG_WITHOUT_DERIVED));
   }
 }
