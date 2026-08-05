2b173da63ca3b2bd03d1909d08f4dfd59aee24e7
Thomas Holenstein <tholenst@google.com>
2026-08-05 06:44:56 -0700

    Add internal KeyDerivationConfig2026 for Tink Java key derivation.
    PiperOrigin-RevId: 959629257
    Change-Id: I85f7003478684155eab990958886ab19efd50206

---
+++ src/main/java/com/google/crypto/tink/keyderivation/internal/KeyDerivationConfig2026.java
@@ -0,0 +1,360 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.keyderivation.internal;
+
+import com.google.crypto.tink.AccessesPartialKey;
+import com.google.crypto.tink.Configuration;
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.LowLevelCryptoCaller;
+import com.google.crypto.tink.Parameters;
+import com.google.crypto.tink.SecretKeyAccess;
+import com.google.crypto.tink.aead.AesCtrHmacAeadKey;
+import com.google.crypto.tink.aead.AesCtrHmacAeadParameters;
+import com.google.crypto.tink.aead.AesGcmKey;
+import com.google.crypto.tink.aead.AesGcmParameters;
+import com.google.crypto.tink.aead.AesGcmSivKey;
+import com.google.crypto.tink.aead.AesGcmSivParameters;
+import com.google.crypto.tink.aead.XChaCha20Poly1305Key;
+import com.google.crypto.tink.aead.XChaCha20Poly1305Parameters;
+import com.google.crypto.tink.aead.internal.XChaCha20Poly1305ProtoSerialization;
+import com.google.crypto.tink.aead.subtle.AesCtrHmacAeadProtoSerialization;
+import com.google.crypto.tink.aead.subtle.AesGcmProtoSerialization;
+import com.google.crypto.tink.aead.subtle.AesGcmSivProtoSerialization;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.daead.AesSivKey;
+import com.google.crypto.tink.daead.AesSivParameters;
+import com.google.crypto.tink.daead.subtle.AesSivProtoSerialization;
+import com.google.crypto.tink.internal.ProtoBasedConfigurationBuilder;
+import com.google.crypto.tink.internal.Util;
+import com.google.crypto.tink.keyderivation.KeysetDeriver;
+import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationKey;
+import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationParameters;
+import com.google.crypto.tink.mac.HmacKey;
+import com.google.crypto.tink.mac.HmacParameters;
+import com.google.crypto.tink.mac.subtle.HmacProtoSerialization;
+import com.google.crypto.tink.prf.HkdfPrfKey;
+import com.google.crypto.tink.prf.HkdfPrfParameters;
+import com.google.crypto.tink.prf.HmacPrfKey;
+import com.google.crypto.tink.prf.HmacPrfParameters;
+import com.google.crypto.tink.prf.subtle.HkdfPrfProtoSerialization;
+import com.google.crypto.tink.prf.subtle.HmacPrfProtoSerialization;
+import com.google.crypto.tink.signature.Ed25519Parameters;
+import com.google.crypto.tink.signature.Ed25519PrivateKey;
+import com.google.crypto.tink.signature.Ed25519PublicKey;
+import com.google.crypto.tink.signature.subtle.Ed25519ProtoSerialization;
+import com.google.crypto.tink.streamingaead.AesGcmHkdfStreamingKey;
+import com.google.crypto.tink.streamingaead.AesGcmHkdfStreamingParameters;
+import com.google.crypto.tink.streamingaead.internal.AesGcmHkdfStreamingProtoSerialization;
+import com.google.crypto.tink.subtle.Ed25519Sign;
+import com.google.crypto.tink.subtle.prf.HkdfStreamingPrf;
+import com.google.crypto.tink.util.Bytes;
+import com.google.crypto.tink.util.SecretBytes;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import javax.annotation.Nullable;
+
+/**
+ * KeyDerivationConfig2026 contains the following primitives and algorithms for {@link
+ * KeysetDeriver}:
+ *
+ * <ul>
+ *   <li>HKDF-PRF-based key derivation
+ * </ul>
+ */
+@LowLevelCryptoCaller
+public class KeyDerivationConfig2026 {
+  private KeyDerivationConfig2026() {}
+
+  // This is the configuration which the {@link PrfBasedKeyDerivationKeyProtoSerialization} uses to
+  // internally serialize PRF keys and derived parameters. It hence includes the following key
+  // types:
+  // ==== Key types which can be used to derive new keys (need key and parameter serialization)
+  //  * HkdfPrf
+  // ==== Key types which can only be derived (only need parameter serialization)
+  //  * Hmac
+  //  * HmacPrf
+  //  * AesGcm
+  //  * AesCtrHmacAead
+  //  * XChaCha20Poly1305
+  //  * AesSiv
+  //  * AesGcmSiv
+  //  * AesGcmHkdfStreaming
+  //  * Ed25519
+  private static final Configuration CONFIG_FOR_KEY =
+      new ProtoBasedConfigurationBuilder()
+          // HkdfPrf
+          .addKeySerializer(HkdfPrfKey.class, HkdfPrfProtoSerialization::serializeKey)
+          .addParametersSerializer(
+              HkdfPrfParameters.class, HkdfPrfProtoSerialization::serializeParameters)
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.HkdfPrfKey",
+              HkdfPrfProtoSerialization::parseKey)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.HkdfPrfKey",
+              HkdfPrfProtoSerialization::parseParameters)
+          // Hmac
+          .addParametersSerializer(
+              HmacParameters.class, HmacProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.HmacKey",
+              HmacProtoSerialization::parseParameters)
+          // HmacPrf
+          .addParametersSerializer(
+              HmacPrfParameters.class, HmacPrfProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.HmacPrfKey",
+              HmacPrfProtoSerialization::parseParameters)
+          // AesGcm
+          .addParametersSerializer(
+              AesGcmParameters.class, AesGcmProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesGcmKey",
+              AesGcmProtoSerialization::parseParameters)
+          // AesCtrHmacAead
+          .addParametersSerializer(
+              AesCtrHmacAeadParameters.class, AesCtrHmacAeadProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesCtrHmacAeadKey",
+              AesCtrHmacAeadProtoSerialization::parseParameters)
+          // XChaCha20Poly1305
+          .addParametersSerializer(
+              XChaCha20Poly1305Parameters.class,
+              XChaCha20Poly1305ProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.XChaCha20Poly1305Key",
+              XChaCha20Poly1305ProtoSerialization::parseParameters)
+          // AesSiv
+          .addParametersSerializer(
+              AesSivParameters.class, AesSivProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesSivKey",
+              AesSivProtoSerialization::parseParameters)
+          // AesGcmSiv
+          .addParametersSerializer(
+              AesGcmSivParameters.class, AesGcmSivProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesGcmSivKey",
+              AesGcmSivProtoSerialization::parseParameters)
+          // AesGcmHkdfStreaming
+          .addParametersSerializer(
+              AesGcmHkdfStreamingParameters.class,
+              AesGcmHkdfStreamingProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.AesGcmHkdfStreamingKey",
+              AesGcmHkdfStreamingProtoSerialization::parseParameters)
+          // Ed25519
+          .addParametersSerializer(
+              Ed25519Parameters.class, Ed25519ProtoSerialization::serializeParameters)
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.Ed25519PrivateKey",
+              Ed25519ProtoSerialization::parseParameters)
+          .build();
+
+  private static final Configuration CONFIGURATION =
+      new ProtoBasedConfigurationBuilder()
+          .addPrimitiveWrapper(
+              KeysetDeriver.class, KeyDeriver.class, KeysetDeriverWrapper.WRAPPER::wrap)
+          // PrfBasedKeyDerivation
+          .addKeyCreator(
+              PrfBasedKeyDerivationParameters.class,
+              KeyDerivationConfig2026::createPrfBasedKeyDerivationKey)
+          .addPrimitiveConstructor(
+              KeyDerivationConfig2026::createPrfBasedKeyDeriver,
+              PrfBasedKeyDerivationKey.class,
+              KeyDeriver.class)
+          .addKeySerializer(
+              PrfBasedKeyDerivationKey.class,
+              (key, access) ->
+                  PrfBasedKeyDerivationKeyProtoSerialization.serializeKey(
+                      key, access, CONFIG_FOR_KEY))
+          .addParametersSerializer(
+              PrfBasedKeyDerivationParameters.class,
+              parameters ->
+                  PrfBasedKeyDerivationKeyProtoSerialization.serializeParameters(
+                      parameters, CONFIG_FOR_KEY))
+          .addKeyParser(
+              "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+              (serialization, access) ->
+                  PrfBasedKeyDerivationKeyProtoSerialization.parseKey(
+                      serialization, access, CONFIG_FOR_KEY))
+          .addParametersParser(
+              "type.googleapis.com/google.crypto.tink.PrfBasedDeriverKey",
+              serialization ->
+                  PrfBasedKeyDerivationKeyProtoSerialization.parseParameters(
+                      serialization, CONFIG_FOR_KEY))
+          .build();
+
+  /** Returns the {@link Configuration} instance. */
+  public static Configuration get() throws GeneralSecurityException {
+    if (TinkFipsUtil.useOnlyFips()) {
+      throw new GeneralSecurityException(
+          "Cannot use non-FIPS-compliant KeyDerivationConfig2026 in FIPS mode");
+    }
+    return CONFIGURATION;
+  }
+
+  // We hard code here the list of supported keys we can derive. It might be that this should
+  // even be hard coded in PrfBasedDerivationKey instead (since it really cannot change, ever). But
+  // right now we hard code it here (at least partially because for some users we might want to
+  // extend the list with keys which are not available for everyone).
+  // The supported keys are:
+  //  * Hmac
+  //  * HmacPrf
+  //  * AesGcm
+  //  * AesCtrHmacAead
+  //  * XChaCha20Poly1305
+  //  * AesSiv
+  //  * AesGcmSiv
+  //  * AesGcmHkdfStreaming
+  //  * Ed25519
+  @AccessesPartialKey
+  private static Key createKeyFromRandomness(
+      Parameters parameters,
+      InputStream stream,
+      @Nullable Integer idRequirement,
+      SecretKeyAccess access)
+      throws GeneralSecurityException {
+    if (parameters instanceof HmacParameters) {
+      HmacParameters hmacParameters = (HmacParameters) parameters;
+      return HmacKey.builder()
+          .setParameters(hmacParameters)
+          .setIdRequirement(idRequirement)
+          .setKeyBytes(Util.readIntoSecretBytes(stream, hmacParameters.getKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof HmacPrfParameters) {
+      HmacPrfParameters hmacPrfParameters = (HmacPrfParameters) parameters;
+      return HmacPrfKey.builder()
+          .setParameters(hmacPrfParameters)
+          .setKeyBytes(
+              Util.readIntoSecretBytes(stream, hmacPrfParameters.getKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof AesGcmParameters) {
+      AesGcmParameters aesGcmParameters = (AesGcmParameters) parameters;
+      return AesGcmKey.builder()
+          .setParameters(aesGcmParameters)
+          .setIdRequirement(idRequirement)
+          .setKeyBytes(Util.readIntoSecretBytes(stream, aesGcmParameters.getKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof AesCtrHmacAeadParameters) {
+      AesCtrHmacAeadParameters aesCtrHmacParameters = (AesCtrHmacAeadParameters) parameters;
+      return AesCtrHmacAeadKey.builder()
+          .setParameters(aesCtrHmacParameters)
+          .setIdRequirement(idRequirement)
+          .setAesKeyBytes(
+              Util.readIntoSecretBytes(stream, aesCtrHmacParameters.getAesKeySizeBytes(), access))
+          .setHmacKeyBytes(
+              Util.readIntoSecretBytes(stream, aesCtrHmacParameters.getHmacKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof XChaCha20Poly1305Parameters) {
+      XChaCha20Poly1305Parameters xChaCha20Poly1305Parameters =
+          (XChaCha20Poly1305Parameters) parameters;
+      return XChaCha20Poly1305Key.create(
+          xChaCha20Poly1305Parameters.getVariant(),
+          Util.readIntoSecretBytes(stream, 32, access),
+          idRequirement);
+    }
+    if (parameters instanceof AesSivParameters) {
+      AesSivParameters aesSivParameters = (AesSivParameters) parameters;
+      return AesSivKey.builder()
+          .setParameters(aesSivParameters)
+          .setIdRequirement(idRequirement)
+          .setKeyBytes(Util.readIntoSecretBytes(stream, aesSivParameters.getKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof AesGcmSivParameters) {
+      AesGcmSivParameters aesGcmSivParameters = (AesGcmSivParameters) parameters;
+      return AesGcmSivKey.builder()
+          .setParameters(aesGcmSivParameters)
+          .setIdRequirement(idRequirement)
+          .setKeyBytes(
+              Util.readIntoSecretBytes(stream, aesGcmSivParameters.getKeySizeBytes(), access))
+          .build();
+    }
+    if (parameters instanceof AesGcmHkdfStreamingParameters) {
+      AesGcmHkdfStreamingParameters streamingParameters =
+          (AesGcmHkdfStreamingParameters) parameters;
+      return AesGcmHkdfStreamingKey.create(
+          streamingParameters,
+          Util.readIntoSecretBytes(stream, streamingParameters.getKeySizeBytes(), access));
+    }
+    if (parameters instanceof Ed25519Parameters) {
+      Ed25519Parameters ed25519Parameters = (Ed25519Parameters) parameters;
+      SecretBytes pseudorandomness =
+          Util.readIntoSecretBytes(stream, Ed25519Sign.SECRET_KEY_LEN, access);
+      Ed25519Sign.KeyPair keyPair =
+          Ed25519Sign.KeyPair.newKeyPairFromSeed(pseudorandomness.toByteArray(access));
+      Ed25519PublicKey publicKey =
+          Ed25519PublicKey.create(
+              ed25519Parameters.getVariant(),
+              Bytes.copyFrom(keyPair.getPublicKey()),
+              idRequirement);
+      return Ed25519PrivateKey.create(
+          publicKey, SecretBytes.copyFrom(keyPair.getPrivateKey(), access));
+    }
+    throw new GeneralSecurityException(
+        "Cannot use key derivation to derive key for parameters "
+            + parameters
+            + ": unsupported parameters class");
+  }
+
+  @AccessesPartialKey
+  private static KeyDeriver createPrfBasedKeyDeriver(PrfBasedKeyDerivationKey key)
+      throws GeneralSecurityException {
+    KeyDeriver deriver =
+        PrfBasedKeyDeriver.create(
+            k -> {
+              if (k instanceof HkdfPrfKey) {
+                return HkdfStreamingPrf.create((HkdfPrfKey) k);
+              }
+              throw new GeneralSecurityException("Unsupported PRF key type: " + k.getClass());
+            },
+            KeyDerivationConfig2026::createKeyFromRandomness,
+            key);
+    return deriver;
+  }
+
+  @AccessesPartialKey
+  private static HkdfPrfKey createHkdfPrfKey(
+      HkdfPrfParameters parameters, @Nullable Integer idRequirement)
+      throws GeneralSecurityException {
+    if (idRequirement != null) {
+      throw new GeneralSecurityException("PRF Keys are not expected to have an id Requirement");
+    }
+    return HkdfPrfKey.builder()
+        .setParameters(parameters)
+        .setKeyBytes(SecretBytes.randomBytes(parameters.getKeySizeBytes()))
+        .build();
+  }
+
+  @AccessesPartialKey
+  @LowLevelCryptoCaller
+  private static PrfBasedKeyDerivationKey createPrfBasedKeyDerivationKey(
+      PrfBasedKeyDerivationParameters parameters, @Nullable Integer idRequirement)
+      throws GeneralSecurityException {
+    Parameters prfParameters = parameters.getPrfParameters();
+    if (prfParameters instanceof HkdfPrfParameters) {
+      HkdfPrfKey prfKey =
+          createHkdfPrfKey((HkdfPrfParameters) prfParameters, /* idRequirement= */ null);
+      return PrfBasedKeyDerivationKey.create(parameters, prfKey, idRequirement);
+    }
+    throw new GeneralSecurityException("Unsupported PRF parameters: " + prfParameters.getClass());
+  }
+}
---
+++ src/test/java/com/google/crypto/tink/keyderivation/internal/KeyDerivationConfig2026Test.java
@@ -0,0 +1,224 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.google.crypto.tink.keyderivation.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.crypto.tink.InsecureSecretKeyAccess;
+import com.google.crypto.tink.Key;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.Parameters;
+import com.google.crypto.tink.TinkProtoKeysetFormat;
+import com.google.crypto.tink.TinkProtoParametersFormat;
+import com.google.crypto.tink.aead.AesGcmKeyManager;
+import com.google.crypto.tink.aead.AesGcmParameters;
+import com.google.crypto.tink.config.internal.TinkFipsUtil;
+import com.google.crypto.tink.keyderivation.KeysetDeriver;
+import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationKey;
+import com.google.crypto.tink.keyderivation.PrfBasedKeyDerivationParameters;
+import com.google.crypto.tink.keyderivation.internal.test.PrfBasedKeyDeriverTestVectors;
+import com.google.crypto.tink.prf.HkdfPrfKey;
+import com.google.crypto.tink.prf.HkdfPrfParameters;
+import com.google.crypto.tink.subtle.Hex;
+import com.google.crypto.tink.util.SecretBytes;
+import java.security.GeneralSecurityException;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Tests for {@link KeyDerivationConfig2026}. */
+@RunWith(JUnit4.class)
+public class KeyDerivationConfig2026Test {
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    AesGcmKeyManager.register(true);
+  }
+
+  private static PrfBasedKeyDerivationKey createTestKey() throws GeneralSecurityException {
+    HkdfPrfParameters hkdfPrfParameters =
+        HkdfPrfParameters.builder()
+            .setKeySizeBytes(32)
+            .setHashType(HkdfPrfParameters.HashType.SHA256)
+            .build();
+    HkdfPrfKey prfKey =
+        HkdfPrfKey.builder()
+            .setParameters(hkdfPrfParameters)
+            .setKeyBytes(
+                SecretBytes.copyFrom(
+                    Hex.decode("0102030405060708091011121314151617181920212123242526272829303132"),
+                    InsecureSecretKeyAccess.get()))
+            .build();
+    AesGcmParameters derivedKeyParameters =
+        AesGcmParameters.builder()
+            .setKeySizeBytes(16)
+            .setIvSizeBytes(12)
+            .setTagSizeBytes(16)
+            .build();
+    PrfBasedKeyDerivationParameters parameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(prfKey.getParameters())
+            .setDerivedKeyParameters(derivedKeyParameters)
+            .build();
+    return PrfBasedKeyDerivationKey.create(parameters, prfKey, null);
+  }
+
+  @Test
+  public void config_throwsIfInFipsMode() throws Exception {
+    Assume.assumeTrue(TinkFipsUtil.useOnlyFips());
+
+    assertThrows(GeneralSecurityException.class, KeyDerivationConfig2026::get);
+  }
+
+  @Test
+  public void createKey_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    PrfBasedKeyDerivationKey key = createTestKey();
+    Key createdKey =
+        KeyDerivationConfig2026.get().createKey(key.getParameters(), key.getIdRequirementOrNull());
+
+    assertThat(createdKey.getParameters()).isEqualTo(key.getParameters());
+    assertThat(createdKey.getIdRequirementOrNull()).isEqualTo(key.getIdRequirementOrNull());
+  }
+
+  @Test
+  public void serializeAndParseKey_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    PrfBasedKeyDerivationKey key = createTestKey();
+    KeysetHandle handle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withRandomId().makePrimary())
+            .build();
+
+    byte[] serializedKeyset =
+        TinkProtoKeysetFormat.serializeKeyset(
+            handle, InsecureSecretKeyAccess.get(), KeyDerivationConfig2026.get());
+    KeysetHandle parsedHandle =
+        TinkProtoKeysetFormat.parseKeyset(
+            serializedKeyset, InsecureSecretKeyAccess.get(), KeyDerivationConfig2026.get());
+
+    assertThat(parsedHandle.size()).isEqualTo(1);
+    assertThat(parsedHandle.getAt(0).getKey().getParameters()).isEqualTo(key.getParameters());
+  }
+
+  @Test
+  public void serializeAndParseParameters_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    PrfBasedKeyDerivationKey key = createTestKey();
+    Parameters parameters = key.getParameters();
+    byte[] serialized =
+        TinkProtoParametersFormat.serialize(parameters, KeyDerivationConfig2026.get());
+    Parameters parsed = TinkProtoParametersFormat.parse(serialized, KeyDerivationConfig2026.get());
+
+    assertThat(parsed).isEqualTo(parameters);
+  }
+
+  @Test
+  public void createPrimitive_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    PrfBasedKeyDerivationKey key = createTestKey();
+    KeysetHandle handle =
+        KeysetHandle.newBuilder()
+            .addEntry(KeysetHandle.importKey(key).withFixedId(123).makePrimary())
+            .build();
+
+    KeysetDeriver deriver = handle.getPrimitive(KeyDerivationConfig2026.get(), KeysetDeriver.class);
+    assertThat(deriver).isNotNull();
+
+    KeysetHandle derivedKeyset = deriver.deriveKeyset(new byte[] {1, 2, 3});
+    assertThat(derivedKeyset.size()).isEqualTo(1);
+  }
+
+  @Test
+  public void testVectors_deriveKeyset_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    for (PrfBasedKeyDeriverTestVectors.TestVector t :
+        PrfBasedKeyDeriverTestVectors.createTestVectors()) {
+      PrfBasedKeyDerivationParameters derivationParameters =
+          PrfBasedKeyDerivationParameters.builder()
+              .setPrfParameters(t.prfKey.getParameters())
+              .setDerivedKeyParameters(t.derivedKeyParameters)
+              .build();
+      Integer idRequirement = t.expectedKey.getIdRequirementOrNull();
+      PrfBasedKeyDerivationKey key =
+          PrfBasedKeyDerivationKey.create(derivationParameters, t.prfKey, idRequirement);
+
+      KeysetHandle handle =
+          KeysetHandle.newBuilder()
+              .addEntry(
+                  KeysetHandle.importKey(key)
+                      .withFixedId(idRequirement == null ? 789789 : idRequirement)
+                      .makePrimary())
+              .build();
+
+      KeysetDeriver deriver =
+          handle.getPrimitive(KeyDerivationConfig2026.get(), KeysetDeriver.class);
+      KeysetHandle derivedKeyset = deriver.deriveKeyset(Hex.decode(t.inputHex));
+
+      assertThat(derivedKeyset.size()).isEqualTo(1);
+      assertThat(derivedKeyset.getAt(0).getKey().getParameters()).isEqualTo(t.derivedKeyParameters);
+      assertThat(derivedKeyset.getAt(0).getKey().getIdRequirementOrNull()).isEqualTo(idRequirement);
+      assertThat(derivedKeyset.getAt(0).getKey().equalsKey(t.expectedKey)).isTrue();
+    }
+  }
+
+  @Test
+  public void testVector_aesGcmSiv_deriveKeyset_works() throws Exception {
+    if (TinkFipsUtil.useOnlyFips()) {
+      return;
+    }
+    PrfBasedKeyDeriverTestVectors.TestVector t =
+        PrfBasedKeyDeriverTestVectors.createAesGcmSivTestVector();
+    PrfBasedKeyDerivationParameters derivationParameters =
+        PrfBasedKeyDerivationParameters.builder()
+            .setPrfParameters(t.prfKey.getParameters())
+            .setDerivedKeyParameters(t.derivedKeyParameters)
+            .build();
+    Integer idRequirement = t.expectedKey.getIdRequirementOrNull();
+    PrfBasedKeyDerivationKey key =
+        PrfBasedKeyDerivationKey.create(derivationParameters, t.prfKey, idRequirement);
+
+    KeysetHandle handle =
+        KeysetHandle.newBuilder()
+            .addEntry(
+                KeysetHandle.importKey(key)
+                    .withFixedId(idRequirement == null ? 789789 : idRequirement)
+                    .makePrimary())
+            .build();
+
+    KeysetDeriver deriver = handle.getPrimitive(KeyDerivationConfig2026.get(), KeysetDeriver.class);
+    KeysetHandle derivedKeyset = deriver.deriveKeyset(Hex.decode(t.inputHex));
+
+    assertThat(derivedKeyset.size()).isEqualTo(1);
+    assertThat(derivedKeyset.getAt(0).getKey().getParameters()).isEqualTo(t.derivedKeyParameters);
+    assertThat(derivedKeyset.getAt(0).getKey().getIdRequirementOrNull()).isEqualTo(idRequirement);
+    assertThat(derivedKeyset.getAt(0).getKey().equalsKey(t.expectedKey)).isTrue();
+  }
+}
